2fe96400ce74b509af8f2243682bd34e1172a93f
Hussein Elgridly <hussein@broadinstitute.org>
2015-07-21 14:01:49 -0400

    Populating table with response from Rawls upload JSON request

--- src/cljs/org/broadinstitute/firecloud_ui/page/import_data.cljs
+++ src/cljs/org/broadinstitute/firecloud_ui/page/import_data.cljs
@@ -3,17 +3,29 @@
     [clojure.string]
     [dmohs.react :as react]
     [org.broadinstitute.firecloud-ui.common.components :as comps]
+    [org.broadinstitute.firecloud-ui.common.table :as table]
     [org.broadinstitute.firecloud-ui.common.style :as style]
     [org.broadinstitute.firecloud-ui.utils :as utils]
     ))
 
 
-(defn- submit-entities [workspaceName workspaceNamespace entityFile]
+(defn- pretend-submit-entities
+  "Insta-populates uploadResults with some sample JSON so you can just hit Upload. For testing purposes."
+  [state workspaceName workspaceNamespace entityFile]
+  (swap! state assoc
+    :entities-loaded? true
+    :uploadResults (utils/parse-json-string "[
+        { \"entityName\" : \"foo\", \"entityType\" : \"bar\", \"success\" : false, \"message\" : \"ohno\" },
+        { \"entityName\" : \"bar\", \"entityType\" : \"baz\", \"success\" : true, \"message\" : \"hooray\" }
+      ]")))
+
+
+(defn- submit-entities [state workspaceName workspaceNamespace entityFile]
   (utils/ajax-orch
     (str "/workspaces/" workspaceNamespace "/" workspaceName "/importEntitiesJSON")
     {:method :post
      :encType "multipart/form-data"
-     :on-done :default
+     :on-done #(swap! state assoc :entities-loaded? true :uploadResults (utils/parse-json-string (.-responseText %)))
      :data (utils/generate-form-data {:entities entityFile})}))
 
 
@@ -29,6 +41,35 @@
        [:input {:type "file" :name "entities" :ref "entities"}]
        [comps/Button {:text "Upload"
                       :onClick (fn [e] (submit-entities
+                                         state
                                          (-> (@refs "workspaceName") .getDOMNode .-value)
                                          (-> (@refs "workspaceNamespace") .getDOMNode .-value)
-                                         (-> (@refs "entities") .getDOMNode .-files (aget 0))))}]]])})
+                                         (-> (@refs "entities") .getDOMNode .-files (aget 0))))}]]
+
+      (if (:entities-loaded? @state)
+        [:div {}
+         [:div {:style {:fontSize "125%" :fontWeight 500}} "Import results"]
+         [table/Table
+          (let [cell-style {:flexBasis "8ex" :flexGrow 1 :whiteSpace "nowrap" :overflow "hidden"
+                            :borderLeft (str "1px solid " (:line-gray style/colors))}
+                header-label (fn [text & [padding]]
+                               [:span {:style {:paddingLeft (or padding "1em")}}
+                                [:span {:style {:fontSize "90%"}} text]])]
+            {:columns [{:label (header-label "Entity Name")
+                        :style cell-style
+                        :header-style {:borderLeft 0}}
+                       {:label (header-label "Entity Type")
+                        :style cell-style
+                        :header-style {:borderLeft 0}}
+                       {:label (header-label "Success")
+                        :style cell-style
+                        :header-style {:borderLeft 0}}
+                       {:label (header-label "Message")
+                        :style cell-style
+                        :header-style {:borderLeft 0}}]
+             :data (map (fn [entity]
+                          [(entity "entityName")
+                           (entity "entityType")
+                           (str (entity "success"))
+                           (entity "message")])
+                     (:uploadResults @state))})]])])})
