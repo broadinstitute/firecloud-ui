eda83fad25dfeba93e03bcd1f3220308e60e2506
Edward A. Salinas <esalinas@broadinstitute.org>
2015-07-23 14:00:41 -0400

    improve aethestics/view of method configs listing

--- src/cljs/org/broadinstitute/firecloud_ui/page/workspace/workspace_method_confs.cljs
+++ src/cljs/org/broadinstitute/firecloud_ui/page/workspace/workspace_method_confs.cljs
@@ -13,24 +13,31 @@
     (fn [i] {:name (rand-nth ["rand_name_1" "rand_name_2" "rand_name_3" "rand_name_4"])
          :namespace (str "ns_s_" (inc i))
          :root-ent-type (str "r_e_t_" (inc i))
-         :workspaceName {:namespace (str "*ws_ns_" (inc i))
-                        :name (str "*ws_n_" (inc i))}
-         :methodStoreMethod {:namespace (str "*ms_ns_" (inc i))
-                            :name (str "*ms_n_" (inc i))
-                            :version (str "*ms_v_" (inc i))}
-         :methodStoreConfig {:namespace (str "*msc_ns_" (inc i))
-                             :name (str "*msc_n_" (inc i))
-                             :version (str "*msc_v_" (inc i))
+         :workspaceName {:namespace (str "ws_ns_" (inc i))
+                        :name (str "ws_n_" (inc i))}
+         :methodStoreMethod {:methodNamespace (str "ms_ns_" (inc i))
+                            :methodName (str "ms_n_" (inc i))
+                            :methodVersion (str "ms_v_" (inc i))}
+         :methodStoreConfig {:namespace (str "msc_ns_" (inc i))
+                             :name (str "msc_n_" (inc i))
+                             :version (str "msc_v_" (inc i))
                              }
-         :inputs {:i1 (str "*i_1_" (inc i))
-                  :i2 (str "*i_2_" (inc i))}
-         :outputs {:o1 (str "*o_1_" (inc i))
-                  :o2 (str "*o_2_" (inc i))}
-         :prerequisites {:p1 (str "*p_1_" (inc i))
-                  :p2 (str "*p_2_" (inc i))}})
+         :inputs {:i1 (str "i_1_" (inc i))
+                  :i2 (str "i_2_" (inc i))}
+         :outputs {:o1 (str "o_1_" (inc i))
+                  :o2 (str "o_2_" (inc i))}
+         :prerequisites {:p1 (str "p_1_" (inc i))
+                  :p2 (str "p_2_" (inc i))}})
     (range (rand-int 50))))
 
 
+
+
+(defn
+  stringify_map
+  [the_map]
+  (for [s (keys the_map)] (let [k s v (get the_map k)] (str k "," v " ; ")))
+  )
 
 
 (react/defc WorkspaceMethodsConfigurationsList
@@ -39,7 +46,7 @@
      [:div {:style {:padding "0 4em"}}
       (if (zero? (count (:method-confs props)))
         [:div {:style {:textAlign "center" :backgroundColor (:background-gray style/colors)
-                       :padding "1em 0" :borderRadius 8}}
+                       :padding   "1em 0" :borderRadius 8}}
          "There are no method configurations to display."]
         [table/Table
          (let [cell-style {:flexBasis  "8ex" :flexGrow 1 :whiteSpace "nowrap" :overflow "hidden"
@@ -49,41 +56,46 @@
                                [:span {:style {:fontSize "90%"}} text]])]
            {:columns [{:label (header-label "Name")
                        :style (merge cell-style {:borderLeft "none"})}
-                      {:label (header-label "Namespace")
-                       :style cell-style
+                      {:label        (header-label "Namespace")
+                       :style        cell-style
                        :header-style {:borderLeft "none"}}
-                      {:label (header-label "Root Entity Type")
-                       :style cell-style
+                      {:label        (header-label "Root Entity Type")
+                       :style        cell-style
                        :header-style {:borderLeft "none"}}
-                      {:label (header-label "Workspace Name")
+                      {:label        (header-label "Workspace Name")
                        :header-style {:borderLeft "none"}
-                       :style cell-style}
-                      {:label (header-label "Method Store Method")
-                       :style cell-style
+                       :style        cell-style}
+                      {:label        (header-label "Method Store Method")
+                       :style        cell-style
                        :header-style {:borderLeft "none"}}
-                      {:label (header-label "Method Store Config")
-                       :style cell-style
+                      {:label        (header-label "Method Store Config")
+                       :style        cell-style
                        :header-style {:borderLeft "none"}}
-                      {:label (header-label "Inputs")
+                      {:label        (header-label "Inputs")
                        :header-style {:borderLeft "none"}
-                       :style cell-style}
-                      {:label (header-label "Outputs")
-                       :style cell-style
+                       :style        cell-style}
+                      {:label        (header-label "Outputs")
+                       :style        cell-style
                        :header-style {:borderLeft "none"}}
-                      {:label (header-label "Pre-Requisites")
-                       :style (merge cell-style {:flexBasis "30ex"})
+                      {:label        (header-label "Pre-Requisites")
+                       :style        (merge cell-style {:flexBasis "30ex"})
                        :header-style {:borderLeft "none"}}]
-            :data (map (fn [m]
-                         [(m "name")
-                          (m "namespace")
-                          (m "root-ent-type")
-                          (m "workspaceName")
-                          (m "methodStoreMethod")
-                          (m "methodStoreConfig")
-                          (m "inputs")
-                          (m "outputs")
-                          (m "prerequisites")])
-                    (:method-confs props))})])])})
+            :data    (map (fn [m]
+                            [(m "name")
+                             (m "namespace")
+                             (m "root-ent-type")
+                             (str (get (m "workspaceName") "namespace") ":"
+                                  (get (m "workspaceName") "name"))
+                             (str (get (m "methodStoreMethod") "methodNamespace") ":"
+                                  (get (m "methodStoreMethod") "methodName") ":"
+                                  (get (m "methodVersion") "methodVersion"))
+                             (str (get (m "methodStoreConfig") "namespace") ":"
+                                  (get (m "methodStoreConfig") "name") ":"
+                                  (get (m "methodStoreConfig") "version"))
+                             (stringify_map (m "inputs"))
+                             (stringify_map (m "outputs"))
+                             (stringify_map (m "prerequisites"))])
+                          (:method-confs props))})])])})
 
 
 (react/defc WorkspaceMethodConfigurations
