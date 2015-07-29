a35f2c35dc51e1b484b72973c937a1d9e9def946
Edward A. Salinas <esalinas@broadinstitute.org>
2015-07-29 16:52:22 -0400

    first work on creating an AJAX call for importing list
    got import list reload when you make it appear/reappear
    
    discovered that the AJAX call was was being made
    twice.  This additional if statement gets it to call once
    
    addressed whitespace concerns/issues
    fixed a function call name to refer to the correct name after renaming in utils
    (map-to-string)
    
    whitespace/newline  adjustment
    
    whitespace rearrangment and minor refactoring

--- src/cljs/org/broadinstitute/firecloud_ui/page/workspace/method_config_importer.cljs
+++ src/cljs/org/broadinstitute/firecloud_ui/page/workspace/method_config_importer.cljs
@@ -1,6 +1,7 @@
 (ns org.broadinstitute.firecloud-ui.page.workspace.method-config-importer
   (:require
     [dmohs.react :as react]
+    clojure.string
     [org.broadinstitute.firecloud-ui.utils :as utils]
     [org.broadinstitute.firecloud-ui.common :as common]
     [org.broadinstitute.firecloud-ui.common.icons :as icons]
@@ -32,64 +33,92 @@
 
 
 (react/defc ImportWorkspaceMethodsConfigurationsList
-  {:render
-   (fn [{:keys [props]}]
+  {:component-did-mount
+   (fn [{:keys [state props]}]
+     (utils/ajax-orch
+       (str "/workspaces/" (:selected-workspace-namespace props)
+         "/" (:selected-workspace props) "/methodconfigs")
+       {:on-done (fn [{:keys [success? xhr]}]
+                   (if success?
+                     (do
+                       (swap! state assoc
+                         :loaded-import-confs? true
+                         :method-confs (utils/parse-json-string (.-responseText xhr)))
+                       )
+                     (swap! state assoc :error-message (.-statusText xhr))))
+        :canned-response {:responseText (utils/->json-string (create-mock-methodconfs-import))
+                          :status 200
+                          :delay-ms (rand-int 2000)}}))
+   :render
+   (fn [{:keys [state props]}]
      [:div {}
-      (if (zero? (count (:method-confs props)))
-        [:div {:style {:textAlign "center" :backgroundColor (:background-gray style/colors)
-                       :padding "1em 0" :margin "0 4em" :borderRadius 8}}
-         "There are no method configurations to display."]
-        [table/AdvancedTable
-         (let [header (fn [children] [:div {:style {:fontWeight 600 :fontSize 13
-                                                    :padding "14px 16px"
-                                                    :borderLeft "1px solid #777777"
-                                                    :color "#fff" :backgroundColor (:header-darkgray style/colors)}}
-                                      children])
-               cell (fn [children] [:span {:style {:paddingLeft 16}} children])]
-           {:columns [{:header-component [:div {:style {:padding "13px 0 12px 12px"
-                                                        :backgroundColor (:header-darkgray style/colors)}}
-                                          [:input {:type "checkbox" :ref "imcallcheck"}]]
-                       :starting-width 42 :resizable? false
-                       :cell-renderer (fn [row-num conf]
-                                        [:div {:style {:paddingLeft 12}}
-                                         [:input {:type "checkbox" :ref (str "imc_" row-num)}]])}
-                      {:header-component (header "Name") :starting-width 200
-                       :cell-renderer (fn [row-num conf]
-                                        (cell [:a {:href "javascript:;"
-                                                   :style {:color (:button-blue style/colors)
-                                                           :textDecoration "none"}} (conf "name")]))}
-                      {:header-component (header "Namespace") :starting-width 200
-                       :cell-renderer (fn [row-num conf] (cell (conf "namespace")))}
-                      {:header-component (header "Type") :starting-width 100
-                       :cell-renderer (fn [row-num conf] (cell (conf "rootEntityType")))}
-                      {:header-component (header "Workspace Name") :starting-width 160
-                       :cell-renderer (fn [row-num conf] (cell (str ((conf "workspaceName") "namespace") ":"
-                                                                 ((conf "workspaceName") "name"))))}
-                      {:header-component (header "Method") :starting-width 210
-                       :cell-renderer (fn [row-num conf] (cell (str ((conf "methodStoreMethod") "methodNamespace") ":"
-                                                                 ((conf "methodStoreMethod") "methodName") ":"
-                                                                 ((conf "methodStoreMethod") "methodVersion"))))}
-                      {:header-component (header "Config") :starting-width 290
-                       :cell-renderer (fn [row-num conf] (cell (str ((conf "methodStoreConfig") "methodConfigNamespace") ":"
-                                                                 ((conf "methodStoreConfig") "methodConfigName") ":"
-                                                                 ((conf "methodStoreConfig") "methodConfigVersion"))))}
-                      {:header-component (header "Inputs") :starting-width 200
-                       :cell-renderer (fn [row-num conf] (cell (utils/map-to-string (conf "inputs"))))}
-                      {:header-component (header "Outputs") :starting-width 200
-                       :cell-renderer (fn [row-num conf] (cell (utils/map-to-string (conf "outputs"))))}
-                      {:header-component (header "Prerequisites") :starting-width 200
-                       :cell-renderer (fn [row-num conf] (cell (clojure.string/join ", " (conf "prerequisites"))))}]
-            :data (:method-confs props)
-            :row-props (fn [row-num conf]
-                         {:style {:fontSize "80%" :fontWeight 500
-                                  :paddingTop 10 :paddingBottom 7
-                                  :backgroundColor (if (even? row-num) (:background-gray style/colors) "#fff")}})
-            :paginator :below :paginator-space 8})])])})
+      (cond
+        (:loaded-import-confs? @state)
+        (if (zero? (count (:method-confs @state)))
+          [:div {:style {:textAlign "center" :backgroundColor (:background-gray style/colors)
+                         :padding "1em 0" :margin "0 4em" :borderRadius 8}}
+           "There are no method configurations to display for import!"]
+          [table/AdvancedTable
+           (let [header (fn [children] [:div {:style {:fontWeight 600 :fontSize 13
+                                                      :padding "14px 16px"
+                                                      :borderLeft "1px solid #777777"
+                                                      :color "#fff" :backgroundColor
+                                                     (:header-darkgray style/colors)}}
+                                        children])
+                 cell (fn [children] [:span {:style {:paddingLeft 16}} children])]
+             {:columns [{:header-component [:div {:style {:padding "13px 0 12px 12px"
+                                                          :backgroundColor (:header-darkgray style/colors)}}
+                                            [:input {:type "checkbox" :ref "imcallcheck"}]]
+                         :starting-width 42 :resizable? false
+                         :cell-renderer (fn [row-num conf]
+                                          [:div {:style {:paddingLeft 12}}
+                                           [:input {:type "checkbox" :ref (str "imc_" row-num)}]])}
+                        {:header-component (header "Name") :starting-width 200
+                         :cell-renderer (fn [row-num conf]
+                                          (cell [:a {:href "javascript:;"
+                                                     :style {:color (:button-blue style/colors)
+                                                             :textDecoration "none"}} (conf "name")]))}
+                        {:header-component (header "Namespace") :starting-width 200
+                         :cell-renderer (fn [row-num conf] (cell (conf "namespace")))}
+                        {:header-component (header "Type") :starting-width 100
+                         :cell-renderer (fn [row-num conf] (cell (conf "rootEntityType")))}
+                        {:header-component (header "Workspace Name") :starting-width 160
+                         :cell-renderer (fn [row-num conf] (cell (str ((conf "workspaceName") "namespace") ":"
+                                                                   ((conf "workspaceName") "name"))))}
+                        {:header-component (header "Method") :starting-width 210
+                         :cell-renderer (fn [row-num conf]
+                                          (cell (str
+                                                  ((conf "methodStoreMethod") "methodNamespace") ":"
+                                                  ((conf "methodStoreMethod") "methodName") ":"
+                                                  ((conf "methodStoreMethod") "methodVersion"))))}
+                        {:header-component (header "Config") :starting-width 290
+                         :cell-renderer (fn [row-num conf]
+                                          (cell (str ((conf "methodStoreConfig") "methodConfigNamespace") ":"
+                                                     ((conf "methodStoreConfig") "methodConfigName") ":"
+                                                      ((conf "methodStoreConfig") "methodConfigVersion"))))}
+                        {:header-component (header "Inputs") :starting-width 200
+                         :cell-renderer (fn [row-num conf] (cell (utils/map-to-string (conf "inputs"))))}
+                        {:header-component (header "Outputs") :starting-width 200
+                         :cell-renderer (fn [row-num conf]
+                                          (cell (utils/map-to-string (conf "outputs"))))}
+                        {:header-component (header "Prerequisites") :starting-width 200
+                         :cell-renderer (fn [row-num conf]
+                                          (clojure.string/join "," (conf "prerequisites")))}]
+              :data (:method-confs @state)
+              :row-props (fn [row-num conf]
+                           {:style {:fontSize "80%" :fontWeight 500
+                                    :paddingTop 10 :paddingBottom 7
+                                    :backgroundColor (if (even? row-num)
+                                                       (:background-gray style/colors)
+                                                       "#fff")}})})])
+
+        (:error-message @state) [:div {:style {:color "red"}}
+                                 "FireCloud service returned error: " (:error-message @state)]
+        :else [comps/Spinner {:text "Loading configurations for import..."}])])})
 
 
-(defn- modal-import-background [state]
+(def modal-import-background
   {:backgroundColor "rgba(82, 129, 197, 0.4)"
-   :display (when-not (:import-overlay-shown? @state) "none")
    :overflowX "hidden" :overflowY "scroll"
    :position "fixed" :zIndex 9999
    :top 0 :right 0 :bottom 0 :left 0})
@@ -104,24 +133,23 @@
    :width "90%"})
 
 
-(defn- render-import-overlay [state]
+(defn render-import-overlay [state]
   (let [clear-import-overlay #(swap! state assoc :import-overlay-shown? false)]
-    [:div {:style (modal-import-background state)
-           :onKeyDown (common/create-key-handler [:esc] clear-import-overlay)}
-     [:div {:style modal-import-content}
-      [:div {:style {:position "absolute" :right 2 :top 2}}
-       [:div {:style {:backgroundColor (:button-blue style/colors) :color "#fff"
-                      :padding "0.5em" :cursor "pointer"}
-              :onClick #(swap! state assoc :import-overlay-shown? false)}
-        (icons/font-icon {:style {:fontSize "60%"}} :x)]]
-      [:div {:style {:backgroundColor "#fff"
-                     :borderBottom (str "1px solid " (:line-gray style/colors))
-                     :padding "20px 48px 18px"}}
-       [:div {:style {:fontSize 24 :align "center" :textAlign "center" :paddingBottom "0.5em"}}
-        "Select Method Configurations For Import"]
-       [ImportWorkspaceMethodsConfigurationsList {:method-confs (utils/parse-json-string
-                                                                  (utils/->json-string
-                                                                    (create-mock-methodconfs-import)))}]
-       [:div {:style {:paddingTop "0.5em"}}
-        [comps/Button {:style :add :text "Add selected to workspace"
-                       :onClick #(swap! state assoc :import-overlay-shown? false)}]]]]]))
+    (when (:import-overlay-shown? @state)
+      [:div {:style modal-import-background
+             :onKeyDown (common/create-key-handler [:esc] clear-import-overlay)}
+       [:div {:style modal-import-content}
+        [:div {:style {:position "absolute" :right 2 :top 2}}
+         [:div {:style {:backgroundColor (:button-blue style/colors) :color "#fff"
+                        :padding "0.5em" :cursor "pointer"}
+                :onClick #(swap! state assoc :import-overlay-shown? false)}
+          (icons/font-icon {:style {:fontSize "60%"}} :x)]]
+        [:div {:style {:backgroundColor "#fff"
+                       :borderBottom (str "1px solid " (:line-gray style/colors))
+                       :padding "20px 48px 18px"}}
+         [:div {:style {:fontSize 24 :align "center" :textAlign "center" :paddingBottom "0.5em"}}
+          "Select Method Configurations For Import"]
+         [ImportWorkspaceMethodsConfigurationsList]
+         [:div {:style {:paddingTop "0.5em"}}
+          [comps/Button {:style :add :text "Add selected to workspace"
+                         :onClick #(swap! state assoc :import-overlay-shown? false)}]]]]])))
