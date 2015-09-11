a344e5babe7b127017be95a9249a15c88cb4f99e
David Mohs <dgit@mohsinc.com>
2015-09-11 10:55:52 -0400

    Use readline wrapper for hot reloader if present

--- script/dev/start-hot-reloader.sh
+++ script/dev/start-hot-reloader.sh
@@ -5,5 +5,12 @@ IFS=$'\n\t'
 # Starts figwheel so changes are immediately loaded into the running browser window and appear
 # without requiring a page reload.
 
-./script/common/build.sh hot-reload
+if which rlfe; then
+  WRAPPER='rlfe '
+elif which rlwrap; then
+  WRAPPER='rlwrap '
+else
+  WRAPPER=''
+fi
 
+exec bash -c "$WRAPPER"'./script/common/build.sh hot-reload'
