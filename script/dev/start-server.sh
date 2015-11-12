92074d0739367bf86a1ac8154c7dca9443f9b3a6
David Mohs <dgit@mohsinc.com>
2015-11-12 18:27:23 -0500

    Pull vault token from standard location if available

--- script/dev/start-server.sh
+++ script/dev/start-server.sh
@@ -2,17 +2,14 @@
 set -euox pipefail
 IFS=$'\n\t'
 
-# Starts server for development.
-# - exposes Figwheel port (3449)
-# - allows HTTP since Figwheel does not support HTTPS
-# - mounts PWD over deployed code
-# - overrides the build type to use non-minimized code
+VAULT_TOKEN=${VAULT_TOKEN:-$(<~/.vault-token)}
 
 if [[ "$PWD" != "$HOME"* ]]; then
   echo 'Docker does not support mounting directories outside of your home directory.'
   exit 1;
 fi
 
-docker run --rm -it -v "$PWD":/working broadinstitute/dsde-toolbox render-templates.sh local $VAULT_TOKEN
+docker run --rm -it -v "$PWD":/working broadinstitute/dsde-toolbox \
+  render-templates.sh local $VAULT_TOKEN
 
-docker-compose -p firecloud -f target/config/docker-compose.yaml up -d
+docker-compose -p firecloud -f target/config/docker-compose.yaml up
