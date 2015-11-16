4b2ee9823824d7e38ae39a4107aa3e456f71acbd
David Mohs <dgit@mohsinc.com>
2015-11-16 11:48:52 -0500

    Don't clobber configs on clean build

--- script/common/clean-build-dir.sh
+++ script/common/clean-build-dir.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-set -euo pipefail
+set -euox pipefail
 IFS=$'\n\t'
 
 mkdir -p target
 # Instead of removing the directory, we remove all files in it. This ensures that if there is
 # already something using the directory (e.g., a web server serving files from it) it doesn't get
 # screwed up.
-rm -rf target/*
+# Also, don't clobber the config directory.
+find target -mindepth 1 -maxdepth 1 -and -not -name 'config' -exec rm -rf '{}' ';'
