3ddafba341f20cb6f32658366186ccd764d0c2c6
David Mohs <dgit@mohsinc.com>
2015-11-18 16:42:38 -0500

    Separate config generation from server starting so configs can be manually tweaked during development

--- script/dev/start-server.sh
+++ script/dev/start-server.sh
@@ -2,14 +2,8 @@
 set -euox pipefail
 IFS=$'\n\t'
 
-VAULT_TOKEN=${VAULT_TOKEN:-$(<~/.vault-token)}
-
-if [[ "$PWD" != "$HOME"* ]]; then
-  echo 'Docker does not support mounting directories outside of your home directory.'
-  exit 1;
+if [ ! -e target/config/firecloud-ui-compose.yaml ]; then
+  ./script/dev/create-config.sh
 fi
 
-docker run --rm -it -v "$PWD":/working broadinstitute/dsde-toolbox \
-  render-templates.sh local $VAULT_TOKEN
-
-docker-compose -p firecloud -f target/config/docker-compose.yaml up
+docker-compose -p firecloud -f target/config/firecloud-ui-compose.yaml up
