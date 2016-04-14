2bdd4156726eda23dfa131c642163bfc466bb420
David Mohs <dgit@mohsinc.com>
2016-04-14 13:09:04 -0400

    Open a new window for sign-in flow

--- src/cljs/org/broadinstitute/firecloud_ui/config.cljs
+++ src/cljs/org/broadinstitute/firecloud_ui/config.cljs
@@ -4,6 +4,9 @@
 (def config (atom nil))
 
 
+(defn sign-in-allowed-access-token-listener-host []
+  (get-in @config ["signIn" "allowedAccessTokenListenerHost"]))
 (defn api-url-root [] (get @config "apiUrlRoot"))
+(defn sign-in-callback-url [] (get-in @config ["signIn" "callbackUrl"]))
 (defn debug? [] (get @config "isDebug"))
 (defn tcga-namespace [] (get @config "tcgaNamespace"))
