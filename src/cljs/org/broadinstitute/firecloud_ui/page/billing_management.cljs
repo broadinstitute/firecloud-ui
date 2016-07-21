9a192bfea6c93ab3d8568a120366cfdba506fc9e
Matt Putnam <putnam@broadinstitute.org>
2016-07-21 12:47:23 -0400

    move loading billing accounts into dialog

--- src/cljs/org/broadinstitute/firecloud_ui/page/billing_management.cljs
+++ src/cljs/org/broadinstitute/firecloud_ui/page/billing_management.cljs
@@ -15,43 +15,58 @@
 
 (react/defc CreateBillingProjectDialog
   {:render
-   (fn [{:keys [props state this]}]
+   (fn [{:keys [state this]}]
      [dialog/OKCancelForm
       {:header "Create Billing Project"
        :dismiss-self #(modal/pop-modal)
        :content
        (react/create-element
-         (if (empty? (:billing-accounts props))
-           [:div {} "No billing accounts :( [todo: help text]"]
-           [:div {}
-            [:div {:style {:fontSize "120%" :marginBottom "0.5ex"}}
-             "Select a billing account:"]
-            [:div {:style {:width 750 :backgroundColor "white" :padding "1em"}}
-             [table/Table
-              {:columns [{:header "Account Name" :starting-width 300
-                          :content-renderer
-                          (fn [[name has-access]]
-                            (if has-access
-                              (style/create-link {:text name :onClick #(swap! state assoc :selected-account name)})
-                              name))}
-                         {:header "Firecloud Access?" :starting-width 150}]
-               :data (:billing-accounts props)
-               :row-style (fn [row-index [[acct-name _] _]]
-                            {:backgroundColor
-                             (cond (= acct-name (:selected-account @state)) "yellow"
-                                   (even? row-index) (:background-gray style/colors)
-                                   :else "#fff")})
-               :->row (fn [{:strs [accountName firecloudHasAccess]}]
-                        [[accountName firecloudHasAccess]
-                         (if firecloudHasAccess "Yes" "No")])}]]
-            [:div {:style {:fontSize "120%" :margin "1em 0 0.5ex 0"}}
-             "Name:"]
-            [input/TextField {:ref "name-field"
-                              :style {:width "100%"}
-                              :predicates [(input/nonempty "Name")]}]
-            (style/create-validation-error-message (:validation-errors @state))
-            [comps/ErrorViewer {:error (:server-error @state)}]]))
+         (let [{:keys [billing-accounts billing-acct-error]} @state]
+           (cond billing-acct-error [comps/ErrorViewer {:error billing-acct-error}]
+                 (not billing-accounts) [comps/Spinner {:text "Loading billing accounts..."}]
+                 :else
+                 (if (empty? billing-accounts)
+                   [:div {} "No billing accounts :( [todo: help text]"]
+                   [:div {}
+                    [:div {:style {:fontSize "120%" :marginBottom "0.5ex"}}
+                     "Select a billing account:"]
+                    [:div {:style {:width 750 :backgroundColor "white" :padding "1em"}}
+                     [table/Table
+                      {:columns [{:header "Account Name" :starting-width 300
+                                  :content-renderer
+                                  (fn [[name has-access]]
+                                    (if has-access
+                                      (style/create-link {:text name :onClick #(swap! state assoc :selected-account name)})
+                                      name))}
+                                 {:header "Firecloud Access?" :starting-width 150}]
+                       :data billing-accounts
+                       :row-style (fn [row-index [[acct-name _] _]]
+                                    {:backgroundColor
+                                     (cond (= acct-name (:selected-account @state)) "yellow"
+                                           (even? row-index) (:background-gray style/colors)
+                                           :else "#fff")})
+                       :->row (fn [{:strs [accountName firecloudHasAccess]}]
+                                [[accountName firecloudHasAccess]
+                                 (if firecloudHasAccess "Yes" "No")])}]]
+                    [:div {:style {:fontSize "120%" :margin "1em 0 0.5ex 0"}}
+                     "Name:"]
+                    [input/TextField {:ref "name-field"
+                                      :style {:width "100%"}
+                                      :predicates [(input/nonempty "Name")]}]
+                    (style/create-validation-error-message (:validation-errors @state))
+                    [comps/ErrorViewer {:error (:server-error @state)}]]))))
        :ok-button [comps/Button {:text "OK" :onClick #(react/call :create-billing-project this)}]}])
+   :component-did-mount
+   (fn [{:keys [state]}]
+     (endpoints/call-ajax-orch
+       {:endpoint (endpoints/get-billing-accounts)
+        :on-done
+        (fn [{:keys [success? get-parsed-response]}]
+          (swap! state assoc :billing-accounts [{"accountName" "billingAccounts/00473A-04A1D8-155CAB"
+                                                 "firecloudHasAccess" false}
+                                                {"accountName" "billingAccounts/foo-bar-baz"
+                                                 "firecloudHasAccess" true}])
+          #_(swap! state assoc (if success? :billing-accounts :billing-acct-error) (get-parsed-response)))}))
    :create-billing-project
    (fn [{:keys [props state refs]}]
      (let [account (:selected-account @state)]
@@ -88,28 +103,16 @@
          (float-right
            (when false ; hidden until implemented
              [comps/Button {:text "Create New Billing Project"
-                            :disabled? (cond (:billing-acct-error @state) (:billing-acct-error @state)
-                                             (nil? (:billing-accounts @state)) "Loading billing accounts")
-                            :onClick #(fn []
+                            :onClick (fn []
                                        (modal/push-modal
-                                         [CreateBillingProjectDialog {:billing-accounts (:billing-accounts @state)
-                                                                      :on-success #(react/call :reload this)}]))}]))
+                                         [CreateBillingProjectDialog {:on-success #(react/call :reload this)}]))}]))
          :data (:projects @state)
          :->row (fn [item]
                   [(item "projectName")
                   (item "role")])}]))
    :component-did-mount
-   (fn [{:keys [this state]}]
-     (react/call :load-data this)
-     (endpoints/call-ajax-orch
-       {:endpoint (endpoints/get-billing-accounts)
-        :on-done
-        (fn [{:keys [success? get-parsed-response]}]
-          (swap! state assoc :billing-accounts [{"accountName" "billingAccounts/00473A-04A1D8-155CAB"
-                                                 "firecloudHasAccess" false}
-                                                {"accountName" "billingAccounts/foo-bar-baz"
-                                                 "firecloudHasAccess" true}])
-          #_(swap! state assoc (if success? :billing-accounts :billing-acct-error) (get-parsed-response)))}))
+   (fn [{:keys [this]}]
+     (react/call :load-data this))
    :load-data
    (fn [{:keys [state]}]
      (endpoints/call-ajax-orch
