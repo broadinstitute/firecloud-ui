7c87b5144c41c694d705d77875e86e1fab7afb7a
David Mohs <dgit@mohsinc.com>
2016-10-24 17:05:39 -0400

    Figwheel always on 3449 (#534)
    * Figwheel always on 3449
    
    * Remove previous Figwheel starting script
    
    * Symlink in clean so it's always there
    
    * Update README

--- script/clean-build-dir.sh
+++ script/clean-build-dir.sh
@@ -4,6 +4,7 @@ set -euox pipefail
 
 
 mkdir -p target
+ln -sf target resources
 
 # Instead of removing the directory, we remove all files in it. This ensures that if there is
 # already something using the directory (e.g., a web server serving files from it) it doesn't get
