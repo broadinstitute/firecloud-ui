4e8be57af798e2db1aeb494cde6b96d23acbcbc0
Janet Whittredge <jlmwhitt@broadinstitute.org>
2016-11-02 08:54:25 -0400

    formatting

--- src/cljs/org/broadinstitute/firecloud_ui/page/workspace/method_configs/launch_analysis.cljs
+++ src/cljs/org/broadinstitute/firecloud_ui/page/workspace/method_configs/launch_analysis.cljs
@@ -88,8 +88,8 @@
                      :warning-triangle)
          (str "Warning: This will launch " wf-count " workflows")]]))
    [:div {:style {:textAlign "right" :fontSize "80%"}} (style/create-link {:text  (str "Cromwell Version: " (:cromwell-version props))
-                                                           :target "_blank"
-                                                           :href 
+                                                                           :target "_blank"
+                                                                           :href
         (str "https://github.com/broadinstitute/cromwell/releases/tag/" (:cromwell-version props))})]
    (style/create-validation-error-message (:validation-errors @state))
    [comps/ErrorViewer {:error (:launch-server-error @state)}]])
