5ccef5f6a1f639352fece4ee3e4092e24dde11f8
Isaac Zarsky <zarsky@broadinstitute.org>
2016-11-18 17:03:20 -0500

    non-editable acl entries no longer in disabled inputs

--- src/cljs/org/broadinstitute/firecloud_ui/page/workspace/summary/acl_editor.cljs
+++ src/cljs/org/broadinstitute/firecloud_ui/page/workspace/summary/acl_editor.cljs
@@ -24,39 +24,37 @@
      [:div {}
       (when (:saving? @state)
         [comps/Blocker {:banner "Updating..."}])
-      [:span {:style {:fontSize "90%"}} "Project Owner(s)"]
+      [:div {:style {:padding "0.5rem 0" :fontSize "90%"}} "Project Owner(s)"]
       (map-indexed
        (fn [i acl-entry]
-         [:div {:style {:padding "0.5em"}}
+         [:div {:style {:padding "0.5rem 0" :borderTop style/standard-line}}
           (:email acl-entry)])
        (:project-owner-acl-vec @state))
-      [:div {:style {:padding "0.5em 0" :fontSize "90%" :marginTop "0.5em"}}
-       [:div {:style {:float "left" :width 400}} "User ID"]
-       [:div {:style {:float "right" :width 200 :marginLeft "1em"}} "Access Level"]
-       (common/clear-both)]
+      [:div {:style {:padding "0.5rem 0" :fontSize "90%" :marginTop "0.5rem"}}
+       [:div {:style {:display "inline-block" :width 400}} "User ID"]
+       [:div {:style {:display "inline-block" :width 200 :marginLeft "1rem"}} "Access Level"]]
       (map-indexed
        (fn [i acl-entry]
-         [:div {}
-          [input/TextField
-           {:ref (str "acl-key" i)
-            :predicates [(input/valid-email-or-empty "User ID")]
-            :style {:float "left" :width 400 :color "black"
-                    :backgroundColor (when (:read-only? acl-entry)
-                                       (:background-light style/colors))}
-            :disabled (:read-only? acl-entry)
-            :spellCheck false
-            :value (:email acl-entry)
-            :onChange #(swap! state assoc-in [:non-project-owner-acl-vec i :email] (.. % -target -value))}]
+         [:div {:style {:borderTop style/standard-line :padding "0.5rem 0"}}
+          (if (:read-only? acl-entry)
+            [:div {:style {:display "inline-block" :width 400 :fontSize "88%"}}
+             (:email acl-entry)]
+            [input/TextField
+             {:ref (str "acl-key" i)
+              :predicates [(input/valid-email-or-empty "User ID")]
+              :style {:display "inline-block" :width 400 :marginBottom 0}
+              :spellCheck false
+              :value (:email acl-entry)
+              :onChange #(swap! state assoc-in [:non-project-owner-acl-vec i :email] (.. % -target -value))}])
           (style/create-identity-select
            {:ref (str "acl-value" i)
-            :style {:float "right" :width 200 :height 33 :marginLeft "1em"}
+            :style {:display "inline-block" :width 200 :height 33 :marginLeft "1rem" :marginBottom 0}
             :value (:accessLevel acl-entry)
             :onChange #(swap! state assoc-in [:non-project-owner-acl-vec i :accessLevel]
                               (.. % -target -value))}
-           access-levels)
-          (common/clear-both)])
+           access-levels)])
        (:non-project-owner-acl-vec @state))
-      [:div {:style {:marginBottom "0.5em"}}
+      [:div {:style {:margin "0.5rem 0"}}
        [comps/Button {:text "Add new" :icon :add
                       :onClick #(swap! state update-in [:non-project-owner-acl-vec]
                                        conj {:email "" :accessLevel "READER"})}]]
@@ -74,13 +72,15 @@
           (style/create-server-error-message (:load-error @state))
           [comps/Spinner {:text "Loading Permissions..."}])]))
    :persist-acl
-   (fn [{:keys [props state refs this]}]
+   (fn [{:keys [props state refs]}]
      (swap! state dissoc :save-error :validation-error)
      (let [filtered-acl (->> (concat (:project-owner-acl-vec @state) (:non-project-owner-acl-vec @state))
                              (map #(dissoc % :read-only?))
                              (map #(update-in % [:email] clojure.string/trim))
                              (filter #(not (empty? (:email %)))))
-           fails (apply input/validate refs (map #(str "acl-key" %) (range (count (:non-project-owner-acl-vec @state)))))]
+           fails (apply input/validate refs (filter
+                                             #(contains? @refs %)
+                                             (map #(str "acl-key" %) (range (count (:non-project-owner-acl-vec @state))))))]
        (if fails
          (swap! state assoc :validation-error fails)
          (do
