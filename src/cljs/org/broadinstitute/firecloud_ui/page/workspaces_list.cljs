92092f50b168473319f4e1ab0c34cc1d68825c2a
Sarah Walker <skwalker@broadinstitute.org>
2016-11-22 16:26:53 -0500

    comments

--- src/cljs/org/broadinstitute/firecloud_ui/page/workspaces_list.cljs
+++ src/cljs/org/broadinstitute/firecloud_ui/page/workspaces_list.cljs
@@ -162,8 +162,8 @@
                                  (if description (-> description split-lines first)
                                                  [:span {:style {:fontStyle "italic"}}
                                                   "No description provided"])])}
-           {:as-text (fn [date] (common/format-date date))
-            :header "Last Modified Date" :starting-width 300 :marginLeft 14
+           {:as-text common/format-date
+            :header "Last Modified Date" :starting-width 300
             :content-renderer (fn [date]
                                 [:div {:style {:paddingLeft 14}} (common/format-date date) ])}
            {:header "Access Level" :starting-width 118 :resizable? false
