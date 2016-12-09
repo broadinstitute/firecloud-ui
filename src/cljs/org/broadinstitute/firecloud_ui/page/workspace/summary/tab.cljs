885a5d12ae1ad2eb4d307268135dc44e3a7724c7
Brian Reilly <breilly@broadinstitute.org>
2016-12-09 09:05:27 -0500

    GAWB-1174 storage cost ui (#596)
    * Added storage cost estimate for everyone but workspace readers.

--- src/cljs/org/broadinstitute/firecloud_ui/page/workspace/summary/tab.cljs
+++ src/cljs/org/broadinstitute/firecloud_ui/page/workspace/summary/tab.cljs
@@ -151,49 +151,64 @@
                                                                           :on-delete on-delete}])}]))]))
 
 
-(defn- render-main [{:keys [workspace curator? owner? writer? bucket-access? editing? submissions-count library-schema request-refresh workspace-id]}]
+(defn- render-main [{:keys [workspace curator? owner? writer? reader? bucket-access? editing? submissions-count
+                            library-schema request-refresh workspace-id storage-cost]}]
   (let [{:keys [owners]
-         {:keys [createdBy createdDate bucketName description workspace-attributes library-attributes]} :workspace} workspace]
+         {:keys [createdBy createdDate bucketName description workspace-attributes library-attributes]} :workspace} workspace
+        render-detail-box (fn [order title & children]
+                            [:div {:style {:flexBasis "50%" :order order}}
+                             (style/create-section-header title)
+                             children])]
     [:div {:style {:flex "1 1 auto" :overflow "hidden"}}
-     [:div {:style {:flex "1 1 auto" :display "flex"}}
-      [:div {:style {:flex "1 1 50%"}}
-       (style/create-section-header (str "Workspace Owner" (when (> (count owners) 1) "s")))
-       (style/create-paragraph
-         [:div {}
-          (interpose ", " owners)
-          (when owner?
-            [:span {}
-             " ("
-             (style/create-link {:text "Sharing..."
-                                 :onClick #(modal/push-modal
-                                            [AclEditor {:workspace-id workspace-id
-                                                        :request-refresh request-refresh}])})
-             ")"])])
-       (style/create-section-header "Created By")
-       (style/create-paragraph
-         [:div {} createdBy]
-         [:div {} (common/format-date createdDate)])]
-      [:div {:style {:flex "1 1 50%" :paddingLeft 10}}
-       (style/create-section-header "Google Bucket")
-       (style/create-paragraph
-         (case bucket-access?
-           nil [:div {:style {:position "absolute" :marginTop "-1.5em"}}
-                [comps/Spinner {:height "1.5ex"}]]
-           true (style/create-link {:text bucketName
-                                    :href (str moncommon/google-cloud-context bucketName "/")
-                                    :style {:color "-webkit-link" :textDecoration "underline"}
-                                    :title "Click to open the Google Cloud Storage browser for this bucket"
-                                    :target "_blank"})
-           false bucketName))
-       (style/create-section-header "Analysis Submissions")
-       (style/create-paragraph
-         (let [count-all (apply + (vals submissions-count))]
-           [:div {}
-            (str count-all " Submission" (when-not (= 1 count-all) "s"))
-            (when (pos? count-all)
-              [:ul {:style {:marginTop "0"}}
-               (for [[status subs] (sort submissions-count)]
-                 [:li {} (str subs " " status)])])]))]]
+     [:div {:style {:display "flex" :flexWrap "wrap"}}
+      (render-detail-box
+        1
+        (str "Workspace Owner" (when (> (count owners) 1) "s"))
+        (style/create-paragraph
+          [:div {}
+           (interpose ", " owners)
+           (when owner?
+             [:span {}
+              " ("
+              (style/create-link {:text "Sharing..."
+                                  :onClick #(modal/push-modal
+                                              [AclEditor {:workspace-id workspace-id
+                                                          :request-refresh request-refresh}])})
+              ")"])]))
+      (render-detail-box
+        3
+        "Created By"
+        (style/create-paragraph
+          [:div {} createdBy]
+          [:div {} (common/format-date createdDate)]))
+      (render-detail-box
+        2
+        "Google Bucket"
+        (style/create-paragraph
+          (case bucket-access?
+            nil [:div {:style {:position "absolute" :marginTop "-1.5em"}}
+                 [comps/Spinner {:height "1.5ex"}]]
+            true (style/create-link {:text bucketName
+                                     :href (str moncommon/google-cloud-context bucketName "/")
+                                     :style {:color "-webkit-link" :textDecoration "underline"}
+                                     :title "Click to open the Google Cloud Storage browser for this bucket"
+                                     :target "_blank"})
+            false bucketName)
+          (when (not reader?)
+            [:div {}
+             [:div {} (str "Total Estimated Storage Fee per month = " storage-cost)]
+             [:div {:style {:fontSize "80%"}} (str "Note: the billing account associated with " (:namespace workspace-id) " will be charged.")]])))
+      (render-detail-box
+        4
+        "Analysis Submissions"
+        (style/create-paragraph
+          (let [count-all (apply + (vals submissions-count))]
+            [:div {}
+             (str count-all " Submission" (when-not (= 1 count-all) "s"))
+             (when (pos? count-all)
+               [:ul {:style {:marginTop "0"}}
+                (for [[status subs] (sort submissions-count)]
+                  [:li {} (str subs " " status)])])])))]
      (style/create-section-header "Description")
      (style/create-paragraph
        (let [description (not-empty description)]
@@ -215,6 +230,9 @@
                                                  :workspace-id workspace-id
                                                  :request-refresh request-refresh}]]))
 
+(defn- reader? [workspace]
+  (= "READER" (:accessLevel workspace)))
+
 (react/defc Summary
   {:get-initial-state
    (fn []
@@ -233,7 +251,8 @@
          :else
          (let [owner? (or (= "PROJECT_OWNER" (:accessLevel workspace)) (= "OWNER" (:accessLevel workspace)))
                writer? (or owner? (= "WRITER" (:accessLevel workspace)))
-               derived {:owner? owner? :writer? writer? :request-refresh #(react/call :refresh this)}]
+               derived {:owner? owner? :writer? writer? :reader? (reader? (:workspace props))
+                        :request-refresh #(react/call :refresh this)}]
            [:div {:style {:margin "45px 25px" :display "flex"}}
             (render-sidebar state refs this
                             (merge (select-keys props [:workspace :workspace-id :on-clone :on-delete])
@@ -241,8 +260,8 @@
                                    derived))
             (render-main (merge (select-keys props [:workspace :workspace-id :bucket-access?])
                                 (select-keys @state [:editing?])
-                                (select-keys server-response [:submissions-count :library-schema :curator?])
-                                (select-keys derived [:owner? :writer? :request-refresh])))
+                                (select-keys server-response [:submissions-count :library-schema :curator? :storage-cost])
+                                derived))
             (when (:updating-attrs? @state)
               [comps/Blocker {:banner "Updating Attributes..."}])
             (when (contains? @state :locking?)
@@ -288,11 +307,11 @@
                      (swap! state update-in [:server-response]
                             assoc :server-error status-text)))})
      (endpoints/get-billing-projects
-      (fn [err-text projects]
-        (if err-text
-          (swap! state update-in [:server-response] assoc :server-error err-text)
-          (swap! state update-in [:server-response]
-                 assoc :billing-projects (map #(% "projectName") projects)))))
+       (fn [err-text projects]
+         (if err-text
+           (swap! state update-in [:server-response] assoc :server-error err-text)
+           (swap! state update-in [:server-response]
+                  assoc :billing-projects (map #(% "projectName") projects)))))
      (endpoints/get-library-attributes
        (fn [{:keys [success? get-parsed-response]}]
          (if success?
@@ -307,4 +326,11 @@
         :on-done (fn [{:keys [success? get-parsed-response]}]
                    (if success?
                      (swap! state update-in [:server-response] assoc :curator? (get (get-parsed-response false) "curator"))
-                     (swap! state update-in [:server-response] assoc :server-error "Unable to determine curator status")))}))})
+                     (swap! state update-in [:server-response] assoc :server-error "Unable to determine curator status")))})
+     (when (not (reader? (:workspace props)))
+       (endpoints/call-ajax-orch
+         {:endpoint (endpoints/storage-cost-estimate (:workspace-id props))
+          :on-done (fn [{:keys [success? status-text get-parsed-response]}]
+                     (if success?
+                       (swap! state update-in [:server-response] assoc :storage-cost (get (get-parsed-response false) "estimate"))
+                       (swap! state update-in [:server-response] assoc :storage-cost status-text)))})))})
