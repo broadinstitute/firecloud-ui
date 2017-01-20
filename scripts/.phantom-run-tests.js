c5b1383b0675b1006c1d76c9bf49408ee8055339
David Mohs <dgit@mohsinc.com>
2017-01-20 10:51:49 -0500

    Headless unit tests script - also added Travis CI configuration

---
+++ scripts/.phantom-run-tests.js
@@ -0,0 +1,41 @@
+var page = require('webpage').create();
+
+// Must match variable of the same name below.
+var logToken = 'e54b0a2b-0934-400f-bf3a-68d01dcde2df';
+var someTestsFailed = false;
+
+function installConsoleListener() {
+  page.onConsoleMessage = function(msg) {
+    if (msg.substring(0, logToken.length) == logToken) {
+      var commandString = msg.substring(logToken.length);
+      if(commandString == 'fail') {
+        someTestsFailed = true;
+      }
+    } else {
+      console.log(msg);
+    }
+  };
+}
+
+page.open('http://server/', function(status) {
+  installConsoleListener();
+  page.evaluate(function() {
+    var logToken = 'e54b0a2b-0934-400f-bf3a-68d01dcde2df';
+    console.group = function(group) { console.log(group + " {"); };
+    console.groupEnd = function() { console.log("}"); };
+    broadfcuitest.utils.report_test_status = function(isSuccess, failureCount, errorCount) {
+      if(isSuccess) {
+        console.log('All tests passed.');
+      } else {
+        console.log('Tests failed: ' + failureCount + ' failures and ' + errorCount + ' errors');
+        console.log(logToken + 'fail');
+      }
+    }
+    broadfcuitest.testrunner.run_all_tests();
+  });
+  if (someTestsFailed) {
+    phantom.exit(1);
+  } else {
+    phantom.exit();
+  }
+});
---
+++ scripts/test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -euxo pipefail
+IFS=$'\n\t'
+
+clean_up () {
+  docker-compose -p fcuitests -f scripts/.tests-compose.yaml down -v
+}
+
+compose_exec() {
+  docker-compose -p fcuitests -f scripts/.tests-compose.yaml exec $@
+}
+
+trap clean_up EXIT HUP INT QUIT PIPE TERM 0 20
+
+docker volume create --name=jar-cache
+docker volume create --name=node-modules
+
+docker-compose -p fcuitests -f scripts/.tests-compose.yaml up -d
+
+docker cp project.clj fcuitests_clojure_1:/w
+docker cp src fcuitests_clojure_1:/w
+docker cp webpack.config.js fcuitests_clojure_1:/w
+
+compose_exec clojure lein with-profile deploy npm install
+compose_exec clojure ./node_modules/webpack/bin/webpack.js -p
+compose_exec clojure lein cljsbuild once
+compose_exec clojure lein resource
+
+docker cp scripts/.phantom-run-tests.js fcuitests_clojure_1:/w/run-tests.js
+compose_exec phantomjs phantomjs run-tests.js
+
+clean_up
