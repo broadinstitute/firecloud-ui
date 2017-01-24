5d58d0aa43e95c9c5942f8a555bca51201847277
David Mohs <dgit@mohsinc.com>
2017-01-24 10:24:07 -0500

    Remove old build scripts

--- script/build-release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euox pipefail
-IFS=$'\n\t'
-
-./script/clean-build-dir.sh
-exec docker run --rm -it -w /work -v "$PWD":/work -v "$HOME"/.m2:/root/.m2 \
-  clojure \
-  rlfe lein with-profile deploy do resource, cljsbuild once
