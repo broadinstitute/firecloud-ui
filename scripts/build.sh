c88101c4fe5ba71bf3fc3aef9a5976624e65cc8d
Isaac Zarsky <zarsky@broadinstitute.org>
2017-02-02 15:56:11 -0500

    fix for jenkins project location

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 docker run --rm \
   -w /work \
-  -v "$PWD/$PROJECT":/work \
+  -v "$PWD":/work \
   -v maven-cache:/root/.m2 \
   broadinstitute/clojure-node \
   lein with-profile deploy do npm install, clean, resource, cljsbuild once
