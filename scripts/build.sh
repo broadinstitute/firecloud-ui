be9cac43a43e03fcb7efa5713c236ece2c312113
Isaac Zarsky <zarsky@broadinstitute.org>
2017-02-02 15:58:49 -0500

    jenkins build fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,5 +6,6 @@ docker run --rm \
   -w /work \
   -v "$PWD":/work \
   -v maven-cache:/root/.m2 \
+  -e npm_config_unsafe_perm="true" \
   broadinstitute/clojure-node \
   lein with-profile deploy do npm install, clean, resource, cljsbuild once
