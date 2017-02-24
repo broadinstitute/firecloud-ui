ac40e7213055ab291e9f6984bf24e256905d1c0a
Andrea Haessly <ahaessly@broadinstitute.org>
2017-02-24 11:45:10 -0500

    merge

--- src/cljs/main/broadfcui/page/library/library_page.cljs
+++ src/cljs/main/broadfcui/page/library/library_page.cljs
@@ -256,7 +256,9 @@
             aggregate-fields)])))})
 
 (def ^:private PERSISTENCE-KEY "library-page")
-(def ^:private VERSION 3)
+(def ^:private VERSION 2)
+(def ^:private TABLE_VERSION 1)
+
 
 (react/defc Page
   {:update-filter
