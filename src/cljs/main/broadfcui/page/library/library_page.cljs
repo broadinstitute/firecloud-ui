24a97a04ddcdc99735faa74f4fc3fae91c27a334
Matt Putnam <putnam@broadinstitute.org>
2017-03-07 17:52:03 -0500

    clean up empty facets

--- src/cljs/main/broadfcui/page/library/library_page.cljs
+++ src/cljs/main/broadfcui/page/library/library_page.cljs
@@ -272,7 +272,9 @@
 (react/defc Page
   {:update-filter
    (fn [{:keys [state after-update refs]} facet-name facet-list]
-     (swap! state assoc-in [:facet-filters facet-name] facet-list)
+     (if (empty? facet-list)
+       (swap! state update :facet-filters dissoc facet-name)
+       (swap! state assoc-in [:facet-filters facet-name] facet-list))
      (after-update #((@refs "dataset-table") :execute-search)))
    :set-expanded-aggregate
    (fn [{:keys [state]} facet-name expanded?]
