a027cf79576117d3d787491e2b0c0bac7469898e
Jackie Roberti <jacqueline.roberti93@gmail.com>
2017-03-31 13:04:42 -0400

    expand build script and add documentation

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,57 @@
 #!/bin/bash
+# Single source of truth for building Firecloud-Ui.
+# @ Jackie Roberti and Isaac Zarsky
+#
+# Provide command line options to do one or several things:
+#   compile: compile clojure proj into /target
+#   -d | --docker : provide arg either "build" or "push", to build and push docker image
+# Jenkins build job should run with all options, for example,
+#   ./docker/build.sh compile -d push
+
+
 IFS=$'\n\t'
 set -euxo pipefail
 
-docker run --rm \
-  -w /work \
-  -v "$PWD":/work \
-  -v maven-cache:/root/.m2 \
-  -e npm_config_unsafe_perm="true" \
-  broadinstitute/clojure-node \
-  lein with-profile deploy do clean, resource, npm install, cljsbuild once
+function clj_build() {
+    docker run --rm \
+      -w /work \
+      -v "$PWD":/work \
+      -v maven-cache:/root/.m2 \
+      -e npm_config_unsafe_perm="true" \
+      broadinstitute/clojure-node \
+      lein with-profile deploy do clean, resource, npm install, cljsbuild once
+
+}
+
+function docker_cmd()
+{
+    if [ $DOCKER_CMD = "build" ] || [ $DOCKER_CMD = "push" ]; then
+        echo "building docker image..."
+        GIT_SHA=$(git rev-parse ${GIT_BRANCH})
+        echo GIT_SHA=$GIT_SHA > env.properties  # for jenkins jobs
+        docker build -t $REPO:${GIT_SHA:0:12} .
+
+        if [ $DOCKER_CMD = "push" ]; then
+            echo "pushing docker image..."
+            docker push $REPO:${GIT_SHA:0:12}
+        fi
+    else
+        echo "Not a valid docker option!  Choose either build or push (which includes build)"
+    fi
+}
+
+# parse command line options
+DOCKER_CMD=
+GIT_BRANCH=${GIT_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}  # default to current branch
+REPO=${REPO:-broadinstitute/$PROJECT}  # default to rawls docker repo
+while [ "$1" != "" ]; do
+    case $1 in
+        compile) clj_build ;;
+        -d | --docker) shift
+                       echo $1
+                       DOCKER_CMD=$1
+                       docker_cmd
+                       ;;
+    esac
+    shift
+done
