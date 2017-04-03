1a4cd63a7dc05606e52a616992d3adf85037e162
Isaac Zarsky <zarsky@broadinstitute.org>
2017-04-03 17:09:06 -0400

    use kebab case

--- src/cljs/main/broadfcui/page/billing/create_project.cljs
+++ src/cljs/main/broadfcui/page/billing/create_project.cljs
@@ -68,7 +68,7 @@
                            [:tr {:style {:borderTop style/standard-line}}
                             [:td {:style {:borderTop style/standard-line}}
                              [:input {:type "radio" :value (account "accountName")
-                                      :name "billingAccountSelect"
+                                      :name "billing-account-select"
                                       :disabled (not (account "firecloudHasAccess"))
                                       :id (account "accountName")
                                       :onChange (fn [event]
