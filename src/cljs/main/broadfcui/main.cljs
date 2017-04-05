6a61156c3c5951a096760c29f15e262e026b339f
David Mohs <dgit@mohsinc.com>
2017-04-05 14:04:04 -0400

    Extract header components into header namespace

--- src/cljs/main/broadfcui/main.cljs
+++ src/cljs/main/broadfcui/main.cljs
@@ -10,8 +10,8 @@
    [broadfcui.config.loader :as config-loader]
    [broadfcui.endpoints :as endpoints]
    [broadfcui.footer :as footer]
+   [broadfcui.header :as header]
    [broadfcui.nav :as nav]
-   [broadfcui.nih-link-warning :as nih-link-warning]
    [broadfcui.page.billing.billing-management :as billing-management]
    [broadfcui.page.library.library-page :as library-page]
    [broadfcui.page.method-repo.method-repo-page :as method-repo]
@@ -66,99 +66,6 @@
    {:key :policy
     :render #(react/create-element Policy %)}])
 
-(react/defc TopNavBarLink
-  {:render
-   (fn [{:keys [props state]}]
-     [:a {:href (:href props)
-          :style {:padding "1em" :textDecoration "none"
-                  :fontWeight (when (:selected props) "bold")
-                  :color (if (:hovering? @state) (:link-active style/colors) "black")}
-          :onMouseOver #(swap! state assoc :hovering? true)
-          :onMouseOut #(swap! state assoc :hovering? false)}
-      (:name props)])})
-
-(react/defc TopNavBar
-  {:render
-   (fn [{:keys [props]}]
-     [:div {}
-      (style/render-text-logo)
-      [:div {:style {:display "inline-block" :paddingLeft "1em" :fontSize 18 :height 38 :verticalAlign "baseline"}}
-       (map (fn [item] [TopNavBarLink {:name (:name item) :href (:href item)
-                                       :selected (= (:selected-item props) (:key item))}])
-            (filter (comp #{:library :workspaces :methods} :key) routes))
-       (when (:show-nih-link-warning? props)
-         [nih-link-warning/NihLinkWarning])]])})
-
-(react/defc GlobalSubmissionStatus
-  {:render
-   (fn [{:keys [state]}]
-     (let [{:keys [status-error status-code status-counts]} @state
-           {:keys [queued active queue-position]} status-counts]
-       (when-not (= status-code 401) ; to avoid displaying "Workflows: Unauthorized"
-         [:div {}
-          (str "Workflows: "
-               (cond status-error status-error
-                     status-counts (str queued " Queued; " active " Active; " queue-position " ahead of yours")
-                     :else "loading..."))])))
-   :component-did-mount
-   (fn [{:keys [this locals]}]
-     ;; Call once for initial load
-     (react/call :load-data this)
-     ;; Add a long-polling call for continuous updates
-     (swap! locals assoc :interval-id (js/setInterval #(react/call :load-data this) (config/submission-status-refresh))))
-   :component-will-unmount
-   (fn [{:keys [locals]}]
-     (js/clearInterval (:interval-id @locals)))
-   :load-data
-   (fn [{:keys [state]}]
-     (endpoints/call-ajax-orch
-       {:endpoint (endpoints/submissions-queue-status)
-        :on-done (fn [{:keys [success? status-text status-code get-parsed-response]}]
-                   (if success?
-                     (swap! state assoc :status-error nil :status-code nil :status-counts (common/queue-status-counts (get-parsed-response false)))
-                     (swap! state assoc :status-error status-text :status-code status-code :status-counts nil)))}))})
-
-(react/defc AccountDropdown
-  {:render
-   (fn [{:keys [props state]}]
-     [:div {:style {:float "right" :position "relative" :marginBottom "0.4rem"}}
-      (when (:show-dropdown? @state)
-        [:div {:style {:position "fixed" :top 0 :left 0 :right 0 :bottom 0}
-               :onClick #(swap! state assoc :show-dropdown? false)}])
-      [:a {:href "javascript:;"
-           :onClick #(swap! state assoc :show-dropdown? true)
-           :style {:display "block"
-                   :borderRadius 2
-                   :backgroundColor (:background-light style/colors)
-                   :color "#000" :textDecoration "none"
-                   :padding "0.5rem" :border style/standard-line
-                   :minWidth 100}}
-       [:div {}
-        (-> (:auth2 props) (.-currentUser) (.get) (.getBasicProfile) (.getEmail))
-        [:div {:style {:display "inline-block" :marginLeft "1em" :fontSize 8}} "▼"]]]
-      (when (:show-dropdown? @state)
-        (let [DropdownItem
-              (react/create-class
-               {:render
-                (fn [{:keys [props state]}]
-                  [:a {:style {:display "block"
-                               :color "#000" :textDecoration "none" :fontSize 14
-                               :padding "0.5rem 1.3rem 0.5rem 0.5rem"
-                               :backgroundColor (when (:hovering? @state) "#e8f5ff")}
-                       :href (:href props)
-                       :onMouseOver #(swap! state assoc :hovering? true)
-                       :onMouseOut #(swap! state assoc :hovering? false)
-                       :onClick (:dismiss props)}
-                   (:text props)])})]
-          [:div {:style {:boxShadow "0px 3px 6px 0px rgba(0, 0, 0, 0.15)"
-                         :backgroundColor "#fff"
-                         :position "absolute" :width "100%"
-                         :border (str "1px solid " (:line-default style/colors))}}
-           [DropdownItem {:href "#profile" :text "Profile" :dismiss #(swap! state assoc :show-dropdown? false)}]
-           [DropdownItem {:href "#billing" :text "Billing" :dismiss #(swap! state assoc :show-dropdown? false)}]
-           [DropdownItem {:href "javascript:;" :text "Sign Out"
-                          :dismiss #(.signOut (:auth2 props))}]]))])})
-
 (react/defc LoggedIn
   {:render
    (fn [{:keys [this props state]}]
@@ -170,14 +77,15 @@
        [:div {}
         [:div {:style {:width "100%" :borderBottom (str "1px solid " (:line-default style/colors))}}
          [:div {:style {:float "right" :fontSize "70%" :margin "0 0 0.5em 0"}}
-          [AccountDropdown {:auth2 (:auth2 props)}]
+          [header/AccountDropdown {:auth2 (:auth2 props)}]
           (common/question-icon-link "FireCloud User Guide" (config/user-guide-url) {:display "block" :float "right"})
           (common/clear-both)
           (when (= :registered (:registration-status @state))
-            [GlobalSubmissionStatus])]
+            [header/GlobalSubmissionStatus])]
          (when (= :registered (:registration-status @state))
-           [TopNavBar {:selected-item page
-                       :show-nih-link-warning? (not (contains? #{:status :profile} page))}])
+           [header/TopNavBar {:routes routes
+                              :selected-item page
+                              :show-nih-link-warning? (not (contains? #{:status :profile} page))}])
          (common/clear-both)]
         (case (:registration-status @state)
           nil [:div {:style {:margin "2em 0" :textAlign "center"}}
