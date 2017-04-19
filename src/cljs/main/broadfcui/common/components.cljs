6a607853ed5cfa9b4c3442b87e22f08ede11b8ce
Matt Putnam <putnam@broadinstitute.org>
2017-04-19 12:23:07 -0400

    post-rebase fix

--- src/cljs/main/broadfcui/common/components.cljs
+++ src/cljs/main/broadfcui/common/components.cljs
@@ -774,32 +774,3 @@
             :onClick #(swap! state assoc :collapsed? (not (:collapsed? @state)))})
           body]
          body)))})
-
-(defn filter-area [attributes & sections]
-  [:div (utils/deep-merge {:style {:fontSize "85%" :padding "16px 12px"}}
-                          attributes)
-   (interpose [:hr] sections)])
-
-(defn filter-section [{:keys [title on-clear content]}]
-  [:div {:style {:paddingBottom "0.9rem"}}
-   [:span {:style {:fontWeight "bold"}} title]
-   (when on-clear
-     [:div {:style {:fontSize "80%" :float "right"}}
-      (style/create-link {:text "Clear" :onClick on-clear})])
-   [:div {:style {:paddingTop "1em"}}
-    content]])
-
-(defn filter-checkboxes [{:keys [items checked-items on-change]}]
-  (map
-   (fn [{:keys [item render hit-count]}]
-     (let [rendered (render item)]
-       [:div {:style {:paddingTop 5}}
-        [:label {:style {:display "inline-block" :width "calc(100% - 30px)"
-                         :textOverflow "ellipsis" :overflow "hidden" :whiteSpace "nowrap"}
-                 :title rendered}
-         [:input {:type "checkbox"
-                  :checked (contains? checked-items item)
-                  :onChange #(on-change item (.. % -target -checked))}]
-         [:span {:style {:marginLeft "0.25rem"}} rendered]]
-        (some-> hit-count style/render-count)]))
-   items))
