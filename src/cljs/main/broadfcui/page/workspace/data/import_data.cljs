a6fdd6ecc861c9de28825596651e366bca5fbc1b
Ursa Stutsman <ursas@broadinstitute.org>
2017-05-12 14:45:45 -0400

    More elegant solution - ala mohs

--- src/cljs/main/broadfcui/page/workspace/data/import_data.cljs
+++ src/cljs/main/broadfcui/page/workspace/data/import_data.cljs
@@ -15,15 +15,28 @@
 (def ^:private preview-limit 4096)
 
 (react/defc Page
-  {:render
+  {:get-initial-state
+   (fn []
+     {:file-input-key (gensym "file-input-")})
+  :render
    (fn [{:keys [state refs this]}]
      [:div {:style {:textAlign "center"}}
       (when (:loading? @state)
         [comps/Blocker {:banner "Uploading file..."}])
-      [:div {:ref "entity-input"}]
+       [:input {:key (:file-input-key @state)
+                :type "file" :name "entities" :ref "entities"
+                :style {:display "none"}
+                :onChange (fn [e]
+                            (let [file (-> e .-target .-files (aget 0))
+                                  reader (js/FileReader.)]
+                              (when file
+                                (swap! state dissoc :upload-result)
+                                (set! (.-onload reader)
+                                      #(swap! state assoc :file file :file-contents (.-result reader) :file-input-key (gensym "file-input-")))
+                                (.readAsText reader (.slice file 0 preview-limit)))))}]
       common/PHI-warning
       [comps/Button {:text (if (:upload-result @state) "Choose another file..." "Choose file...")
-                     :onClick #(-> (@refs "entity-input") .-firstChild .click)}]
+                     :onClick #(-> (@refs "entities") .click)}]
       (when (:file-contents @state)
         [:div {:style {:margin "0.5em 2em" :padding "0.5em" :border style/standard-line}}
          (str "Previewing '" (-> (:file @state) .-name) "':")
@@ -43,9 +56,6 @@
            [:span {:style {:marginLeft "1em"}} "Success!"])
           [:div {:style {:paddingTop "1em"}}
            [comps/ErrorViewer {:error (:error result)}]]))])
-   :component-did-mount
-   (fn [{:keys [this]}]
-     (this :-create-file-input))
    :do-upload
    (fn [{:keys [props state]}]
      (swap! state assoc :loading? true)
@@ -62,21 +72,4 @@
                     (do
                       (swap! state assoc :upload-result {:success? true})
                       ((:on-data-imported props)))
-                    (swap! state assoc :upload-result {:success? false :error (get-parsed-response false)})))}))
-   ;; We put this here to trick html into using the onChange method EVERY time regardless of filename
-   :-create-file-input
-   (fn [{:keys [state refs this after-update]}]
-     (react/unmount-component-at-node (@refs "entity-input"))
-     (react/render
-      (react/create-element
-       [:input {:type "file" :name "entities"
-                :style {:display "none"}
-                :onChange (fn [e]
-                            (let [file (-> e .-target .-files (aget 0))
-                                  reader (js/FileReader.)]
-                              (when file
-                                (set! (.-onload reader)
-                                      #(swap! state assoc :file file :file-contents (.-result reader)))
-                                (.readAsText reader (.slice file 0 preview-limit)))
-                              (after-update #(this :-create-file-input))))}])
-      (@refs "entity-input")))})
+                    (swap! state assoc :upload-result {:success? false :error (get-parsed-response false)})))}))})
