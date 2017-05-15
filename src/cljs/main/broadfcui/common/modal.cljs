ba2239e981910b566cd76e2e7ec06544e4b919c8
Isaac Zarsky <zarsky@broadinstitute.org>
2017-05-15 16:26:33 -0400

    Iz gawb 2040 quick fixes for first merge (#791)
    * make wf abort button red
    
    * missed refactor when moving ServiceAlertContainer

--- src/cljs/main/broadfcui/common/modal.cljs
+++ src/cljs/main/broadfcui/common/modal.cljs
@@ -2,6 +2,7 @@
   (:require
     [dmohs.react :as react]
     [broadfcui.common :as common]
+    [broadfcui.common.notifications :as notifications]
     [broadfcui.utils :as utils]))
 
 
@@ -43,7 +44,7 @@
                [:div {:style {:position "fixed" :zIndex 513 :overflow "auto"
                               :top 0 :bottom 0 :left 0 :right 0
                               :backgroundColor "rgba(110,110,110,0.4)"}}
-                [common/ServiceAlertContainer]
+                [notifications/ServiceAlertContainer]
                 [:div {:style {:padding "2rem 0"
                                :display "flex" :justifyContent "center" :alignItems "flex-start"}}
                  [:div {:style {:backgroundColor "white" :maxWidth "95%" :minWidth 500}}
