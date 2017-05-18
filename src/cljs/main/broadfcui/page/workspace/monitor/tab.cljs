0a2b78da5a9b6129b4ef13efb81ac218d25eba72
Matt Putnam <putnam@broadinstitute.org>
2017-05-18 13:27:23 -0400

    Add default case for monitor tab table icons

--- src/cljs/main/broadfcui/page/workspace/monitor/tab.cljs
+++ src/cljs/main/broadfcui/page/workspace/monitor/tab.cljs
@@ -38,7 +38,8 @@
                  [:div {:style {:height table-style/table-icon-size}}
                   (case (:status submission)
                     "Done" (moncommon/icon-for-sub-status (:workflowStatuses submission))
-                    "Aborted" moncommon/failure-icon)
+                    "Aborted" moncommon/failure-icon
+                    nil)
                   (:status submission)])}
       {:header "Method Configuration" :initial-width 300
        :column-data (juxt :methodConfigurationNamespace :methodConfigurationName)
