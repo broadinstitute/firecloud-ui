9cc5c4e69a9527b56d5bd8901fc5ee50697fd7ce
Robert Title <rtitle@broadinstitute.org>
2017-06-13 14:03:19 -0400

    [GAWB-2070] Javascript error and not able to navigate between data ta… (#829)
    * [GAWB-2070] Javascript error and not able to navigate between data tabs after importing attribute file
    
    * Fancy approach wasn't working
    
    * Found a nicer way to do it with select-keys
    
    * Code review feedback

--- src/cljs/main/broadfcui/page/workspace/data/utils.cljs
+++ src/cljs/main/broadfcui/page/workspace/data/utils.cljs
@@ -32,3 +32,20 @@
                                  v))]
                          (update-parent-state :selected-attr-list (utils/map-values attr-value-mapper attrs) :loading-attributes false)))
                      (update-parent-state :server-error (get-parsed-response false) :loading-attributes false))))})))
+
+(defn get-column-defaults [json-column-defaults]
+  (let [parsed
+        (try
+          (utils/parse-json-string json-column-defaults)
+          (catch js/Object e
+            (utils/jslog e) nil))
+        validate-schema
+        (fn [[k v]]
+          (if (some #(= k %) common/root-entity-types)
+            (cond
+              (map? v) {k (select-keys v ["shown" "hidden"])}
+              ;; Default to shown if the shown/hidden key is not present
+              (vector? v) {k {"shown" v}}
+              :else {})
+            {}))]
+    (into {} (mapcat validate-schema parsed))))
