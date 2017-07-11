b8b691fdd51f4371da06e8a6146cd97198024197
Jackie Roberti <jacqueline.roberti93@gmail.com>
2017-07-11 18:32:11 -0400

    write out logs from syslog driver for selenium chrome nodes

--- automation/docker/run-tests.sh
+++ automation/docker/run-tests.sh
@@ -86,9 +86,6 @@ docker run -e DOCKERHOST=$DOCKERHOST \
     --link docker_hub_1:hub --name ${TEST_CONTAINER} -w /app \
     ${TEST_CONTAINER}:latest
 
-mkdir -p logs
-docker logs docker_hub_1 > logs/hublog.txt
-docker logs docker_chrome_1 > logs/nodelog.txt
 
 # Grab exit code of tests
 TEST_EXIT_CODE=$?
