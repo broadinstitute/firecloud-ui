f36676c873acfcf330970cdb633d265aca97ad22
Jackie Roberti <jacqueline.roberti93@gmail.com>
2017-07-18 14:45:42 -0400

    fix documentation and render local script to get rid of authDomain var

--- automation/render-local-env.sh
+++ automation/render-local-env.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 WORKING_DIR=${1-$(pwd)}
-HOST_NAME=$2
-VAULT_TOKEN=$3
+VAULT_TOKEN=$2
 
 # render ctmpls
 docker pull broadinstitute/dsde-toolbox:dev
-docker run --rm -e VAULT_TOKEN=${VAULT_TOKEN} -e HOST_NAME=${HOST_NAME} \
+docker run --rm -e VAULT_TOKEN=${VAULT_TOKEN} \
     -e ENVIRONMENT=${ENV} -e ROOT_DIR=${WORKING_DIR} -v ${WORKING_DIR}:/working \
     -e OUT_PATH=/working/src/test/resources -e INPUT_PATH=/working/docker \
     broadinstitute/dsde-toolbox:dev render-templates.sh
