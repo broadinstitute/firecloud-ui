ef97c506c8a44a17b3a37b164571e4516e12f00c
Matt Putnam <putnam@broadinstitute.org>
2017-07-19 14:08:41 -0400

    post-rebase bugfix and cleanup

---
+++ src/cljs/main/broadfcui/page/workspace/summary/synchronize.cljs
@@ -0,0 +1,9 @@
+(ns broadfcui.page.workspace.summary.synchronize
+  (:require
+    [dmohs.react :as react]
+    [broadfcui.utils :as utils]
+    ))
+
+
+(defn handle-sync [parsed-perms-report can-share?]
+  (let [methods (:referencedMethods parsed-perms-report)]))
