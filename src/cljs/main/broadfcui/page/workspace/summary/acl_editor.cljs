dac2615a25a3972cf988e382b397eb5a5a660ef8
Matt Putnam <putnam@broadinstitute.org>
2017-07-26 13:11:54 -0400

    post-rebase fixup

--- src/cljs/main/broadfcui/page/workspace/summary/acl_editor.cljs
+++ src/cljs/main/broadfcui/page/workspace/summary/acl_editor.cljs
@@ -2,7 +2,7 @@
   (:require
    [dmohs.react :as react]
    [clojure.set :as set]
-    [broadfcui.common :as common]
+   [broadfcui.common :as common]
    [broadfcui.common.components :as comps]
    [broadfcui.common.input :as input]
    [broadfcui.common.modal :as modal]
@@ -57,102 +57,100 @@
         (swap! state assoc :user-groups groups))))
    :-render-acl-content
    (fn [{:keys [props state this]}]
-     (let [{:keys [workspace-id user-access-level ]} props]
-  [comps/OKCancelForm
-   {:header
-    (str "Permissions for " (:namespace workspace-id) "/" (:name workspace-id))
-    :content
-    (react/create-element
-     [:div {}
-      (when (:saving? @state)
-        [comps/Blocker {:banner "Updating..."}])
-      [:div {:style {:padding "0.5rem 0" :fontSize "90%"}} "Billing Project Owner(s)"]
-      (map
-       (fn [acl-entry]
-         [:div {:style {:padding "0.5rem 0" :fontSize "90%" :borderTop style/standard-line}}
-          (:email acl-entry)])
-       (:project-owner-acl-vec @state))
-      [:div {:style {:padding "0.5rem 0" :fontSize "90%" :marginTop "0.5rem"}}
-       [:div {:style {:display "inline-block" :width 400}} "User ID"]
-       [:div {:style {:display "inline-block" :width 200 :marginLeft "1rem"}} "Access Level"]
-       (when (common/access-greater-than-equal-to? user-access-level "OWNER")
-         [:div {:style {:display "inline-block" :width 80 :marginLeft "1rem"}} "Can Share"])]
-      [:datalist {:id "groups-datalist"}
-       (when-let [groups (:user-groups @state)]
-         (map (fn [group]
-                [:option {:value (:groupEmail group)}])
-              groups))]
-      (map-indexed
-       (fn [i acl-entry]
-         [:div {:style {:borderTop style/standard-line :padding "0.5rem 0"}}
-          (if (:read-only? acl-entry)
-            [:div {:style {:display "inline-block" :width 400 :fontSize "88%"}}
-             (:email acl-entry)]
-            [input/TextField
-             {:ref (str "acl-key" i) :autoFocus true
-              :predicates [(input/valid-email-or-empty "User ID")]
-              :style {:display "inline-block" :width 400 :marginBottom 0}
-              :spellCheck false
-              :value (:email acl-entry)
-              :list "groups-datalist"
-              :onChange #(swap! state assoc-in [:non-project-owner-acl-vec i :email] (.. % -target -value))}])
-          (let [available-access-levels (filter #(common/access-greater-than-equal-to? user-access-level %) access-levels)
-                disabled? (or (common/access-greater-than? (:accessLevel acl-entry) user-access-level)
-                              (= (:email acl-entry) (utils/get-user-email)))]
-            (style/create-identity-select
-             {:ref (str "acl-value" i)
-              :style {:display "inline-block" :width 200 :height 33 :marginLeft "1rem" :marginBottom 0}
-              :disabled disabled?
-              :value (:accessLevel acl-entry)
-              :onChange #(swap! state assoc-in [:non-project-owner-acl-vec i :accessLevel] (.. % -target -value))}
-             (if disabled? access-levels available-access-levels)))
-          (if (common/access-greater-than-equal-to? user-access-level "OWNER")
-            [:label {:style {:marginLeft "1rem" :cursor "pointer" :verticalAlign "middle" :display "inline-block" :width 80 :textAlign "center"}}
-             [:input {:type "checkbox"
-                      :style {:verticalAlign "middle" :float "none"}
-                      :onChange #(swap! state assoc-in [:non-project-owner-acl-vec i :canShare] (.. % -target -checked))
-                      :disabled (common/access-greater-than-equal-to? (:accessLevel acl-entry) "OWNER")
-                      :checked (or (:canShare acl-entry) (common/access-equal-to? (:accessLevel acl-entry) "OWNER"))}]])
-          (when (:pending? acl-entry)
-            [:span {:style {:fontStyle "italic" :color (:text-light style/colors) :marginLeft "1rem"}}
-             "Pending..."])])
-       (:non-project-owner-acl-vec @state))
-      [:div {:style {:margin "0.5rem 0"}}
-       [comps/Button {:text "Add new" :icon :add-new
-                      :data-test-id (config/when-debug "add-new-acl-button")
+     (let [{:keys [workspace-id user-access-level]} props]
+       [comps/OKCancelForm
+        {:header
+         (str "Permissions for " (:namespace workspace-id) "/" (:name workspace-id))
+         :content
+         (react/create-element
+          [:div {}
+           (when (:saving? @state)
+             [comps/Blocker {:banner "Updating..."}])
+           [:div {:style {:padding "0.5rem 0" :fontSize "90%"}} "Billing Project Owner(s)"]
+           (map (fn [acl-entry]
+                  [:div {:style {:padding "0.5rem 0" :fontSize "90%" :borderTop style/standard-line}}
+                   (:email acl-entry)])
+                (:project-owner-acl-vec @state))
+           [:div {:style {:padding "0.5rem 0" :fontSize "90%" :marginTop "0.5rem"}}
+            [:div {:style {:display "inline-block" :width 400}} "User ID"]
+            [:div {:style {:display "inline-block" :width 200 :marginLeft "1rem"}} "Access Level"]
+            (when (common/access-greater-than-equal-to? user-access-level "OWNER")
+              [:div {:style {:display "inline-block" :width 80 :marginLeft "1rem"}} "Can Share"])]
+           [:datalist {:id "groups-datalist"}
+            (when-let [groups (:user-groups @state)]
+              (map (fn [group]
+                     [:option {:value (:groupEmail group)}])
+                   groups))]
+           (map-indexed
+            (fn [i acl-entry]
+              [:div {:style {:borderTop style/standard-line :padding "0.5rem 0"}}
+               (if (:read-only? acl-entry)
+                 [:div {:style {:display "inline-block" :width 400 :fontSize "88%"}}
+                  (:email acl-entry)]
+                 [input/TextField
+                  {:ref (str "acl-key" i) :autoFocus true
+                   :predicates [(input/valid-email-or-empty "User ID")]
+                   :style {:display "inline-block" :width 400 :marginBottom 0}
+                   :spellCheck false
+                   :value (:email acl-entry)
+                   :list "groups-datalist"
+                   :onChange #(swap! state assoc-in [:non-project-owner-acl-vec i :email] (.. % -target -value))}])
+               (let [available-access-levels (filter #(common/access-greater-than-equal-to? user-access-level %) access-levels)
+                     disabled? (or (common/access-greater-than? (:accessLevel acl-entry) user-access-level)
+                                   (= (:email acl-entry) (utils/get-user-email)))]
+                 (style/create-identity-select
+                  {:ref (str "acl-value" i)
+                   :style {:display "inline-block" :width 200 :height 33 :marginLeft "1rem" :marginBottom 0}
+                   :disabled disabled?
+                   :value (:accessLevel acl-entry)
+                   :onChange #(swap! state assoc-in [:non-project-owner-acl-vec i :accessLevel] (.. % -target -value))}
+                  (if disabled? access-levels available-access-levels)))
+               (if (common/access-greater-than-equal-to? user-access-level "OWNER")
+                 [:label {:style {:marginLeft "1rem" :cursor "pointer" :verticalAlign "middle" :display "inline-block" :width 80 :textAlign "center"}}
+                  [:input {:type "checkbox"
+                           :style {:verticalAlign "middle" :float "none"}
+                           :onChange #(swap! state assoc-in [:non-project-owner-acl-vec i :canShare] (.. % -target -checked))
+                           :disabled (common/access-greater-than-equal-to? (:accessLevel acl-entry) "OWNER")
+                           :checked (or (:canShare acl-entry) (common/access-equal-to? (:accessLevel acl-entry) "OWNER"))}]])
+               (when (:pending? acl-entry)
+                 [:span {:style {:fontStyle "italic" :color (:text-light style/colors) :marginLeft "1rem"}}
+                  "Pending..."])])
+            (:non-project-owner-acl-vec @state))
+           [:div {:style {:margin "0.5rem 0"}}
+            [comps/Button {:text "Add new" :icon :add-new
+                           :data-test-id (config/when-debug "add-new-acl-button")
                       :onClick #(swap! state update :non-project-owner-acl-vec
-                                       conj (let [permissions {:email "" :accessLevel "READER"}]
-                                              ; Only owners can set new canShare permissions, so we only want to include
-                                              ; those in the default settings when the user is at least an owner
-                                              (if (common/access-greater-than-equal-to? user-access-level "OWNER")
-                                                (assoc permissions :canShare false)
-                                                permissions)))}]]
-      (style/create-validation-error-message (:validation-error @state))
-      [comps/ErrorViewer {:error (:save-error @state)}]])
-    :ok-button {:text "Save" :onClick #(this :-persist-aclfalse)}}]))
-
-  :-render-invite-offer(fn [{:keys [props state this]}]
-     (let [{:keys [workspace-id ]} props]
-  [comps/OKCancelForm
-   {:header (str "Invite new users to " (:namespace workspace-id) "/" (:name workspace-id))
-    :content (react/create-element
-              [:div {}
-               (when (:saving? @state)
-                 [comps/Blocker {:banner "Updating..."}])
-               [:div {:style {:padding "0.5rem 0" :fontSize "90%" :marginTop "0.5rem"}}
-                [:div {:style {:display "inline-block" :width 400}} "User ID"]
-                [:div {:style {:display "inline-block" :width 200 :marginLeft "1rem"}} "Access Level"]]
-               (map
-                (fn [ acl-entry]
-                  [:div {:style {:borderTop style/standard-line :padding "0.5rem 0"}}
-                   [:div {:style {:display "inline-block" :width 400 :fontSize "88%"}}
-                    (:email acl-entry)]
-                   [:span {:style {:display "inline-block" :lineHeight "33px"
-                                   :width 200 :height 33 :verticalAlign "middle"
-                                   :marginLeft "1rem" :marginBottom 0}}
-                    (:accessLevel acl-entry)]])
-                (:users-not-found @state))])
-    :ok-button {:text "Invite" :onClick #(this :-persist-acl true)}}]))
+                                            conj (let [permissions {:email "" :accessLevel "READER"}]
+                                                   ; Only owners can set new canShare permissions, so we only want to include
+                                                   ; those in the default settings when the user is at least an owner
+                                                   (if (common/access-greater-than-equal-to? user-access-level "OWNER")
+                                                     (assoc permissions :canShare false)
+                                                     permissions)))}]]
+           (style/create-validation-error-message (:validation-error @state))
+           [comps/ErrorViewer {:error (:save-error @state)}]])
+         :ok-button {:text "Save" :onClick #(this :-persist-acl false)}}]))
+   :-render-invite-offer
+   (fn [{:keys [props state this]}]
+     (let [{:keys [workspace-id]} props]
+       [comps/OKCancelForm
+        {:header (str "Invite new users to " (:namespace workspace-id) "/" (:name workspace-id))
+         :content (react/create-element
+                   [:div {}
+                    (when (:saving? @state)
+                      [comps/Blocker {:banner "Updating..."}])
+                    [:div {:style {:padding "0.5rem 0" :fontSize "90%" :marginTop "0.5rem"}}
+                     [:div {:style {:display "inline-block" :width 400}} "User ID"]
+                     [:div {:style {:display "inline-block" :width 200 :marginLeft "1rem"}} "Access Level"]]
+                    (map (fn [acl-entry]
+                           [:div {:style {:borderTop style/standard-line :padding "0.5rem 0"}}
+                            [:div {:style {:display "inline-block" :width 400 :fontSize "88%"}}
+                             (:email acl-entry)]
+                            [:span {:style {:display "inline-block" :lineHeight "33px"
+                                            :width 200 :height 33 :verticalAlign "middle"
+                                            :marginLeft "1rem" :marginBottom 0}}
+                             (:accessLevel acl-entry)]])
+                         (:users-not-found @state))])
+         :ok-button {:text "Invite" :onClick #(this :-persist-acl true)}}]))
    :-persist-acl
    (fn [{:keys [props state refs locals this]} invite-new?]
      (swap! state dissoc :save-error :validation-error)
