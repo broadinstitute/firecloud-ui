25ec9fea8f237a6c6bf0e842a53ebe0e30d80fb7
Matt Bemis <MatthewBemis@users.noreply.github.com>
2017-07-27 14:11:27 -0400

    GAWB-2377: Fix group dropdown in workspace create/clone (#871)
    * fix group dropdown in ws create/clone modals

--- src/cljs/main/broadfcui/page/workspace/summary/workspace_cloner.cljs
+++ src/cljs/main/broadfcui/page/workspace/summary/workspace_cloner.cljs
@@ -66,7 +66,7 @@
    :component-did-mount
    (fn [{:keys [state]}]
      (endpoints/get-groups
-      (fn [parsed-response]
+      (fn [_ parsed-response]
         (swap! state assoc :groups
                (conj (map :groupName parsed-response)
                      "None")))))
