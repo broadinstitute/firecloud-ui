58a83c11a6dfc8b17f10728163b1030eeb50e736
Adam Nichols <anichols@broadinstitute.org>
2017-08-09 10:18:49 -0400

    Bug fix

--- src/cljs/main/broadfcui/page/library/library_page.cljs
+++ src/cljs/main/broadfcui/page/library/library_page.cljs
@@ -148,7 +148,7 @@
    :build-aggregate-fields
    (fn [{:keys [props]}]
      (reduce
-      (fn [results field] (assoc results field (if (contains? (:expanded-aggregates props) field) 0 5))) ;
+      (fn [results field] (assoc results field (if (contains? (utils/log field (:expanded-aggregates props)) field) 0 5))) ;
       {}
       (:aggregate-fields props)))
    :pagination
@@ -281,39 +281,39 @@
                aggregates)))
 
 (react/defc- Facet
-             {:render
-              (fn [{:keys [this state props refs]}]
-                (let [aggregate-field (:aggregate-field props)
-                      properties (:aggregate-properties props)
-                      title (:title properties)
-                      render-hint (get-in properties [:aggregate :renderHint])
-                      aggregations (get-aggregations-for-property aggregate-field (:aggregates props))]
-                  [:div {}
-                   [:hr {}]
-                   (cond
-                     (= render-hint "checkbox") [FacetCheckboxes
-                                                 (merge
-                                                  {:title title :field aggregate-field}
-                                                  (select-keys aggregations [:numOtherDocs :buckets])
-                                                  (select-keys props [:expanded? :selected-items :update-filter ; 4A
-                                                                      :expanded-callback-function]))]
-                     (= render-hint "typeahead-multiselect")
-                     [:div {}
-                      (let [possible-tags (mapv :key (:buckets aggregations))
-                            selected-possible-tags (clojure.set/intersection (utils/log (:selected-items props)) possible-tags)] ; subset of selected tags that are "possible"
-                        ; The above because a tag that has disappeared from the universe of selectable possibilities may still be included in a query from persistence, and not show in the UI
-
-                        (filter/section
-                       {:title title
-                        :content (react/create-element
-                                  [comps/TagAutocomplete {:ref "tag-autocomplete"
-                                                          :tags  ; need to fetch from persistence
-                                                          :data  ;(utils/log (:tags @state)) ; if null calls autocomplete endpoint
-                                                          :show-counts? false
-                                                          :allow-new? false
-                                                          :on-change (partial (:update-filter props) aggregate-field) ; 4B
-                                                          }])
-                        :on-clear #((@refs "tag-autocomplete") :set-tags #{})}))])]))})
+  {:render
+   (fn [{:keys [this state props refs]}]
+     (let [aggregate-field (:aggregate-field props)
+           properties (:aggregate-properties props)
+           title (:title properties)
+           render-hint (get-in properties [:aggregate :renderHint])
+           aggregations (get-aggregations-for-property aggregate-field (:aggregates props))]
+       [:div {}
+        [:hr {}]
+        (cond
+          (= render-hint "checkbox") [FacetCheckboxes
+                                      (merge
+                                       {:title title :field aggregate-field}
+                                       (select-keys aggregations [:numOtherDocs :buckets])
+                                       (select-keys props [:expanded? :selected-items :update-filter ; 4A
+                                                           :expanded-callback-function]))]
+          (= render-hint "typeahead-multiselect")
+          [:div {}
+           (let [tags (mapv :key (:buckets aggregations))
+                 ;; Don't show tags that we pulled out of persistence, but which no longer exist (workspace or tag deletion)
+                 selected-tags (set/intersection (:selected-items props) (set tags))]
+             (filter/section
+              {:title title
+               :content (react/create-element
+                         [comps/TagAutocomplete
+                          {:ref "tag-autocomplete"
+                           :tags (utils/log ":tags " selected-tags)
+                           :data (utils/log ":data " (set tags)) ;(set/difference (set tags) selected-tags)) ; don't show twice
+                           :show-counts? false
+                           :allow-new? false
+                           :on-change (partial (:update-filter props) aggregate-field) ; arity 2; field name passed here, value(s) in component
+                           }])
+               :on-clear #((@refs "tag-autocomplete") :set-tags #{})}))])]))})
 
 (react/defc- FacetSection
   {:render
