4687297a561a5f7be83145bf6ec1bd28faf8e483
Matt Putnam <putnam@broadinstitute.org>
2017-08-22 15:18:55 -0400

    hide error message on optional inputs

--- src/cljs/main/broadfcui/common/method/config_io.cljs
+++ src/cljs/main/broadfcui/common/method/config_io.cljs
@@ -12,11 +12,6 @@
    ))
 
 
-(defn- process-name [{:keys [name] :as item}]
-  (let [[task variable] (take-last 2 (string/split name "."))]
-    (merge item (utils/restructure task variable))))
-
-
 (defn- process-type [string]
   (re-find #"[^?]+" string))
 
@@ -57,7 +52,16 @@
                           "{padding-right: 8px}"
                           "#" id " .select2-container--default .select2-selection--single .select2-selection__arrow"
                           "{display: none}")])
-        [Table {:data (map process-name (io-key inputs-outputs))
+        [Table {:data (->> (io-key inputs-outputs)
+                           (map (fn [{:keys [name inputType outputType optional] :as item}]
+                                  (let [[task variable] (take-last 2 (string/split name "."))
+                                        k-name (keyword name)
+                                        error-message (get-in invalid-values [io-key k-name])
+                                        value (get-in values [io-key k-name])]
+                                    (merge (dissoc item :inputType :outputType :optional)
+                                           (utils/restructure task variable error-message value)
+                                           {:type (process-type (or inputType outputType))
+                                            :optional? optional})))))
                 :body {:empty-message (str "No " (string/capitalize (name io-key)))
                        :style (merge
                                table-style/table-light
@@ -69,21 +73,18 @@
                        :columns
                        (concat [{:header "Task" :initial-width 200
                                  :as-text :task
-                                 :render (fn [{:keys [task optional]}]
-                                           [:div {:style (clip (if optional
+                                 :render (fn [{:keys [task optional?]}]
+                                           [:div {:style (clip (if optional?
                                                                  table-style/table-cell-plank-left-optional
                                                                  table-style/table-cell-plank-left))}
                                             task])}
                                 {:header "Variable" :initial-width 200
                                  :as-text :variable
-                                 :render (fn [{:keys [variable optional]}]
-                                           [:div {:style (clip (if optional
+                                 :render (fn [{:keys [variable optional?]}]
+                                           [:div {:style (clip (if optional?
                                                                  table-style/table-cell-plank-middle-optional
                                                                  table-style/table-cell-plank-middle))} variable])}
                                 {:header "Type" :initial-width 100
-                                 :column-data (fn [{:keys [inputType outputType optional]}]
-                                                {:type (process-type (or inputType outputType))
-                                                 :optional? optional})
                                  :sort-by :type
                                  :as-text (fn [{:keys [type optional?]}]
                                             (str type (when optional? " (optional)")))
@@ -96,41 +97,39 @@
                                     type])}]
                                (when values
                                  [{:header "Attribute" :initial-width 200
-                                   :as-text (fn [{:keys [name]}] (get (io-key values) (keyword name)))
+                                   :as-text :value
                                    :sort-by :text
                                    :render
-                                   (fn [{:keys [name optional]}]
-                                     (let [value (get (io-key values) (keyword name))]
-                                       [:div {:style (clip table-style/default-cell-left)}
-                                        (if editing?
-                                          ;; (ab)using TagAutocomplete instead of Typeahead because it
-                                          ;; plays nicer with tables
-                                          [comps/TagAutocomplete
-                                           {:multiple false :show-counts? false :allow-clear? true
-                                            :minimum-input-length 0
-                                            :tags value
-                                            ;; "" allows for initial empty selection
-                                            ;; `value` ensures that custom selections are initially selected when going to edit
-                                            ;; `distinct` because having multiple copies of the same screws things up
-                                            :data (distinct (concat ["" value] data))
-                                            :placeholder (if optional "Optional" "Select or enter")
-                                            :on-change (fn [value]
-                                                         (swap! locals update io-key assoc (keyword name)
-                                                                (if (empty? value) "" value)))}]
-                                          (if-not (string/blank? value)
-                                            value
-                                            (when optional
-                                              [:span {:style {:color (:text-lighter style/colors)}} "Optional"])))]))}])
+                                   (fn [{:keys [name value optional?]}]
+                                     [:div {:style (clip table-style/default-cell-left)}
+                                      (if editing?
+                                        ;; (ab)using TagAutocomplete instead of Typeahead because it
+                                        ;; plays nicer with tables
+                                        [comps/TagAutocomplete
+                                         {:multiple false :show-counts? false :allow-clear? true
+                                          :minimum-input-length 0
+                                          :tags value
+                                          ;; "" allows for initial empty selection
+                                          ;; `value` ensures that custom selections are initially selected when going to edit
+                                          ;; `distinct` because having multiple copies of the same screws things up
+                                          :data (distinct (concat ["" value] data))
+                                          :placeholder (if optional? "Optional" "Select or enter")
+                                          :on-change (fn [value]
+                                                       (swap! locals update io-key assoc (keyword name)
+                                                              (if (empty? value) "" value)))}]
+                                        (if-not (string/blank? value)
+                                          value
+                                          (when optional?
+                                            [:span {:style {:color (:text-lighter style/colors)}} "Optional"])))])}])
                                (when invalid-values
                                  [{:header "Message" :initial-width 400
-                                   :column-data (fn [{:keys [name]}]
-                                                  (get (io-key invalid-values) (keyword name)))
+                                   :as-text :error-message
                                    :render
-                                   (fn [message]
-                                     (when message
+                                   (fn [{:keys [optional? error-message]}]
+                                     (when (and error-message (not optional?))
                                        [:div {:style (clip table-style/default-cell-left)}
                                         (icons/icon {:style {:marginRight "0.5rem"
                                                              :color (:exception-state style/colors)}}
                                                     :error)
-                                        message]))}]))}
+                                        error-message]))}]))}
                 :paginator :none}]]))})
