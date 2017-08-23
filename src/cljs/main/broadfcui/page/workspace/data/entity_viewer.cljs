47df6942dddab2de2aa9c608bc9fc0ea01a56337
Matt Putnam <matt.s.putnam@gmail.com>
2017-08-23 17:00:33 -0400

    fix the entity viewer (#917)

--- src/cljs/main/broadfcui/page/workspace/data/entity_viewer.cljs
+++ src/cljs/main/broadfcui/page/workspace/data/entity_viewer.cljs
@@ -3,6 +3,7 @@
    [dmohs.react :as react]
    [clojure.string :as string]
    [broadfcui.common :as common]
+   [broadfcui.common.components :as comps]
    [broadfcui.common.entity-table :as entity-table]
    [broadfcui.common.gcs-file-preview :refer [GCSFilePreviewLink]]
    [broadfcui.common.icons :as icons]
@@ -30,45 +31,44 @@
   {:get-initial-state
    (fn []
      {:last-entity []})
+   :component-will-mount
+   (fn [{:keys [this props]}]
+     (let [{:keys [entity-type entity-name]} props]
+       (this :-update-and-load entity-type entity-name)))
    :render
-   (fn [{:keys [props state]}]
-     (let [{:keys [update-parent-state entity-type entity-name attr-list]} props
-           item-column-name (get-column-name entity-type)
-           update-and-load (fn [item-type item-name]
-                             (update-parent-state :selected-entity item-name
-                                                  :selected-attr-list nil
-                                                  :loading-attributes true)
-                             (data-utils/get-entity-attrs {:entity-name item-name
-                                                           :entity-type item-type
-                                                           :workspace-id (:workspace-id props)
-                                                           :update-parent-state update-parent-state}))
+   (fn [{:keys [props state this]}]
+     (let [{:keys [update-parent-state]} props
+           {:keys [selected-entity selected-entity-type selected-attr-list]} @state
+           item-column-name (get-column-name selected-entity-type)
            item-link (fn [item-type item-name]
                        (links/create-internal
                          {:onClick (fn []
                                      (swap! state update
                                             :last-entity conj {:type (:entity-type props)
                                                                :name (:entity-name props)})
-                                     (update-and-load item-type item-name))}
+                                     (this :-update-and-load item-type item-name))}
                          item-name))]
-       [:div {:style {:width "30%" :padding "0.5rem" :marginLeft ".38em"
+       [:div {:style {:position "relative" :width "30%" :padding "0.5rem" :marginLeft ".38em"
                       :border (str "1px solid " (:line-default style/colors))}}
         [:a {:onClick #(update-parent-state :selected-entity nil)
              :href "javascript:;"
              :style {:padding "1rem" :float "right"
                      :fontSize "80%" :color (:text-light style/colors)}}
          (icons/icon {} :close)]
+        (when (:loading-attributes? @state)
+          [comps/Blocker {:banner "Loading entity attributes..."}])
         (when (not-empty (:last-entity @state))
           (let [last-entity (last (:last-entity @state))]
             [:a {:onClick (fn []
                             (swap! state update :last-entity pop)
-                            (update-and-load (:type last-entity) (:name last-entity)))
+                            (this :-update-and-load (:type last-entity) (:name last-entity)))
                  :href "javascript:;"
                  :style {:padding "1rem"
                          :color (:text-light style/colors)}}
              (icons/icon {} :angle-left)]))
-        [:div {:style {:display "inline-block" :fontWeight "bold" :padding "1rem 0 0 1rem" :marginBottom "1em"}} entity-name]
-        [Table {:key entity-type ; key to enforce re-evaluating columns when changing entity types
-                :data (or attr-list [])
+        [:div {:style {:display "inline-block" :fontWeight "bold" :padding "1rem 0 0 1rem" :marginBottom "1em"}} selected-entity]
+        [Table {:key selected-entity-type ; key to enforce re-evaluating columns when changing entity types
+                :data (or selected-attr-list [])
                 :body {:empty-message (if (= item-column-name "Entity")
                                         "No Entity Attributes defined"
                                         (str "No " item-column-name "s defined"))
@@ -78,7 +78,7 @@
                                 :header-row {:borderBottom (str "2px solid " (:line-default style/colors))
                                              :backgroundColor "white" :color "black" :fontWeight "bold"}})
                        :behavior {:reorderable-columns? false :filterable? false}
-                       :columns (if (data-utils/is-entity-set? entity-type)
+                       :columns (if (data-utils/is-entity-set? selected-entity-type)
                                   [{:header item-column-name :initial-width :auto
                                     :as-text :entityName :sort-by :text
                                     :sort-initial :asc
@@ -119,4 +119,14 @@
                                                :for-render v})))
                                     :sort-by :for-sort
                                     :render :for-render}])}
-                :paginator :none}]]))})
+                :paginator :none}]]))
+   :-update-and-load
+   (fn [{:keys [state props]} item-type item-name]
+     (let [update-viewer-state (fn [& args]
+                                 (apply swap! state assoc args))]
+       (swap! state assoc :selected-entity item-name :selected-entity-type item-type
+              :loading-attributes? true :selected-attr-list nil)
+       (data-utils/get-entity-attrs {:entity-name item-name
+                                     :entity-type item-type
+                                     :workspace-id (:workspace-id props)
+                                     :update-parent-state update-viewer-state})))})
