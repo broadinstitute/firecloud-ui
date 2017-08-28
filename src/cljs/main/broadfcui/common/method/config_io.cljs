09206468047305f683e8ad512c2dbbd88c897810
Matt Putnam <putnam@broadinstitute.org>
2017-08-28 13:00:40 -0400

    Only put one style tag when editing

--- src/cljs/main/broadfcui/common/method/config_io.cljs
+++ src/cljs/main/broadfcui/common/method/config_io.cljs
@@ -32,20 +32,10 @@
      (swap! state dissoc :editing?)
      @locals)
    :render
-   (fn [{:keys [this props]}]
-     [:div {:style (:style props)}
-      [Collapse {:title "Inputs"
-                 :contents (this :-render-table :inputs)}]
-      [Collapse {:style {:marginTop "1rem"}
-                 :title "Outputs"
-                 :contents (this :-render-table :outputs)}]])
-   :-render-table
-   (fn [{:keys [props state locals]} io-key]
-     (let [{:keys [inputs-outputs values invalid-values data]} props
-           {:keys [editing?]} @state
-           id (gensym "io-table-")]
-       [:div {:id id}
-        (when editing?
+   (fn [{:keys [props state this]}]
+     (let [id (gensym "io-table-")]
+       [:div {:id id :style (:style props)}
+        (when (:editing? @state)
           [:style {} (str ".select2-results__option"
                           "{font-size: 80%}"
                           "#" id " .select2-container--default .select2-selection--multiple .select2-selection__choice__remove"
@@ -56,85 +46,94 @@
                           "{padding-top: 0.1rem}"
                           "#" id " .select2-selection__rendered > li+li"
                           "{width: 10px}")])
-        [Table {:data (->> (io-key inputs-outputs)
-                           (map (fn [{:keys [name inputType outputType optional] :as item}]
-                                  (let [[task variable] (take-last 2 (string/split name "."))
-                                        k-name (keyword name)
-                                        error-message (get-in invalid-values [io-key k-name])
-                                        value (get-in values [io-key k-name])]
-                                    (merge (dissoc item :inputType :outputType :optional)
-                                           (utils/restructure task variable error-message value)
-                                           {:type (process-type (or inputType outputType))
-                                            :optional? optional})))))
-                :body {:empty-message (str "No " (string/capitalize (name io-key)))
-                       :style (merge
-                               table-style/table-light
-                               {:body-cell {:padding 0}
-                                :header-row {:borderBottom style/standard-line}
-                                :body-row (constantly {:margin "4px 0" :alignItems "baseline"})})
-                       :behavior {:filterable? false :reorderable-columns? false :allow-no-sort? true
-                                  :sortable-columns? (not editing?)}
-                       :columns
-                       (concat [{:header "Task" :initial-width 200
-                                 :column-data :task
-                                 :render (fn [task]
-                                           [:div {:style (clip table-style/table-cell-plank-left)} task])}
-                                {:header "Variable" :initial-width 200
-                                 :as-text (fn [{:keys [variable optional?]}]
-                                            (str variable (when optional? " (optional)")))
-                                 :sort-by :text
-                                 :render (fn [{:keys [variable optional?]}]
-                                           [:div {:style (clip (if optional?
-                                                                 (merge table-style/table-cell-plank-middle
-                                                                        table-style/table-cell-optional)
-                                                                 table-style/table-cell-plank-middle))} variable])}
-                                {:header "Type" :initial-width 120
-                                 :sort-by :type
-                                 :as-text (fn [{:keys [type optional?]}]
-                                            (str type (when optional? " (optional)")))
-                                 :render
-                                 (fn [{:keys [type optional?]}]
-                                   [:div {:style (clip (if optional?
-                                                         (merge table-style/table-cell-plank-right
-                                                                table-style/table-cell-optional)
-                                                         table-style/table-cell-plank-right))}
-                                    type])}]
-                               (when values
-                                 [{:header "Attribute" :initial-width 200
-                                   :as-text :value
-                                   :sort-by :text
-                                   :render
-                                   (fn [{:keys [name value optional?]}]
-                                     (let [value (when-not (string/blank? value) value)]
-                                       [:div {:style (clip table-style/default-cell-left)}
-                                        (if editing?
-                                          ;; (ab)using TagAutocomplete instead of Typeahead because it
-                                          ;; plays nicer with tables
-                                          [comps/TagAutocomplete
-                                           {:multiple true :show-counts? false :allow-clear? true
-                                            :minimum-input-length 0
-                                            :tags [value] :maximum-selection-length 1
-                                            :language {:maximumSelected (fn [] "")}
-                                            ;; `value` ensures that custom selections are initially selected when going to edit
-                                            ;; `distinct` because having multiple copies of the same screws things up
-                                            :data (distinct (if value (conj data value) data))
-                                            :placeholder (if optional? "Optional" "Select or enter")
-                                            :on-change (fn [value]
-                                                         (swap! locals update io-key assoc (keyword name)
-                                                                (if (empty? value) "" (first value))))}]
-                                          (if value
-                                            [:span {:style (when optional? table-style/table-cell-optional)} value]
-                                            (when optional?
-                                              [:span {:style {:color (:text-lighter style/colors)}} "Optional"])))]))}])
-                               (when invalid-values
-                                 [{:header "Message" :initial-width 400
-                                   :as-text :error-message :sort-by :text
-                                   :render
-                                   (fn [{:keys [optional? error-message]}]
-                                     (when (and error-message (not optional?))
-                                       [:div {:style (clip table-style/default-cell-left)}
-                                        (icons/icon {:style {:marginRight "0.5rem"
-                                                             :color (:exception-state style/colors)}}
-                                                    :error)
-                                        error-message]))}]))}
-                :paginator :none}]]))})
+        [Collapse {:title "Inputs"
+                   :contents (this :-render-table :inputs)}]
+        [Collapse {:style {:marginTop "1rem"}
+                   :title "Outputs"
+                   :contents (this :-render-table :outputs)}]]))
+   :-render-table
+   (fn [{:keys [props state locals]} io-key]
+     (let [{:keys [inputs-outputs values invalid-values data]} props
+           {:keys [editing?]} @state]
+       [Table {:data (->> (io-key inputs-outputs)
+                          (map (fn [{:keys [name inputType outputType optional] :as item}]
+                                 (let [[task variable] (take-last 2 (string/split name "."))
+                                       k-name (keyword name)
+                                       error-message (get-in invalid-values [io-key k-name])
+                                       value (get-in values [io-key k-name])]
+                                   (merge (dissoc item :inputType :outputType :optional)
+                                          (utils/restructure task variable error-message value)
+                                          {:type (process-type (or inputType outputType))
+                                           :optional? optional})))))
+               :body {:empty-message (str "No " (string/capitalize (name io-key)))
+                      :style (merge
+                              table-style/table-light
+                              {:body-cell {:padding 0}
+                               :header-row {:borderBottom style/standard-line}
+                               :body-row (constantly {:margin "4px 0" :alignItems "baseline"})})
+                      :behavior {:filterable? false :reorderable-columns? false :allow-no-sort? true
+                                 :sortable-columns? (not editing?)}
+                      :columns
+                      (concat [{:header "Task" :initial-width 180
+                                :column-data :task
+                                :render (fn [task]
+                                          [:div {:style (clip table-style/table-cell-plank-left)} task])}
+                               {:header "Variable" :initial-width 180
+                                :as-text (fn [{:keys [variable optional?]}]
+                                           (str variable (when optional? " (optional)")))
+                                :sort-by :text
+                                :render (fn [{:keys [variable optional?]}]
+                                          [:div {:style (clip (if optional?
+                                                                (merge table-style/table-cell-plank-middle
+                                                                       table-style/table-cell-optional)
+                                                                table-style/table-cell-plank-middle))} variable])}
+                               {:header "Type" :initial-width 120
+                                :sort-by :type
+                                :as-text (fn [{:keys [type optional?]}]
+                                           (str type (when optional? " (optional)")))
+                                :render
+                                (fn [{:keys [type optional?]}]
+                                  [:div {:style (clip (if optional?
+                                                        (merge table-style/table-cell-plank-right
+                                                               table-style/table-cell-optional)
+                                                        table-style/table-cell-plank-right))}
+                                   type])}]
+                              (when values
+                                [{:header "Attribute" :initial-width 200
+                                  :as-text :value
+                                  :sort-by :text
+                                  :render
+                                  (fn [{:keys [name value optional?]}]
+                                    (let [value (when-not (string/blank? value) value)]
+                                      [:div {:style (clip table-style/default-cell-left)}
+                                       (if editing?
+                                         ;; (ab)using TagAutocomplete instead of Typeahead because it
+                                         ;; plays nicer with tables
+                                         [comps/TagAutocomplete
+                                          {:multiple true :show-counts? false :allow-clear? true
+                                           :minimum-input-length 0
+                                           :tags [value] :maximum-selection-length 1
+                                           :language {:maximumSelected (fn [] "")}
+                                           ;; `value` ensures that custom selections are initially selected when going to edit
+                                           ;; `distinct` because having multiple copies of the same screws things up
+                                           :data (distinct (if value (conj data value) data))
+                                           :placeholder (if optional? "Optional" "Select or enter")
+                                           :on-change (fn [value]
+                                                        (swap! locals update io-key assoc (keyword name)
+                                                               (if (empty? value) "" (first value))))}]
+                                         (if value
+                                           [:span {:style (when optional? table-style/table-cell-optional)} value]
+                                           (when optional?
+                                             [:span {:style {:color (:text-lighter style/colors)}} "Optional"])))]))}])
+                              (when invalid-values
+                                [{:header "Message" :initial-width 400
+                                  :as-text :error-message :sort-by :text
+                                  :render
+                                  (fn [{:keys [optional? error-message]}]
+                                    (when (and error-message (not optional?))
+                                      [:div {:style (clip table-style/default-cell-left)}
+                                       (icons/icon {:style {:marginRight "0.5rem"
+                                                            :color (:exception-state style/colors)}}
+                                                   :error)
+                                       error-message]))}]))}
+               :paginator :none}]))})
