016c6760300cd3874c3b972d830d6dc1dd070811
Jackie Roberti <jacqueline.roberti93@gmail.com>
2017-09-08 16:10:12 -0400

    add jenkins script

---
+++ jenkins/run-automation-tests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ -z "${HOST_IP}" ]; then
+    echo "FATAL ERROR: SSH_HOST undefined."
+    exit 1
+fi
+
+if [ -z "${ENV}" ]; then
+    echo "FATAL ERROR: ENV undefined."
+    exit 2
+fi
+
+SCALATESTS=automation
+VAULT_TOKEN=$(cat /etc/vault-token-dsde)
+
+# build test docker image
+cd automation
+docker build -f Dockerfile-tests -t $SCALATESTS .
+
+# run tests
+cd docker
+./run-tests.sh 3 $ENV $HOST_IP $SCALATESTS $VAULT_TOKEN
+TEST_EXIT_CODE=$?
+
+# do some cleanup after
+sudo chmod -R 777 logs
+
+# exit with exit code of test script
+exit $TEST_EXIT_CODE
