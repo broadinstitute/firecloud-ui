d4de9967f4d917d8186e45656fccc9ff67bcb458
David An <davidan@broadinstitute.org>
2017-09-14 18:42:42 -0400

    PR feedback: margins for Collapse

--- src/cljs/main/broadfcui/components/collapse.cljs
+++ src/cljs/main/broadfcui/components/collapse.cljs
@@ -9,17 +9,19 @@
   {:get-initial-state
    (fn [{:keys [props]}]
      {:visible? (not (:default-hidden? props))})
+   :get-default-props
+   (fn [] {:show-title-when-expanded? true})
    :render
    (fn [{:keys [props state]}]
-     (let [{:keys [title title-expand contents]} props
+     (let [{:keys [title title-expand show-title-when-expanded? contents]} props
            {:keys [visible?]} @state]
-       [:div (dissoc props :title :title-expand :contents :default-hidden?)
+       [:div (dissoc props :title :title-expand :show-title-when-expanded? :contents :default-hidden?)
         [:div {:style {:display "flex"}}
          [:div {:style {:display "flex" :cursor "pointer" :alignItems "baseline"}
                 :onClick #(swap! state update :visible? not)}
           (icons/icon {:className "fa-fw" :style {:flexShrink 0}}
                       (if visible? :disclosure-opened :disclosure-closed))
-          title]
+          (when (or show-title-when-expanded? (not visible?)) title)]
          (when visible? title-expand)]
         [:div {:style {:display (when-not visible? "none")
                        :marginLeft "1.25rem"}} ; to match the disclosure triangle
