e68598ff4500c1668d5f610f1007ea825e02a89d
Jackie Roberti <jacqueline.roberti93@gmail.com>
2017-09-14 16:33:45 -0400

    capture test output summary

--- automation/test.sh
+++ automation/test.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-sbt test -Djsse.enableSNIExtension=false -Dheadless=true
+sbt test -Djsse.enableSNIExtension=false -Dheadless=true | tee testout.txt
 TEST_EXIT_CODE=$?
 sbt clean
+
+cat testout.txt | sed -n -e '/Run completed/,$p' | sed $'s,\x1b\\[[0-9;]*[a-zA-Z],,g' > output/testsummary.txt
 
 if [[ $TEST_EXIT_CODE != 0 ]]; then exit $TEST_EXIT_CODE; fi
