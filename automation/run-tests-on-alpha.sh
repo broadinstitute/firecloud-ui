70162f721df799f677d512eb491c3a822d698d37
Andrea Haessly <ahaessly@broadinstitute.org>
2017-09-21 13:55:13 -0400

    echo message reminding user to run on alpha branch

--- automation/run-tests-on-alpha.sh
+++ automation/run-tests-on-alpha.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-
+echo ""
+echo "!!! Checkout alpha branch of firecloud-ui !!!"
+echo ""
 docker build -f Dockerfile-tests -t automation .
 cd docker
 ./run-tests.sh 4 alpha alpha automation $(cat ~/.vault-token)
