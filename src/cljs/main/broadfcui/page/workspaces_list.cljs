7150d55275e304ffe71f5c655b3038a64a0bef1f
Greg Rushton <grushton@broadinstitute.org>
2017-09-22 10:53:12 -0400

    fix controlled logic

--- src/cljs/main/broadfcui/page/workspaces_list.cljs
+++ src/cljs/main/broadfcui/page/workspaces_list.cljs
@@ -161,7 +161,7 @@
     :render #(if (= % :open) "TCGA Open Access" "TCGA Controlled Access")
     :predicate (fn [ws option]
                  (and (= (config/tcga-namespace) (get-in ws [:workspace :namespace]))
-                      ((if (= option :open) empty? identity) (get-in ws [:workspace :authorizationDomain]))))}])
+                      ((if (= option :open) empty? not-empty) (get-in ws [:workspace :authorizationDomain]))))}])
 
 (def ^:private persistence-key "workspace-table-types")
 (def ^:private VERSION 2)
