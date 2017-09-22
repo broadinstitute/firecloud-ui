0263a98cda269fabcf1381bb5c0068ae51a9a1f4
Isaac Zarsky <zarsky@broadinstitute.org>
2017-09-22 18:01:05 -0400

    initial: using react-autosuggest

--- src/cljs/main/broadfcui/common/method/config_io.cljs
+++ src/cljs/main/broadfcui/common/method/config_io.cljs
@@ -2,11 +2,11 @@
   (:require
    [dmohs.react :as react]
    [clojure.string :as string]
-   [broadfcui.common.components :as comps]
    [broadfcui.common.icons :as icons]
    [broadfcui.common.style :as style]
    [broadfcui.common.table :refer [Table]]
    [broadfcui.common.table.style :as table-style]
+   [broadfcui.components.autosuggest :refer [Autosuggest]]
    [broadfcui.components.collapse :refer [Collapse]]
    [broadfcui.utils :as utils]
    ))
@@ -54,7 +54,7 @@
                    :default-hidden? (:default-hidden? props)
                    :contents (this :-render-table :outputs)}]]))
    :-render-table
-   (fn [{:keys [props state locals]} io-key]
+   (fn [{:keys [props state]} io-key]
      (let [{:keys [inputs-outputs values invalid-values data]} props
            {:keys [editing?]} @state]
        [Table {:data (->> (io-key inputs-outputs)
@@ -108,28 +108,16 @@
                                   :sort-by :text
                                   :render
                                   (fn [{:keys [name value optional?]}]
-                                    (let [value (when-not (string/blank? value) value)]
-                                      [:div {:style (clip table-style/default-cell-left)}
-                                       (if editing?
-                                         ;; (ab)using TagAutocomplete instead of Typeahead because it
-                                         ;; plays nicer with tables
-                                         [comps/TagAutocomplete
-                                          {:data-test-id (str name "-text-input")
-                                           :show-counts? false :allow-clear? true
-                                           :minimum-input-length 0
-                                           :tags [value] :maximum-selection-length 1
-                                           :language {:maximumSelected (fn [] "")}
-                                           ;; `value` ensures that custom selections are initially selected when going to edit
-                                           ;; `distinct` because having multiple copies of the same screws things up
-                                           :data (distinct (if value (conj data value) data))
-                                           :placeholder (if optional? "Optional" "Select or enter")
-                                           :on-change (fn [value]
-                                                        (swap! locals update io-key assoc (keyword name)
-                                                               (if (empty? value) "" (first value))))}]
-                                         (if value
-                                           [:span {:style (when optional? table-style/table-cell-optional)} value]
-                                           (when optional?
-                                             [:span {:style {:color (:text-lighter style/colors)}} "Optional"])))]))}])
+                                    [:div {:style (clip table-style/default-cell-left)}
+                                     (if editing?
+                                       [Autosuggest
+                                        {:data data
+                                         :label name
+                                         :placeholder (if optional? "Optional" "Select or enter")}]
+                                       (if value
+                                         [:span {:style (when optional? table-style/table-cell-optional)} value]
+                                         (when optional?
+                                           [:span {:style {:color (:text-lighter style/colors)}} "Optional"])))])}])
                               (when invalid-values
                                 [{:header "Message" :initial-width 400
                                   :as-text :error-message :sort-by :text
---
+++ src/cljs/main/broadfcui/components/autosuggest.cljs
@@ -0,0 +1,67 @@
+(ns broadfcui.components.autosuggest
+  (:require
+   [dmohs.react :as react]
+   [cljsjs.react-autosuggest]
+   [clojure.string :as string]
+   [broadfcui.common.style :as style]
+   [broadfcui.utils :as utils]
+   ))
+
+
+(react/defc Autosuggest
+  {:component-will-mount
+   (fn [{:keys [locals]}]
+     (swap! locals assoc :id (gensym "autosuggest")))
+   :render
+   (fn [{:keys [state props locals]}]
+     (let [{:keys [data placeholder label]} props
+           {:keys [suggestions value]} @state
+           on-change (fn [_ value]
+                       (let [value (.-newValue value)]
+                         (swap! state assoc :value (or value ""))))
+           get-suggestions (fn [value]
+                             (filterv
+                              #(string/includes? (string/lower-case %)
+                                                 (string/lower-case (.-value value)))
+                              data))]
+       [js/Autosuggest
+        (clj->js (utils/deep-merge
+                  {:suggestions (or suggestions [])
+                   :onSuggestionsFetchRequested #(swap! state assoc :suggestions (get-suggestions %))
+                   :onSuggestionsClearRequested #(swap! state assoc :suggestions [])
+                   :getSuggestionValue identity
+                   :renderSuggestion (fn [suggestion]
+                                       (react/create-element [:div {} suggestion]))
+                   :inputProps
+                   {:data-test-id (str label "-text-input")
+                    :value (or value "")
+                    :placeholder placeholder
+                    :onChange on-change}
+                   :highlightFirstSuggestion true
+                   :id (:id @locals)
+                   :theme
+                   {:container {}
+                    :containerOpen {}
+                    :input (merge style/input-text-style {:width "95%"})
+                    :inputOpen {:borderBottomLeftRadius 0 :borderBottomRightRadius 0}
+                    :inputFocused {:outline "none"}
+                    :suggestionsContainer {}
+                    :suggestionsContainerOpen {:display "block"
+                                               :position "absolute"
+                                               :width 280
+                                               :border "1px solid #aaa"
+                                               :background-color "#fff"
+                                               :borderBottomLeftRadius 4
+                                               :borderBottomRightRadius 4
+                                               :zIndex 2}
+                    :suggestionsList {:margin 0
+                                      :padding 0
+                                      :listStyle "none"}
+                    :suggestion {:cursor "pointer"
+                                 :padding "10px 20px"}
+                    :suggestionFirst {}
+                    :suggestionHighlighted {:backgroundColor (:tag-background style/colors)}
+                    :sectionContainer {}
+                    :sectionContainerFirst {}
+                    :sectionTitle {}}}
+                  (dissoc props :data :placeholder :label)))]))})
