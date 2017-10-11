3c9d9d894388f3456c350d33e09d05a3ca7e6011
Greg Rushton <grushton@broadinstitute.org>
2017-10-11 08:17:03 -0400

    finish the actual commit

---
+++ src/cljs/main/broadfcui/common/method/messages.cljs
@@ -0,0 +1,15 @@
+(ns broadfcui.common.method.messages
+  (:require
+   [broadfcui.common.icons :as icons]
+   [broadfcui.common.style :as style]
+   [broadfcui.utils :as utils]
+   ))
+
+
+(defn get-methods-repo-group-alert []
+  [:div {:style {:paddingBottom "1em" :fontSize "100%"}}
+   (icons/icon {:style {:fontSize 22 :color (:exception-state style/colors) :marginRight "1rem"}}
+               :warning)
+   (str "Currently user groups are not supported in the Method Repository. "
+        "That is you cannot share a method/configuration with a user group. "
+        "We appreciate your patience as we work on developing this feature.")])
--- src/cljs/main/broadfcui/common/method/sync.cljs
+++ src/cljs/main/broadfcui/common/method/sync.cljs
@@ -4,6 +4,7 @@
    [clojure.string :as string]
    [broadfcui.common.components :as comps]
    [broadfcui.common.links :as links]
+   [broadfcui.common.method.messages :as messages]
    [broadfcui.common.style :as style]
    [broadfcui.components.buttons :as buttons]
    [broadfcui.components.modals :as modals]
@@ -25,6 +26,7 @@
        :content
        (react/create-element
         [:div {:style {:maxWidth 670}}
+         (messages/get-methods-repo-group-alert)
          [:div {:style {:marginBottom "1.5rem"}}
           (str "In order to allow other users of this "
                (:flavor props)
