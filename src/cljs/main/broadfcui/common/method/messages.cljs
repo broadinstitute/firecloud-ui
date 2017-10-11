82649645ab3ed9f9a971f8ccbac36b5f83548331
Greg Rushton <grushton@broadinstitute.org>
2017-10-11 14:14:45 -0400

    PR feedback

--- src/cljs/main/broadfcui/common/method/messages.cljs
+++ src/cljs/main/broadfcui/common/method/messages.cljs
@@ -7,9 +7,9 @@
 
 
 (defn get-methods-repo-group-alert []
-  [:div {:style {:paddingBottom "1em" :fontSize "100%"}}
+  [:p {:style {:fontSize "100%"}}
    (icons/icon {:style {:fontSize 22 :color (:exception-state style/colors) :marginRight "1rem"}}
                :warning)
-   (str "Currently user groups are not supported in the Method Repository. "
-        "That is, you cannot share a method/configuration with a user group. "
-        "We appreciate your patience as we work on developing this feature.")])
+   "Currently user groups are not supported in the Method Repository. "
+   "That is, you cannot share a method/configuration with a user group. "
+   "We appreciate your patience as we work on developing this feature."])
