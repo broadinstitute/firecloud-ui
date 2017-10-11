d7a4237bbad828b737cf7664ad904a91b51b2343
Jackie Roberti <jacqueline.roberti93@gmail.com>
2017-10-11 16:02:38 -0400

    add withSignIn fixture; fix workspace spec tests

---
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/fixture/UserFixtures.scala
@@ -0,0 +1,30 @@
+package org.broadinstitute.dsde.firecloud.fixture
+
+import org.broadinstitute.dsde.firecloud.config.{AuthToken, Config, Credentials}
+import org.broadinstitute.dsde.firecloud.page.workspaces.WorkspaceListPage
+import org.broadinstitute.dsde.firecloud.test.{CleanUp, WebBrowserSpec}
+import org.openqa.selenium.WebDriver
+import org.scalatest.Suite
+
+trait UserFixtures extends CleanUp { self: WebBrowserSpec with Suite =>
+
+  def withSignIn(user: Credentials,
+                 cleanUp: Boolean = true)
+                (testCode: (WorkspaceListPage) => Any)(implicit webdriver: WebDriver): Unit = {
+
+    val listPage = signIn(user)
+    val userEmail: String = user.email
+
+    try {
+      testCode(listPage)
+    } finally {
+      if (cleanUp) {
+        try {
+          listPage.signOut()
+        } catch nonFatalAndLog(s"Error logging out user: $userEmail")
+      }
+    }
+
+  }
+
+}
--- automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/user/SignInSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/user/SignInSpec.scala
@@ -2,25 +2,28 @@ package org.broadinstitute.dsde.firecloud.test.user
 
 import org.broadinstitute.dsde.firecloud.config.{AuthToken, AuthTokens, Config}
 import org.broadinstitute.dsde.firecloud.test.{CleanUp, WebBrowserSpec}
+import org.broadinstitute.dsde.firecloud.fixture.UserFixtures
 import org.scalatest._
 import org.broadinstitute.dsde.firecloud.test.Tags
 
 
-class SignInSpec extends FreeSpec with WebBrowserSpec with CleanUp {
+class SignInSpec extends FreeSpec with WebBrowserSpec with UserFixtures with CleanUp {
 
-  implicit val authToken: AuthToken = AuthTokens.draco
+  // implicit val authToken: AuthToken = AuthTokens.draco
 
   "A user" - {
     "with a registered account" - {
 
       "should be able to log in and out multiple times as multiple users" taggedAs Tags.GooglePassing in withWebDriver { implicit driver =>
-        var listPageAsUser1 = signIn(Config.Users.draco)
-        listPageAsUser1.signOut()
-        val listPageAsUser2 = signIn(Config.Users.snape)
-        assert(listPageAsUser2.readUserEmail().equals(Config.Users.snape.email))
-        listPageAsUser2.signOut()
-        listPageAsUser1 = signIn(Config.Users.draco)
-        assert(listPageAsUser1.readUserEmail().equals(Config.Users.draco.email))
+        withSignIn(Config.Users.draco) { listPage =>
+          listPage.signOut()
+          val listPageAsUser2 = signIn(Config.Users.snape)
+          assert(listPageAsUser2.readUserEmail().equals(Config.Users.snape.email))
+          listPageAsUser2.signOut()
+          val listPageAsUser1 = signIn(Config.Users.draco)
+          assert(listPageAsUser1.readUserEmail().equals(Config.Users.draco.email))
+        }
+
       }
     }
 
