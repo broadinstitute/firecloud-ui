75c3a061291269b0410ad7b58c5b0cf994b79ae4
Greg Rushton <grushton@broadinstitute.org>
2017-10-11 10:01:55 -0400

    comma

--- src/cljs/main/broadfcui/common/method/messages.cljs
+++ src/cljs/main/broadfcui/common/method/messages.cljs
@@ -11,5 +11,5 @@
    (icons/icon {:style {:fontSize 22 :color (:exception-state style/colors) :marginRight "1rem"}}
                :warning)
    (str "Currently user groups are not supported in the Method Repository. "
-        "That is you cannot share a method/configuration with a user group. "
+        "That is, you cannot share a method/configuration with a user group. "
         "We appreciate your patience as we work on developing this feature.")])
