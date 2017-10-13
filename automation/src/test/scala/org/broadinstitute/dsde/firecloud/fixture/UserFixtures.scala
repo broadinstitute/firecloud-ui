3f5762d10fa646c310eaf2fd6e6dc1bb41a31647
Jackie <jacqueline.roberti93@gmail.com>
2017-10-12 20:00:34 -0400

    use different users; tag tests as using google workaround (#996)
    * use different users; tag tests as using google workaround
    
    * add register spec and auth domain spec
    
    * add withSignIn fixture; fix workspace spec tests
    
    * fix auth domain spec
    
    * hasMethod fixture; test if hermione is unblocked for MethodRepoSpec
    
    * Dataspec
    
    * use withSignIn fixture with everything else; run all tests except those specifically marked as failing
    
    * more fluent signin spec
    
    * use ignore instead of Failing tag
    
    * oops
    
    * don't make withSignIn have an optional logout
    
    * disable bad test
    
    * get rid of GooglePassing tags

---
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/fixture/UserFixtures.scala
@@ -0,0 +1,27 @@
+package org.broadinstitute.dsde.firecloud.fixture
+
+import org.broadinstitute.dsde.firecloud.config.{AuthToken, Config, Credentials}
+import org.broadinstitute.dsde.firecloud.page.workspaces.WorkspaceListPage
+import org.broadinstitute.dsde.firecloud.test.{CleanUp, WebBrowserSpec}
+import org.openqa.selenium.WebDriver
+import org.scalatest.Suite
+
+trait UserFixtures extends CleanUp { self: WebBrowserSpec with Suite =>
+
+  def withSignIn(user: Credentials)
+                (testCode: (WorkspaceListPage) => Any)(implicit webdriver: WebDriver): Unit = {
+
+    val listPage = signIn(user)
+    val userEmail: String = user.email
+
+    try {
+      testCode(listPage)
+    } finally {
+      try {
+        listPage.signOut()
+      } catch nonFatalAndLog(s"Error logging out user: $userEmail")
+    }
+
+  }
+
+}
