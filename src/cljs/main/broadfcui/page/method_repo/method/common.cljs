96f6347d540a1338005c5580171482b92004b544
Matt Putnam <putnam@broadinstitute.org>
2017-10-30 15:59:57 -0400

    create ConfigTable component that loads its own data, use for method repo page

--- src/cljs/main/broadfcui/page/method_repo/method/common.cljs
+++ src/cljs/main/broadfcui/page/method_repo/method/common.cljs
@@ -3,6 +3,7 @@
    [dmohs.react :as react]
    [clojure.string :as string]
    [broadfcui.common.components :as comps]
+   [broadfcui.common.icons :as icons]
    [broadfcui.common.links :as links]
    [broadfcui.common.method.config-io :as config-io]
    [broadfcui.common.style :as style]
@@ -11,6 +12,7 @@
    [broadfcui.components.modals :as modals]
    [broadfcui.endpoints :as endpoints]
    [broadfcui.nav :as nav]
+   [broadfcui.net :as net]
    [broadfcui.page.workspace.method-configs.synchronize :as mc-sync]
    [broadfcui.utils :as utils]
    ))
@@ -36,6 +38,78 @@
                   (if success?
                     (swap! state assoc :inputs-outputs (get-parsed-response))
                     (swap! state assoc :error (get-parsed-response false))))}))})
+
+
+(react/defc ConfigTable
+  {:refresh
+   (fn [{:keys [props state]}]
+     (swap! state dissoc :associated-configs :compatible-configs :resolved-configs :configs-error)
+     (endpoints/call-ajax-orch
+      {:endpoint (endpoints/get-agora-method-configs (:method-id props))
+       :on-done (net/handle-ajax-response
+                 (fn [{:keys [success? parsed-response]}]
+                   (if success?
+                     (swap! state assoc :associated-configs parsed-response)
+                     (swap! state assoc :configs-error (:message parsed-response)))))})
+     (endpoints/call-ajax-orch
+      {:endpoint (endpoints/get-agora-compatible-configs (conj (:method-id props) (select-keys props [:snapshot-id])))
+       :on-done (net/handle-ajax-response
+                 (fn [{:keys [success? parsed-response]}]
+                   (if success?
+                     (swap! state assoc :compatible-configs (set parsed-response))
+                     (swap! state assoc :configs-error (:message parsed-response)))))}))
+   :render
+   (fn [{:keys [props state]}]
+     (let [{:keys [style make-config-link-props]} props
+           {:keys [resolved-configs configs-error]} @state]
+       (cond configs-error
+             [:div {:style {:textAlign "center" :color (:state-exception style/colors)}}
+              "Error loading configs: " configs-error]
+
+             (not resolved-configs)
+             [:div {:style {:textAlign "center" :padding "1rem"}}
+              [comps/Spinner {:text "Loading configs..."}]]
+
+             :else
+             [Table
+              {:data resolved-configs
+               :body {:empty-message "You don't have access to any published configurations for this method."
+                      :style (utils/deep-merge table-style/table-light
+                                               {:table {:backgroundColor "white"}}
+                                               style)
+                      :behavior {:reorderable-columns? false}
+                      :columns [{:id "compatible?" :initial-width 30 :resizable? false :sortable? false
+                                 :column-data :compatible?
+                                 :as-text (fn [c] (if c "Compatible" "Incompatible"))
+                                 :render (fn [c] (if c (icons/render-icon {:style {:color (:state-success style/colors)}} :done)
+                                                       (icons/render-icon {:style {:color (:state-warning style/colors)}} :warning)))}
+                                {:header "Configuration" :initial-width 400
+                                 :as-text (fn [{:keys [name namespace snapshotId]}]
+                                            (str namespace "/" name " snapshot " snapshotId))
+                                 :sort-by #(replace % [:namespace :name :snapshotId])
+                                 :render (fn [{:keys [name namespace snapshotId] :as config}]
+                                           (links/create-internal
+                                             (merge {:data-test-id (str namespace "-" name "-" snapshotId "-link")}
+                                                    (make-config-link-props config))
+                                             (style/render-name-id (str namespace "/" name) snapshotId)))}
+                                {:header "Method Snapshot" :initial-width 135 :filterable? false
+                                 :column-data #(get-in % [:payloadObject :methodRepoMethod :methodVersion])}
+                                {:header "Synopsis" :initial-width :auto
+                                 :column-data :synopsis}]}
+               :toolbar {:style {:padding 2} ;; gives room for highlight around filter field
+                         :filter-bar {:inner {:width 300}}}}])))
+   :component-did-mount
+   (fn [{:keys [this]}]
+     (this :refresh))
+   :component-did-update
+   (fn [{:keys [prev-state state]}]
+     (let [has-both? (fn [s] (and (:associated-configs s) (:compatible-configs s)))]
+       (when (and (not (has-both? prev-state))
+                  (has-both? @state))
+         (swap! state assoc :resolved-configs
+                (mapv (fn [config]
+                        (assoc config :compatible? (contains? (:compatible-configs @state) config)))
+                      (:associated-configs @state))))))})
 
 
 (defn render-config-table [{:keys [make-config-link-props configs style]}]
