0744532c2c976bb392437bb3943dd397629889bf
Matt Putnam <putnam@broadinstitute.org>
2017-11-13 14:10:56 -0500

    * Look up notebook access once per user * Various cleanup

--- src/cljs/main/broadfcui/page/workspace/notebooks/tab.cljs
+++ src/cljs/main/broadfcui/page/workspace/notebooks/tab.cljs
@@ -1,27 +1,15 @@
 (ns broadfcui.page.workspace.notebooks.tab
   (:require
    [dmohs.react :as react]
-   [broadfcui.common :as common]
-   [broadfcui.common.modal :as modal]
-   [broadfcui.components.buttons :as buttons]
    [broadfcui.page.workspace.notebooks.notebooks :refer [NotebooksContainer]]
    [broadfcui.utils :as utils]
    ))
 
 
-(def ^:private tracks-cache (atom {}))
-
 (react/defc Page
   {:refresh
-   (fn [])
-   :get-initial-state
-   (fn [{:keys [props]}]
-     {:tracks (get @tracks-cache (:workspace-id props) [])})
+   (fn [{:keys [refs]}]
+     ((@refs "container") :refresh))
    :render
-   (fn [{:keys [state this props]}]
-     [:div {}
-      [NotebooksContainer props]
-      (common/clear-both)])
-   :component-will-unmount
-   (fn [{:keys [props state]}]
-     (swap! tracks-cache assoc (:workspace-id props) (:tracks @state)))})
+   (fn [{:keys [props]}]
+     [NotebooksContainer (assoc props :ref "container")])})
