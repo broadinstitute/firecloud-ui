3bc6f94fe6c8c11876e15f720d20dc337d2157a6
Joel Thibault <thibault@broadinstitute.org>
2017-11-14 11:48:29 -0500

    remove SA keys when done

--- automation/src/test/scala/org/broadinstitute/dsde/firecloud/auth/ServiceAccountAuthToken.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/auth/ServiceAccountAuthToken.scala
@@ -7,7 +7,7 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
 import org.broadinstitute.dsde.firecloud.config.Config
 import org.broadinstitute.dsde.workbench.google.HttpGoogleIamDAO
 import org.broadinstitute.dsde.workbench.google.model.GoogleProject
-import org.broadinstitute.dsde.workbench.model.WorkbenchUserServiceAccountEmail
+import org.broadinstitute.dsde.workbench.model.{WorkbenchUserServiceAccountEmail, WorkbenchUserServiceAccountKey, WorkbenchUserServiceAccountKeyId}
 
 import scala.collection.JavaConverters._
 import scala.concurrent.{Await, ExecutionContext, ExecutionContextExecutor}
@@ -17,16 +17,23 @@ import scala.concurrent.duration._
 
 case class ServiceAccountAuthToken(saId: WorkbenchUserServiceAccountEmail) extends AuthToken {
 
-  val appName = "Swateroo"
-  val metricBaseName = "Swintegration"
+  val appName = "Swintegration"
+  val metricBaseName = appName
   lazy val system = ActorSystem()
   val ec: ExecutionContextExecutor = ExecutionContext.global
   lazy val googleIamDAO = new HttpGoogleIamDAO(Config.GCS.qaEmail, Config.GCS.pathToQAPem, appName, metricBaseName)(system, ec)
 
-  override protected def buildCredential(): GoogleCredential = {
-    val newSaKey = googleIamDAO.createServiceAccountKey(GoogleProject(Config.Projects.default), saId)
-    val privateKeyJsonString = Await.result(newSaKey, 1.minute).privateKeyData.decode.get
+  // creates a new Google private key.  Be sure to call removePrivateKey() when you are done with it!
+  private lazy val serviceAccountPrivateKey: WorkbenchUserServiceAccountKey = {
+    Await.result(googleIamDAO.createServiceAccountKey(GoogleProject(Config.Projects.default), saId), 1.minute)
+  }
 
+  def removePrivateKey(): Unit = {
+    Await.result(googleIamDAO.removeServiceAccountKey(GoogleProject(Config.Projects.default), saId, serviceAccountPrivateKey.id), 1.minute)
+  }
+
+  override protected def buildCredential(): GoogleCredential = {
+    val privateKeyJsonString = serviceAccountPrivateKey.privateKeyData.decode.get
     GoogleCredential.fromStream(new ByteArrayInputStream(privateKeyJsonString.getBytes())).createScoped(authScopes.asJava)
   }
 }
