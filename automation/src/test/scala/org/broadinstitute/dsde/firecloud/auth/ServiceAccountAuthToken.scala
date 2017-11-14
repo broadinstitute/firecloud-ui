d01fbe502bc43d03450b33f1b6981082f69be7c2
Joel Thibault <thibault@broadinstitute.org>
2017-11-14 12:44:29 -0500

    Clean up after your pets - create new Google dao

--- automation/src/test/scala/org/broadinstitute/dsde/firecloud/auth/ServiceAccountAuthToken.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/auth/ServiceAccountAuthToken.scala
@@ -2,26 +2,19 @@ package org.broadinstitute.dsde.firecloud.auth
 
 import java.io.ByteArrayInputStream
 
-import akka.actor.ActorSystem
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
 import org.broadinstitute.dsde.firecloud.config.Config
-import org.broadinstitute.dsde.workbench.google.HttpGoogleIamDAO
+import org.broadinstitute.dsde.firecloud.dao.Google.googleIamDAO
 import org.broadinstitute.dsde.workbench.google.model.GoogleProject
 import org.broadinstitute.dsde.workbench.model.{WorkbenchUserServiceAccountEmail, WorkbenchUserServiceAccountKey, WorkbenchUserServiceAccountKeyId}
 
 import scala.collection.JavaConverters._
-import scala.concurrent.{Await, ExecutionContext, ExecutionContextExecutor}
+import scala.concurrent.Await
 import scala.concurrent.duration._
 
 // Note: we are creating a new service account private key every time we call this case class
 
 case class ServiceAccountAuthToken(saId: WorkbenchUserServiceAccountEmail) extends AuthToken {
-
-  val appName = "Swintegration"
-  val metricBaseName = appName
-  lazy val system = ActorSystem()
-  val ec: ExecutionContextExecutor = ExecutionContext.global
-  lazy val googleIamDAO = new HttpGoogleIamDAO(Config.GCS.qaEmail, Config.GCS.pathToQAPem, appName, metricBaseName)(system, ec)
 
   // creates a new Google private key.  Be sure to call removePrivateKey() when you are done with it!
   private lazy val serviceAccountPrivateKey: WorkbenchUserServiceAccountKey = {
--- automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/api/sam/SamApiSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/api/sam/SamApiSpec.scala
@@ -1,35 +1,61 @@
 package org.broadinstitute.dsde.firecloud.test.api.sam
 
 import org.broadinstitute.dsde.firecloud.api.Sam
-import org.broadinstitute.dsde.firecloud.api.Sam.user.UserStatus
-import org.broadinstitute.dsde.firecloud.auth.{AuthToken, ServiceAccountAuthToken, UserAuthToken}
-import org.broadinstitute.dsde.firecloud.config.{Credentials, UserPool}
-import org.broadinstitute.dsde.firecloud.test.CleanUp
+import org.broadinstitute.dsde.firecloud.api.Sam.user.UserStatusDetails
+import org.broadinstitute.dsde.firecloud.auth.{AuthToken, ServiceAccountAuthToken}
+import org.broadinstitute.dsde.firecloud.config.{Config, Credentials, UserPool}
+import org.broadinstitute.dsde.workbench.model.{WorkbenchUserServiceAccount, WorkbenchUserServiceAccountEmail, WorkbenchUserServiceAccountName}
+import org.broadinstitute.dsde.firecloud.dao.Google.googleIamDAO
+import org.broadinstitute.dsde.workbench.google.model.GoogleProject
 import org.scalatest.{FreeSpec, Matchers}
 
-class SamApiSpec extends FreeSpec with Matchers with CleanUp {
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+class SamApiSpec extends FreeSpec with Matchers {
   val anyUser: Credentials = UserPool.chooseAnyUser
   val userAuthToken: AuthToken = anyUser.makeAuthToken()
+
+  def findPetInGoogle(userInfo: UserStatusDetails): Option[WorkbenchUserServiceAccount] = {
+    val petName = WorkbenchUserServiceAccountName(s"pet-${userInfo.userSubjectId}")
+    val find = googleIamDAO.findServiceAccount(GoogleProject(Config.Projects.default), petName)
+    Await.result(find, 1.minute)
+  }
 
   "Sam" - {
     "should give pets the same access as their owners" in {
 
       // set auth tokens explicitly to control which credentials are used
 
-      val userInfo: UserStatus = Sam.user.status()(userAuthToken)
+      val userStatus = Sam.user.status()(userAuthToken)
+
+      // ensure known state for pet (not present)
+
+      Sam.admin.deletePetServiceAccount(userStatus.userInfo.userSubjectId)(UserPool.chooseAdmin.makeAuthToken())
+      findPetInGoogle(userStatus.userInfo) shouldBe None
+
 
       val petAccountEmail = Sam.user.petServiceAccountEmail()(userAuthToken)
+      petAccountEmail.value should not be userStatus.userInfo.userEmail
+      findPetInGoogle(userStatus.userInfo).map(_.email) shouldBe Some(petAccountEmail)
+
 
       // first call should create pet.  confirm that a second call to create/retrieve gives the same results
       Sam.user.petServiceAccountEmail()(userAuthToken) shouldBe petAccountEmail
 
-      val petAuthToken = ServiceAccountAuthToken(petAccountEmail)
-      register cleanUp petAuthToken.removePrivateKey()
 
-      Sam.user.status()(petAuthToken) shouldBe userInfo
+      val petAuthToken = ServiceAccountAuthToken(petAccountEmail)
+
+      Sam.user.status()(petAuthToken) shouldBe userStatus
 
       // who is my pet -> who is my user's pet -> it's me
       Sam.user.petServiceAccountEmail()(petAuthToken) shouldBe petAccountEmail
+
+      // clean up
+
+      petAuthToken.removePrivateKey()
+      Sam.admin.deletePetServiceAccount(userStatus.userInfo.userSubjectId)(UserPool.chooseAdmin.makeAuthToken())
+      findPetInGoogle(userStatus.userInfo) shouldBe None
     }
   }
 }
