10d82d64e0e5278dc955f743c26f029589ddce96
Matt Putnam <matt.s.putnam@gmail.com>
2017-11-15 17:16:34 -0500

    GAWB-2820: Custom Data Access Instructions (#1053)
    * Custom Data Access Instructions
    
    * Made MarkdownEditor managed or unmanaged
    * Respond to 'markdown' render hint in catalog wizard
    * Render instructions if present
    
    * language: managed->controlled
    
    * fix initial slider position
    
    * fix tabs getting cut off

--- src/cljs/main/broadfcui/common/markdown.cljs
+++ src/cljs/main/broadfcui/common/markdown.cljs
@@ -32,29 +32,38 @@
 
 (react/defc MarkdownEditor
   {:get-trimmed-text
-   (fn [{:keys [state]}]
+   (fn [{:keys [state this]}]
+     (assert (not (this :-controlled?)) "Trying to read text from a controlled component")
      (:text @state))
    :get-initial-state
-   (fn [{:keys [props]}]
-     {:mode :edit
-      :text (or (:initial-text props) "")})
+   (fn [{:keys [props this]}]
+     (merge
+      {:mode :edit}
+      (when-not (this :-controlled?)
+        {:text (or (:initial-text props) "")})))
    :render
-   (fn [{:keys [props state]}]
-     (let [{:keys [mode text]} @state
+   (fn [{:keys [props state this]}]
+     (let [{:keys [mode]} @state
            tab (fn [mode-key label]
                  (let [selected? (= mode-key mode)]
-                   [:span {:style {:border style/standard-line :padding "3px 8px" :cursor "pointer"
-                                   :color (when selected? "white")
-                                   :backgroundColor ((if selected? :button-primary :background-light) style/colors)}
-                           :onClick #(swap! state assoc :mode mode-key)}
+                   [:div {:style {:display "inline-block"
+                                  :border style/standard-line :padding "3px 8px" :cursor "pointer"
+                                  :color (when selected? "white")
+                                  :backgroundColor ((if selected? :button-primary :background-light) style/colors)}
+                          :onClick #(swap! state assoc :mode mode-key)}
                     label]))
+           controlled? (this :-controlled?)
+           text (if controlled? (:value props) (:text @state))
            markdown-view [MarkdownView {:text text}]
-           text-area (style/create-text-area {:value (:text @state)
-                                              :onChange #(swap! state assoc :text (-> % .-target .-value))
+           text-area (style/create-text-area {:value text
+                                              :onChange #(let [new-value (-> % .-target .-value)]
+                                                           (if controlled?
+                                                             ((:on-change props) new-value)
+                                                             (swap! state assoc :text new-value)))
                                               :style {:width "100%" :height "100%" :minHeight 200
                                                       :resize (if (= mode :edit) "vertical" "none")}})]
        [:div {}
-        [:div {}
+        [:div {:style {:marginTop "0.5rem"}}
          (tab :edit "Edit")
          (tab :preview "Preview")
          (tab :side-by-side "Side-by-side")]
@@ -64,4 +73,7 @@
           :side-by-side [SplitPane
                          {:left text-area :overflow-left "initial"
                           :right [:div {:style {:marginLeft 2}} markdown-view]
-                          :initial-slider-position (:or (:initial-slider-position props) 500)}])]))})
+                          :initial-slider-position (or (:initial-slider-position props) 500)}])]))
+   :-controlled?
+   (fn [{:keys [props]}]
+     (contains? props :value))})
