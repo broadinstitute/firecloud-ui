672939c670dce6166baeea24a4404cced06fecc1
vkumra-broad <vkumra@broadinstitute.org>
2017-11-16 13:57:44 -0500

    Rawls Swateroo - petSA switcheroo test for rawls (#1056)
    * rawls swateroo

--- automation/src/test/scala/org/broadinstitute/dsde/firecloud/api/Rawls.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/api/Rawls.scala
@@ -19,4 +19,28 @@ object Rawls extends FireCloudClient with LazyLogging {
       deleteRequest(url + s"api/admin/billing/$projectName")
     }
   }
+
+  object workspaces {
+
+    def create(namespace: String, name: String, authDomain: Set[String] = Set.empty)
+              (implicit token: AuthToken): Unit = {
+      logger.info(s"Creating workspace: $namespace/$name authDomain: $authDomain")
+
+      val authDomainGroups = authDomain.map(a => Map("membersGroupName" -> a))
+
+      val request = Map("namespace" -> namespace, "name" -> name,
+        "attributes" -> Map.empty, "authorizationDomain" -> authDomainGroups)
+
+      postRequest(url + s"api/workspaces", request)
+    }
+
+    def delete(namespace: String, name: String)(implicit token: AuthToken): Unit = {
+      logger.info(s"Deleting workspace: $namespace/$name")
+      deleteRequest(url + s"api/workspaces/$namespace/$name")
+    }
+
+    def list()(implicit token: AuthToken):String  = {
+      parseResponse(getRequest(url + s"api/workspaces"))
+    }
+  }
 }
--- automation/src/test/scala/org/broadinstitute/dsde/firecloud/api/Sam.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/api/Sam.scala
@@ -3,15 +3,35 @@ package org.broadinstitute.dsde.firecloud.api
 import akka.http.scaladsl.model.StatusCodes
 import com.typesafe.scalalogging.LazyLogging
 import org.broadinstitute.dsde.firecloud.config.{AuthToken, Config}
+import org.broadinstitute.dsde.firecloud.api.Sam.user.UserStatusDetails
+import org.broadinstitute.dsde.firecloud.config.UserPool
+import org.broadinstitute.dsde.firecloud.dao.Google.googleIamDAO
+import org.broadinstitute.dsde.workbench.google.model.GoogleProject
+import org.broadinstitute.dsde.workbench.model.WorkbenchUserServiceAccountName
+import org.scalatest.time.{Seconds, Span}
+import org.scalatest.concurrent.ScalaFutures
+import org.broadinstitute.dsde.firecloud.auth.AuthToken
+import org.broadinstitute.dsde.firecloud.config.Config
+import org.broadinstitute.dsde.workbench.model.WorkbenchUserServiceAccountEmail
 
 /**
   * Sam API service client. This should only be used when Orchestration does
   * not provide a required endpoint. This should primarily be used for admin
   * functions.
   */
-object Sam extends FireCloudClient with LazyLogging {
+object Sam extends FireCloudClient with LazyLogging with ScalaFutures{
 
   private val url = Config.FireCloud.samApiUrl
+
+  implicit override val patienceConfig: PatienceConfig = PatienceConfig(timeout = scaled(Span(5, Seconds)))
+
+  def petName(userInfo: UserStatusDetails) = WorkbenchUserServiceAccountName(s"pet-${userInfo.userSubjectId}")
+
+  def removePet(userInfo: UserStatusDetails): Unit = {
+    Sam.admin.deletePetServiceAccount(userInfo.userSubjectId)(UserPool.chooseAdmin.makeAuthToken())
+    // TODO: why is this necessary?  GAWB-2867
+    googleIamDAO.removeServiceAccount(GoogleProject(Config.Projects.default), petName(userInfo)).futureValue
+  }
 
   object admin {
 
