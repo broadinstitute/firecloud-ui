7e71736465d47fae8d0065ce10838d3ed8f0e2f6
Adam Nichols <anichols@broadinstitute.org>
2017-12-04 14:38:07 -0500

    Disabled users do not see a banner

--- src/cljs/main/broadfcui/common/notifications.cljs
+++ src/cljs/main/broadfcui/common/notifications.cljs
@@ -110,56 +110,57 @@
      (let [{:keys [dismissed? loading? messages]} @state]
        (when-not dismissed?
          (when-let [current-trial-state (keyword (:trialState @profile/saved-user-profile))]
-           (let [{:keys [title message warning? link button]} (messages current-trial-state)]
-             (flex/box
-              {:style {:color "white"
-                       :background-color ((if warning? :state-warning :button-primary) style/colors)}}
-              (flex/box
-               {:style {:padding "1rem" :flexGrow 1
-                        :justifyContent "center" :alignItems "center"}}
-               [:div {:data-test-id "trial-banner-title"
-                      :style {:fontSize "1.5rem" :fontWeight 500 :textAlign "right"
-                              :borderRight "1px solid white"
-                              :paddingRight "1rem" :marginRight "1rem"
-                              :maxWidth 200 :flexShrink 0}}
-                title]
-               [:span {:style {:maxWidth 600 :lineHeight "1.5rem"}}
-                message
-                (when link
-                  (let [{:keys [label url]} link]
-                    (links/create-external {:href url :style {:color "white" :marginLeft "0.5rem"}} label)))]
-               (when button
-                 (let [{:keys [label url external?]} button]
-                   [:a {:data-test-id "trial-banner-button"
-                        :data-test-state (if loading? "loading" "ready")
-                        :style {:display "block"
-                                :color "white" :textDecoration "none" :fontSize "1.125rem"
-                                :fontWeight 500
-                                :border "2px solid white" :borderRadius "0.25rem"
-                                :padding "0.5rem 1rem" :marginLeft "0.5rem" :flexShrink 0}
-                        :href (if external? url (when-not loading? "javascript:;"))
-                        :onClick (when-not (or external? loading?)
-                                   (fn []
-                                     (utils/ajax-orch
-                                      "/profile/trial"
-                                      {:method :post
-                                       :on-done (fn []
-                                                  (profile/reload-user-profile
-                                                   #(swap! state dissoc :loading?)))})
-                                     (swap! state assoc :loading? true)))
-                        :target (when external? "_blank")}
-                    (if loading?
-                      (spinner {:style {:fontSize "1rem" :margin 0}})
-                      label)
-                    (when external?
-                      (icons/render-icon
-                       {:style {:margin "-0.5em -0.3em -0.5em 0.5em" :fontSize "1rem"}}
-                       :external-link))])))
-              [:button {:className "button-reset" :onClick #(swap! state assoc :dismissed? true)
-                        :style {:alignSelf "center" :fontSize "1.5rem" :padding "1rem"
-                                :color "white" :cursor "pointer"}
-                        :title "Hide for now"}
-               (icons/render-icon {} :close)]))))))
+           (when-not (= :Disabled current-trial-state) ; Disabled users do not see a banner
+             (let [{:keys [title message warning? link button]} (messages current-trial-state)]
+               (flex/box
+                {:style {:color "white"
+                         :background-color ((if warning? :state-warning :button-primary) style/colors)}}
+                (flex/box
+                 {:style {:padding "1rem" :flexGrow 1
+                          :justifyContent "center" :alignItems "center"}}
+                 [:div {:data-test-id "trial-banner-title"
+                        :style {:fontSize "1.5rem" :fontWeight 500 :textAlign "right"
+                                :borderRight "1px solid white"
+                                :paddingRight "1rem" :marginRight "1rem"
+                                :maxWidth 200 :flexShrink 0}}
+                  title]
+                 [:span {:style {:maxWidth 600 :lineHeight "1.5rem"}}
+                  message
+                  (when link
+                    (let [{:keys [label url]} link]
+                      (links/create-external {:href url :style {:color "white" :marginLeft "0.5rem"}} label)))]
+                 (when button
+                   (let [{:keys [label url external?]} button]
+                     [:a {:data-test-id "trial-banner-button"
+                          :data-test-state (if loading? "loading" "ready")
+                          :style {:display "block"
+                                  :color "white" :textDecoration "none" :fontSize "1.125rem"
+                                  :fontWeight 500
+                                  :border "2px solid white" :borderRadius "0.25rem"
+                                  :padding "0.5rem 1rem" :marginLeft "0.5rem" :flexShrink 0}
+                          :href (if external? url (when-not loading? "javascript:;"))
+                          :onClick (when-not (or external? loading?)
+                                     (fn []
+                                       (utils/ajax-orch
+                                        "/profile/trial"
+                                        {:method :post
+                                         :on-done (fn []
+                                                    (profile/reload-user-profile
+                                                     #(swap! state dissoc :loading?)))})
+                                       (swap! state assoc :loading? true)))
+                          :target (when external? "_blank")}
+                      (if loading?
+                        (spinner {:style {:fontSize "1rem" :margin 0}})
+                        label)
+                      (when external?
+                        (icons/render-icon
+                         {:style {:margin "-0.5em -0.3em -0.5em 0.5em" :fontSize "1rem"}}
+                         :external-link))])))
+                [:button {:className "button-reset" :onClick #(swap! state assoc :dismissed? true)
+                          :style {:alignSelf "center" :fontSize "1.5rem" :padding "1rem"
+                                  :color "white" :cursor "pointer"}
+                          :title "Hide for now"}
+                 (icons/render-icon {} :close)])))))))
    :component-will-mount
    (fn [{:keys [this state]}]
      (add-watch profile/saved-user-profile :trial-alerts
