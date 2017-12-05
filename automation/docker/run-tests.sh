ca11efbc5137a9ebec911107760f17313d0c509b
Brian Reilly <breilly@broadinstitute.org>
2017-12-04 20:42:29 -0500

    test

--- automation/docker/run-tests.sh
+++ automation/docker/run-tests.sh
@@ -19,7 +19,7 @@ fi
 # Defaults
 ENV=dev
 NUM_NODES=2
-TEST_ENTRYPOINT="testOnly *BillingSpec"
+TEST_ENTRYPOINT="testOnly *BillingSpec -- -z \"should be able to add a user to the billing project\""
 TEST_CONTAINER="automation-$(head /dev/urandom | env LC_CTYPE=C tr -dc a-z0-9 | head -c 8)"
 DOCKERHOST_ADDRESS=$(docker network inspect docker_default | grep Gateway | awk -F '"' '{ print $4 }')
 
