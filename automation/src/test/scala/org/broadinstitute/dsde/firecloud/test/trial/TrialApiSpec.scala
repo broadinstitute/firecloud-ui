6ceb737129c6514cbda14d8fbd2bcf37fc49a3d5
Kaan Yuksel <kyuksel@broadinstitute.org>
2017-12-20 17:32:34 -0500

    Trial api integration test shell

---
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/trial/TrialApiSpec.scala
@@ -0,0 +1,68 @@
+package org.broadinstitute.dsde.firecloud.test.trial
+
+import org.broadinstitute.dsde.firecloud.api.{Orchestration, Sam, Thurloe}
+import org.broadinstitute.dsde.firecloud.auth.AuthToken
+import org.broadinstitute.dsde.firecloud.config.{Credentials, UserPool}
+import org.broadinstitute.dsde.workbench.model._
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.time.{Seconds, Span}
+import org.scalatest.{FreeSpec, Matchers}
+
+final class TrialApiSpec extends FreeSpec with Matchers with ScalaFutures {
+  implicit override val patienceConfig: PatienceConfig = PatienceConfig(timeout = scaled(Span(5, Seconds)))
+
+  def registerAsNewUser(email: WorkbenchEmail)(implicit authToken: AuthToken): Unit = {
+    val newUserProfile = Orchestration.profile.BasicProfile (
+      firstName = "Enroller",
+      lastName = "FreeTrial",
+      title = "User",
+      contactEmail = Option(email.value),
+      institute = "Broad",
+      institutionalProgram = "DSP",
+      programLocationCity = "Cambridge",
+      programLocationState = "MA",
+      programLocationCountry = "USA",
+      pi = "Albus Dumbledore",
+      nonProfitStatus = "true"
+    )
+    Orchestration.profile.registerUser(newUserProfile)
+  }
+
+  // TODO: Factor out if continued to be used
+  def removeUser(subjectId: String): Unit = {
+    implicit val token: AuthToken = UserPool.chooseAdmin.makeAuthToken()
+    if (Sam.admin.doesUserExist(subjectId).getOrElse(false)) {
+      Sam.admin.deleteUser(subjectId)
+    }
+    Thurloe.keyValuePairs.deleteAll(subjectId)
+  }
+
+  "Free Trial" - {
+    "should be idempotent for user status cycle" in {
+
+      // Pick a temporary non-existent user
+      val tempUser: Credentials = UserPool.chooseTemp
+      val tempAuthToken: AuthToken = tempUser.makeAuthToken()
+      Sam.user.status()(tempAuthToken) shouldBe None
+
+      // Register the user
+      registerAsNewUser(WorkbenchEmail(tempUser.email))(tempAuthToken)
+
+      // Verify the user is registered
+      val tempUserInfo = Sam.user.status()(tempAuthToken).get.userInfo
+      tempUserInfo.userEmail shouldBe tempUser.email
+
+      // TODO: Enable the user
+
+      // TODO: Verify the user is enrolled with the expected record
+
+      // TODO: Have the user enrolled
+
+      // TODO: Verify the user is enrolled with the expected record
+
+      // Remove the user
+      removeUser(tempUserInfo.userSubjectId)
+      Sam.user.status()(tempAuthToken) shouldBe None
+    }
+  }
+}
