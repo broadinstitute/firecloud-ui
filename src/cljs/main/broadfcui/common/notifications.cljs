10c4c41dcfbb329d0c17943692f480e2ce0b3aca
Isaac Zarsky <zarsky@broadinstitute.org>
2018-01-03 15:06:10 -0500

    and broad eula comes from the json

--- src/cljs/main/broadfcui/common/notifications.cljs
+++ src/cljs/main/broadfcui/common/notifications.cljs
@@ -113,7 +113,7 @@
      (let [{:keys [dismissed? loading? messages error]} @state]
        (when-let [current-trial-state (keyword (:trialState @user-info/saved-user-profile))]
          (when (and (not dismissed?) (current-trial-state messages)) ; Disabled or mis-keyed users do not see a banner
-           (let [{:keys [title message warning? link button eula]} (messages current-trial-state)]
+           (let [{:keys [title message warning? link button eulas]} (messages current-trial-state)]
              (apply ;; needed until dmohs/react deals with nested seq's
               flex/box
               {:style {:color "white"
@@ -158,7 +158,7 @@
               (modals/show-modals
                state
                {:displaying-eula?
-                (this :-show-eula-modal eula)
+                (this :-show-eula-modal eulas)
                 :error
                 (modals/render-error {:text error :on-dismiss #(swap! state dissoc :error)})})))))))
    :component-will-mount
@@ -175,8 +175,9 @@
                   :on-done (fn [{:keys [get-parsed-response]}]
                              (swap! state assoc :messages (get-parsed-response)))}))
    :-show-eula-modal
-   (fn [{:keys [state]} eula]
+   (fn [{:keys [state]} eulas]
      (let [{:keys [page-2? terms-agreed? cloud-terms-agreed?]} @state
+           {:keys [broad onix]} eulas
            accept-eula (fn []
                          (utils/ajax-orch
                           "/profile/trial/userAgreement"
@@ -205,42 +206,21 @@
         {:header "Welcome to the FireCloud Free Credit Program!"
          :dismiss #(swap! state dissoc :displaying-eula? :page-2? :terms-agreed? :cloud-terms-agreed?)
          :content
-         [:div {:style {:backgroundColor "white" :padding "1rem" :maxWidth 850}}
-          (if-not page-2?
-            (let [make-li (fn [& body]
-                            [:li {:style {:paddingBottom "0.5rem"}} body])]
-              [:ol {:style {:paddingRight "1rem"}}
-               (make-li "The FireCloud Free Credits Program (\"credits\"), sponsored by Google, is administered by "
-                        (links/create-external
-                         {:href "https://www.onixnet.com/products/google-cloud/google-cloud-platform/google-app-engine"}
-                         "Onix Networking") " (\"Onix\"), a Google Cloud Premier Partner.")
-               (make-li "By opting into this program, you are authorizing FireCloud to give Onix and
-                Google access to your FireCloud user profile information. This is necessary for Onix
-                and Google to give you the free credits.")
-               (make-li "Your credits of $250 will expire December 30, 2018 or 60 days after they
-                were issued, whichever comes first.")
-               (make-li "Onix will contact you during the trial with information on options for
-               creating your own billing account to further use FireCloud once the credits expire.
-               Other billing options will be available on the FireCloud website.")
-               (make-li "FireCloud has no obligation to maintain a billing account or any data saved
-               under an account once credits are exhausted.")
-               (make-li "Credits are not redeemable for cash and are not transferable.")
-               (make-li "All use of FireCloud by researchers is subject to the "
-                        (links/create-external
-                         {:href "https://software.broadinstitute.org/firecloud/documentation/article?id=6819"}
-                         "FireCloud Terms of Use") ", which may be updated from time to time.
-                         FireCloud reserves the right to revoke credits for any activity that violates the Terms of Use.")])
-            (let [div-id (gensym "eula")]
-              [:div {:id div-id}
-               [:style {}
-                (str "#" div-id " .markdown-body strong {text-decoration: underline}\n"
-                     "#" div-id " .markdown-body ol {counter-reset: item}\n"
-                     "#" div-id " .markdown-body li:before {content: counters(item, \".\") \".\";\n"
-                     "  counter-increment: item; margin-left: -2em; position: absolute}\n"
-                     "#" div-id " .markdown-body li li:before {margin-left: -3em}\n"
-                     "#" div-id " .markdown-body li li li:before {margin-left: -4em}\n"
-                     "#" div-id " .markdown-body li {display: block}")]
-               [markdown/MarkdownView {:text eula}]
+         (let [div-id (gensym "eula")]
+           [:div {:id div-id
+                  :style {:backgroundColor "white" :padding "1rem" :maxWidth 850}}
+            [:style {}
+             (str "#" div-id " .markdown-body strong {text-decoration: underline}\n"
+                  "#" div-id " .markdown-body ol {counter-reset: item}\n"
+                  "#" div-id " .markdown-body li:before {content: counters(item, \".\") \".\";\n"
+                  "  counter-increment: item; margin-left: -2em; position: absolute}\n"
+                  "#" div-id " .markdown-body li li:before {margin-left: -3em}\n"
+                  "#" div-id " .markdown-body li li li:before {margin-left: -4em}\n"
+                  "#" div-id " .markdown-body li {display: block}")]
+            (if-not page-2?
+              [markdown/MarkdownView {:text broad}]
+              [:div {}
+               [markdown/MarkdownView {:text onix}]
                [:div {:style {:padding "1rem" :marginTop "0.5rem"
                               :border style/standard-line :background (:background-light style/colors)}}
                 [:label {:style {:marginBottom "0.5rem" :display "block"}}
@@ -251,7 +231,7 @@
                  "I agree to the Google Cloud Terms of Service."]
                 [:div {:style {:paddingLeft "1rem"}} "Google Cloud Terms of Service: "
                  (links/create-external {:href "https://cloud.google.com/terms/"}
-                                        "https://cloud.google.com/terms/")]]]))]
+                                        "https://cloud.google.com/terms/")]]])])
          :data-test-id "eula-modal"
          :button-bar (flex/box
                       {:style {:justifyContent "center" :width "100%"}}
