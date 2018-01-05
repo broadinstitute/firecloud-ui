9fc238976f0f49e4ac20d9d4446360e2a8e3ce90
Matt Bemis <mbemis@broadinstitute.org>
2018-01-05 10:18:02 -0500

    add test for changing billing project account

--- automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/billing/BillingSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/billing/BillingSpec.scala
@@ -6,6 +6,7 @@ import com.typesafe.scalalogging.LazyLogging
 import org.broadinstitute.dsde.firecloud.api.Rawls
 import org.broadinstitute.dsde.firecloud.auth.{AuthToken, UserAuthToken}
 import org.broadinstitute.dsde.firecloud.config.{Config, UserPool}
+import org.broadinstitute.dsde.firecloud.api.Google
 import org.broadinstitute.dsde.firecloud.fixture.{MethodData, SimpleMethodConfig, TestData, UserFixtures}
 import org.broadinstitute.dsde.firecloud.page.billing.BillingManagementPage
 import org.broadinstitute.dsde.firecloud.page.workspaces.methodconfigs.{WorkspaceMethodConfigDetailsPage, WorkspaceMethodConfigListPage}
@@ -24,9 +25,11 @@ class BillingSpec extends FreeSpec with WebBrowserSpec with UserFixtures with Cl
       "should be able to create a billing project" ignore withWebDriver { implicit driver =>
         val userOwner = UserPool.chooseProjectOwner
         implicit val authToken: AuthToken = userOwner.makeAuthToken()
+
+        val billingProjectName = "billing-spec-create-" + makeRandomId() // is this a unique ID?
+
         withSignIn(userOwner) { _ =>
           val billingPage = new BillingManagementPage().open
-          val billingProjectName = "billing-spec-create-" + makeRandomId() // is this a unique ID?
           logger.info(s"Creating billing project: $billingProjectName")
 
           billingPage.createBillingProject(billingProjectName, Config.Projects.billingAccount)
@@ -58,6 +61,22 @@ class BillingSpec extends FreeSpec with WebBrowserSpec with UserFixtures with Cl
             val isSuccess = billingPage.isUserInBillingProject(secondUser)
             isSuccess shouldEqual true
           }
+
+          //note that this next part of the test *could* be a different test on it's own, but we have chosen to
+          //build it into this test case because:
+          // a) having it in it's own test case would add another ~5 minutes to the test cycle
+          // b) this test would fail anyway if billing project creation failed
+
+          //should you decide to break this out into it's own test, billing project creation for it should
+          //be driven by API and not UI
+
+          val originalBillingAccount = Google.billing.getBillingProjectAccount(billingProjectName)
+          assertResult(Some(Config.Projects.billingAccountId))(originalBillingAccount)
+
+          Google.billing.removeBillingProjectAccount(billingProjectName)
+
+          val newBillingAccount = Google.billing.getBillingProjectAccount(billingProjectName)
+          assertResult(None)(newBillingAccount)
 
         }
 
