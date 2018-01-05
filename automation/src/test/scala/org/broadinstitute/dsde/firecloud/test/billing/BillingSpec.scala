95a435e194908b489538bbb1e12199c750e11673
Matt Bemis <mbemis@broadinstitute.org>
2018-01-05 10:26:35 -0500

    re-enable old test and move my test code into it

--- automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/billing/BillingSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/billing/BillingSpec.scala
@@ -22,13 +22,13 @@ class BillingSpec extends FreeSpec with WebBrowserSpec with UserFixtures with Cl
   "A user" - {
     "with a billing account" - {
       // Need to tweak background sign-in to make this test work
-      "should be able to create a billing project" ignore withWebDriver { implicit driver =>
+      "should be able to create a billing project" in withWebDriver { implicit driver =>
         val userOwner = UserPool.chooseProjectOwner
         implicit val authToken: AuthToken = userOwner.makeAuthToken()
 
         val billingProjectName = "billing-spec-create-" + makeRandomId() // is this a unique ID?
 
-        withSignIn(userOwner) { _ =>
+        withSignInReal(userOwner) { _ =>
           val billingPage = new BillingManagementPage().open
           logger.info(s"Creating billing project: $billingProjectName")
 
@@ -38,6 +38,22 @@ class BillingSpec extends FreeSpec with WebBrowserSpec with UserFixtures with Cl
           val status = billingPage.waitForCreateCompleted(billingProjectName)
           withClue(s"Creating billing project: $billingProjectName") { status shouldEqual "success" }
         }
+
+        //note that this next part of the test *could* be a different test on it's own, but we have chosen to
+        //build it into this test case because:
+        // a) having it in it's own test case would add another ~5 minutes to the test cycle
+        // b) this test would fail anyway if billing project creation failed
+
+        //should you decide to break this out into it's own test, billing project creation for it should
+        //be driven by API and not UI
+
+        val originalBillingAccount = Google.billing.getBillingProjectAccount(billingProjectName)
+        assertResult(Some(Config.Projects.billingAccountId))(originalBillingAccount)
+
+        Google.billing.removeBillingProjectAccount(billingProjectName)
+
+        val newBillingAccount = Google.billing.getBillingProjectAccount(billingProjectName)
+        assertResult(None)(newBillingAccount)
       }
 
       "with a new billing project" - {
@@ -61,22 +77,6 @@ class BillingSpec extends FreeSpec with WebBrowserSpec with UserFixtures with Cl
             val isSuccess = billingPage.isUserInBillingProject(secondUser)
             isSuccess shouldEqual true
           }
-
-          //note that this next part of the test *could* be a different test on it's own, but we have chosen to
-          //build it into this test case because:
-          // a) having it in it's own test case would add another ~5 minutes to the test cycle
-          // b) this test would fail anyway if billing project creation failed
-
-          //should you decide to break this out into it's own test, billing project creation for it should
-          //be driven by API and not UI
-
-          val originalBillingAccount = Google.billing.getBillingProjectAccount(billingProjectName)
-          assertResult(Some(Config.Projects.billingAccountId))(originalBillingAccount)
-
-          Google.billing.removeBillingProjectAccount(billingProjectName)
-
-          val newBillingAccount = Google.billing.getBillingProjectAccount(billingProjectName)
-          assertResult(None)(newBillingAccount)
 
         }
 
