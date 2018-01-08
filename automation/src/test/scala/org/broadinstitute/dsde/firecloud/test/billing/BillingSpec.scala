2028d7841990b526cbb6d5b9c5f6049533da2376
Matt Bemis <MatthewBemis@users.noreply.github.com>
2018-01-08 12:57:49 -0500

    GAWB-2877: Test changing billing accounts for billing project owners (#1107)
    * add test for changing billing project account

--- automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/billing/BillingSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/billing/BillingSpec.scala
@@ -6,6 +6,7 @@ import com.typesafe.scalalogging.LazyLogging
 import org.broadinstitute.dsde.firecloud.api.Rawls
 import org.broadinstitute.dsde.firecloud.auth.{AuthToken, UserAuthToken}
 import org.broadinstitute.dsde.firecloud.config.{Config, UserPool}
+import org.broadinstitute.dsde.firecloud.api.Google
 import org.broadinstitute.dsde.firecloud.fixture.{MethodData, SimpleMethodConfig, TestData, UserFixtures}
 import org.broadinstitute.dsde.firecloud.page.billing.BillingManagementPage
 import org.broadinstitute.dsde.firecloud.page.workspaces.methodconfigs.{WorkspaceMethodConfigDetailsPage, WorkspaceMethodConfigListPage}
@@ -21,12 +22,14 @@ class BillingSpec extends FreeSpec with WebBrowserSpec with UserFixtures with Cl
   "A user" - {
     "with a billing account" - {
       // Need to tweak background sign-in to make this test work
-      "should be able to create a billing project" ignore withWebDriver { implicit driver =>
+      "should be able to create a billing project" in withWebDriver { implicit driver =>
         val userOwner = UserPool.chooseProjectOwner
         implicit val authToken: AuthToken = userOwner.makeAuthToken()
-        withSignIn(userOwner) { _ =>
+
+        val billingProjectName = "billing-spec-create-" + makeRandomId() // is this a unique ID?
+
+        withSignInReal(userOwner) { _ =>
           val billingPage = new BillingManagementPage().open
-          val billingProjectName = "billing-spec-create-" + makeRandomId() // is this a unique ID?
           logger.info(s"Creating billing project: $billingProjectName")
 
           billingPage.createBillingProject(billingProjectName, Config.Projects.billingAccount)
@@ -35,6 +38,22 @@ class BillingSpec extends FreeSpec with WebBrowserSpec with UserFixtures with Cl
           val status = billingPage.waitForCreateCompleted(billingProjectName)
           withClue(s"Creating billing project: $billingProjectName") { status shouldEqual "success" }
         }
+
+        //note that this next part of the test *could* be a different test on it's own, but we have chosen to
+        //build it into this test case because:
+        // a) having it in it's own test case would add another ~5 minutes to the test cycle
+        // b) this test would fail anyway if billing project creation failed
+
+        //should you decide to break this out into it's own test, billing project creation for it should
+        //be driven by API and not UI
+
+        val originalBillingAccount = Google.billing.getBillingProjectAccount(billingProjectName)
+        originalBillingAccount shouldBe Some(Config.Projects.billingAccountId)
+
+        Google.billing.removeBillingProjectAccount(billingProjectName)
+
+        val newBillingAccount = Google.billing.getBillingProjectAccount(billingProjectName)
+        newBillingAccount shouldBe None
       }
 
       "with a new billing project" - {
