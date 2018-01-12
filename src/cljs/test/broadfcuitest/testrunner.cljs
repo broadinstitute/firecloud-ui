90552255da67d8b3da98c020c3c33f05828dee1b
Adam Nichols <anichols@broadinstitute.org>
2018-01-12 18:00:20 -0500

    Cleanup

--- src/cljs/test/broadfcuitest/testrunner.cljs
+++ src/cljs/test/broadfcuitest/testrunner.cljs
@@ -11,9 +11,9 @@
    ))
 
 (defn ^:export run-all-tests []
-  ;(cljs.test/run-tests 'broadfcuitest.config)
-  ;(cljs.test/run-tests 'broadfcuitest.duration)
-  ;(cljs.test/run-tests 'broadfcuitest.style)
-  ;(cljs.test/run-tests 'broadfcuitest.components.buttons)
+  (cljs.test/run-tests 'broadfcuitest.config)
+  (cljs.test/run-tests 'broadfcuitest.duration)
+  (cljs.test/run-tests 'broadfcuitest.style)
+  (cljs.test/run-tests 'broadfcuitest.components.buttons)
   (cljs.test/run-tests 'broadfcuitest.components.research-purpose)
   )
