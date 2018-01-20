7a1804459c3d4eea217ea3be8ca650afed34cf15
Andrew Jang <andew7i@gmail.com>
2018-01-19 22:38:43 -0500

    put everything into launchSubmission function

--- automation/alpha-perf-test.sh
+++ automation/alpha-perf-test.sh
@@ -11,36 +11,46 @@ echo "FOR EASE OF USE: IF YOU RUN THIS SCRIPT AND A BROWSER OPENS WITH GOOGLE LO
 # gcloud auth login elvin.testerson@gmail.com
 # gcloud auth login test.firec@gmail.com
 
+launchSubmission() {
+    user=$1
+    namespace=$2
+    name=$3
+    methodConfigurationNamespace=$4
+    methodConfigurationName=$5
+    entityType=$6
+    entityName=$7
+    useCallCache=$8
+    expression=$9  #optional
 
-echo "Logging in with dominique.testerson@gmail.com"
-gcloud auth login dominique.testerson@gmail.com
-ACCESS_TOKEN=$(gcloud auth print-access-token)
-curl 'https://firecloud-orchestration.dsde-alpha.broadinstitute.org/api/workspaces/perf-test-a/Perf-test-A-workspace/submissions' -H 'origin: https://firecloud.dsde-alpha.broadinstitute.org' -H 'accept-encoding: gzip, deflate, br' -H "authorization: Bearer $ACCESS_TOKEN" -H 'content-type: application/json' --data-binary '{"methodConfigurationNamespace":"qamethods","methodConfigurationName":"sleep1hr_echo_strings","entityType":"sample_set","entityName":"sample_set6k","useCallCache":false,"expression":"this.samples"}' --compressed
+    echo "
+    Launching submission for:
+        user=$1
+        namespace=$2
+        name=$3
+        methodConfigurationNamespace=$4
+        methodConfigurationName=$5
+        entityType=$6
+        entityName=$7
+        useCallCache=$8
+        expression=$9
+    "
 
-echo "Logging in with gary.testerson1@gmail.com"
-gcloud auth login gary.testerson1@gmail.com
-ACCESS_TOKEN=$(gcloud auth print-access-token)
-curl 'https://firecloud-orchestration.dsde-alpha.broadinstitute.org/api/workspaces/perf-test-b/Perf-Test-B-W/submissions' -H 'origin: https://firecloud.dsde-alpha.broadinstitute.org' -H 'accept-encoding: gzip, deflate, br' -H "authorization: Bearer $ACCESS_TOKEN" -H 'content-type: application/json' --data-binary '{"methodConfigurationNamespace":"alex_methods","methodConfigurationName":"sleep_echo_strings","entityType":"sample_set","entityName":"sample_set6k","useCallCache":false,"expression":"this.samples"}' --compressed
+    gcloud auth login $user
+    ACCESS_TOKEN=$(gcloud auth print-access-token)
 
-echo "Logging in with felicity.testerson@gmail.com"
-gcloud auth login felicity.testerson@gmail.com
-ACCESS_TOKEN=$(gcloud auth print-access-token)
-curl 'https://firecloud-orchestration.dsde-alpha.broadinstitute.org/api/workspaces/perf-test-d/Perf-Test-D-W_copy/submissions' -H 'origin: https://firecloud.dsde-alpha.broadinstitute.org' -H 'accept-encoding: gzip, deflate, br' -H "authorization: Bearer $ACCESS_TOKEN" -H 'content-type: application/json' --data-binary '{"methodConfigurationNamespace":"alex_methods","methodConfigurationName":"sleep_echo_strings","entityType":"sample_set","entityName":"sample_set6k","useCallCache":false,"expression":"this.samples"}' --compressed
+    #check if $9 is set for 'expression'
+    if [ -z ${9+x} ] ; then
+        curl "https://firecloud-orchestration.dsde-alpha.broadinstitute.org/api/workspaces/$namespace/$name/submissions" -H "origin: https://firecloud.dsde-alpha.broadinstitute.org" -H "accept-encoding: gzip, deflate, br" -H "authorization: Bearer $ACCESS_TOKEN" -H "content-type: application/json" --data-binary "{\"methodConfigurationNamespace\":\"$methodConfigurationNamespace\",\"methodConfigurationName\":\"$methodConfigurationName\",\"entityType\":\"$entityType\",\"entityName\":\"$entityName\",\"useCallCache\":$useCallCache}" --compressed
+    else
+        curl "https://firecloud-orchestration.dsde-alpha.broadinstitute.org/api/workspaces/$namespace/$name/submissions" -H "origin: https://firecloud.dsde-alpha.broadinstitute.org" -H "accept-encoding: gzip, deflate, br" -H "authorization: Bearer $ACCESS_TOKEN" -H "content-type: application/json" --data-binary "{\"methodConfigurationNamespace\":\"$methodConfigurationNamespace\",\"methodConfigurationName\":\"$methodConfigurationName\",\"entityType\":\"$entityType\",\"entityName\":\"$entityName\",\"useCallCache\":$useCallCache,\"expression\":\"$expression\"}" --compressed
+    fi
+}
 
-echo "Logging in with frida.testerson@gmail.com"
-gcloud auth login frida.testerson@gmail.com
-ACCESS_TOKEN=$(gcloud auth print-access-token)
-curl 'https://firecloud-orchestration.dsde-alpha.broadinstitute.org/api/workspaces/perf-test-e/Perf-Test_E_W/submissions' -H 'origin: https://firecloud.dsde-alpha.broadinstitute.org' -H 'accept-encoding: gzip, deflate, br' -H "authorization: Bearer $ACCESS_TOKEN" -H 'content-type: application/json' --data-binary '{"methodConfigurationNamespace":"qamethods","methodConfigurationName":"sleep1hr_echo_strings","entityType":"sample_set","entityName":"sample_set6k","useCallCache":false,"expression":"this.samples"}' --compressed
+launchSubmission dominique.testerson@gmail.com perf-test-a Perf-test-A-workspace qamethods sleep1hr_echo_strings sample_set sample_set6k false "this.samples"
+launchSubmission gary.testerson1@gmail.com perf-test-b Perf-Test-B-W alex_methods sleep_echo_strings sample_set sample_set6k false "this.samples"
+launchSubmission felicity.testerson@gmail.com perf-test-d Perf-Test-D-W_copy alex_methods sleep_echo_strings sample_set sample_set6k false "this.samples"
+launchSubmission frida.testerson@gmail.com perf-test-e Perf-Test_E_W qamethods sleep1hr_echo_strings sample_set sample_set6k false "this.samples"
+launchSubmission elvin.testerson@gmail.com aa-test041417 Perf-Test-G-W alex_methods sleep_echo_strings sample_set sample_set6k false "this.samples"
+launchSubmission test.firec@gmail.com aa-test-042717a test-042717 anuMethods callCacheWDL participant subject_HCC1143 true
 
-echo "Logging in with elvin.testerson@gmail.com"
-gcloud auth login elvin.testerson@gmail.com
-ACCESS_TOKEN=$(gcloud auth print-access-token)
-curl 'https://firecloud-orchestration.dsde-alpha.broadinstitute.org/api/workspaces/aa-test041417/Perf-Test-G-W/submissions' -H 'origin: https://firecloud.dsde-alpha.broadinstitute.org' -H 'accept-encoding: gzip, deflate, br' -H "authorization: Bearer $ACCESS_TOKEN" -H 'content-type: application/json' --data-binary '{"methodConfigurationNamespace":"alex_methods","methodConfigurationName":"sleep_echo_strings","entityType":"sample_set","entityName":"sample_set6k","useCallCache":false,"expression":"this.samples"}' --compressed
-
-echo "Logging in with test.firec@gmail.com"
-gcloud auth login test.firec@gmail.com
-ACCESS_TOKEN=$(gcloud auth print-access-token)
-curl 'https://firecloud-orchestration.dsde-alpha.broadinstitute.org/api/workspaces/aa-test-042717a/test-042717/submissions' -H 'origin: https://firecloud.dsde-alpha.broadinstitute.org' -H 'accept-encoding: gzip, deflate, br' -H "authorization: Bearer $ACCESS_TOKEN" -H 'content-type: application/json' --data-binary '{"methodConfigurationNamespace":"anuMethods","methodConfigurationName":"callCacheWDL","entityType":"participant","entityName":"subject_HCC1143","useCallCache":true}' --compressed
-
-
-echo "Done"
+printf "\nDone"
