df7dc9aa061f0e8db5a0f6123ca21ece0e974176
Brian Reilly <breilly@broadinstitute.org>
2018-01-26 16:11:23 -0500

    Resolved logical merge conflicts.

--- automation/src/test/scala/org/broadinstitute/dsde/firecloud/page/library/DataLibraryPage.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/page/library/DataLibraryPage.scala
@@ -5,7 +5,7 @@ import org.broadinstitute.dsde.firecloud.component._
 import org.broadinstitute.dsde.firecloud.component.Component._
 import org.broadinstitute.dsde.workbench.config.Config
 import org.broadinstitute.dsde.firecloud.page.{BaseFireCloudPage, PageUtil}
-import org.broadinstitute.dsde.firecloud.util.Retry.retry
+import org.broadinstitute.dsde.workbench.service.util.Retry.retry
 import org.openqa.selenium.WebDriver
 import org.scalatest.selenium.Page
 
