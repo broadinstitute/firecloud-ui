e951d06e2aab0f55d0a9ba2f412b360ad89601ec
aweng98 <bweng@broadinstitute.org>
2018-01-31 22:33:31 -0500

    new modification

--- automation/src/test/scala/org/broadinstitute/dsde/firecloud/fixture/WorkspaceData.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/fixture/WorkspaceData.scala
@@ -5,4 +5,12 @@ object WorkspaceData {
   val tags = Map(
     "tag:tags" -> Seq("testing","diabetes")
   )
+
+  val tagA = Map(
+    "tag:tags" -> Seq("aTag", "cTag")
+  )
+
+  val tagB = Map(
+    "tag:tags" -> Seq("bTag")
+  )
 }
