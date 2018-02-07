e28f170b42e214d3566342d726e094a9ae88d869
Matt Bemis <mbemis@broadinstitute.org>
2018-02-07 16:34:10 -0500

    update dep again

--- automation/project/Dependencies.scala
+++ automation/project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val akkaV = "2.5.7"
   val akkaHttpV = "10.0.10"
 
-  val serviceTestV = "0.1-4f876ad-SNAP"
+  val serviceTestV = "0.1-a9f61a3-SNAP"
 
   val workbenchModelV  = "0.8-d97f551"
   val workbenchModel: ModuleID = "org.broadinstitute.dsde.workbench" %% "workbench-model" % workbenchModelV
