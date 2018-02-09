c6a331db103af3f83709ddd3cda991fcf66c6b94
ansingh7115 <singh.an@husky.neu.edu>
2018-02-09 15:56:13 -0500

    go back to using .open

--- automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/workspace/WorkspaceSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/workspace/WorkspaceSpec.scala
@@ -471,8 +471,7 @@ class WorkspaceSpec extends FreeSpec with WebBrowserSpec with WorkspaceFixtures 
         withSignIn(user) { listPage =>
           val detailPage = listPage.enterWorkspace(billingProject, workspaceName)
           //go directly to notebooks page
-          val notebooksTab = new WorkspaceNotebooksPage(billingProject, workspaceName)
-          go to notebooksTab
+          val notebooksTab = new WorkspaceNotebooksPage(billingProject, workspaceName).open
           notebooksTab.checkUnauthorized
         }
       }
