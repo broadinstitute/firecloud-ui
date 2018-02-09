ad20f0c4f93ff41813de9b2951dffbb59499576f
Gary Dlugy <garyd@broadinstitute.org>
2018-02-09 10:53:26 -0500

    adding stashed

--- automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/library/DataLibrarySpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/library/DataLibrarySpec.scala
@@ -51,4 +51,32 @@ class DataLibrarySpec extends FreeSpec with WebBrowserSpec with UserFixtures wit
       }
     }
   }
+
+  "For a dataset with facets" in withWebDriver { implicit driver =>
+    val curatorUser = UserPool.chooseCurator
+    implicit val authToken: AuthToken = curatorUser.makeAuthToken()
+    withWorkspace(namespace, "Facets", attributes = Some(WorkspaceData.tags)) { wsName =>
+      withCleanUp {
+        val data = LibraryData.metadataBasic + ("library:datasetName" -> wsName,
+                                                "library:indication" -> s"$wsName+1",
+                                                "library:datatype" -> Seq(s"$wsName+2"),
+                                                "library:projectName" -> s"$wsName+3",
+                                                "library:primaryDiseaseSite" -> s"$wsName+4",
+                                                "library:dataUseRestriction" -> s"$wsName+5")
+        api.library.setLibraryAttributes(namespace, wsName, data)
+        register cleanUp api.library.unpublishWorkspace(namespace, wsName)
+        api.library.publishWorkspace(namespace, wsName)
+        withSignIn(curatorUser) { _ =>
+          val page = new DataLibraryPage().open
+          page.hasDataset(wsName) shouldBe true
+          //verifying that facets are what I expect
+          val expected = Map("Experimental Strategy" -> s"$wsName+2")
+          expected.foreach { case (title, item) =>
+            page.isFacetCombinatorVisible(title, item) shouldBe true
+          }
+
+        }
+      }
+    }
+  }
 }
