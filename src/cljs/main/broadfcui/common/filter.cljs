c6813030be298655dc8c93caa9716d13902b1464
Gary Dlugy <garyd@broadinstitute.org>
2018-02-12 16:49:51 -0500

    ticket DSDEQA-168

--- src/cljs/main/broadfcui/common/filter.cljs
+++ src/cljs/main/broadfcui/common/filter.cljs
@@ -17,12 +17,12 @@
 
 
 (defn section [{:keys [title on-clear content]}]
-  [:div {}
+  [:div {:data-test-id (str title "-facet_section")}
    (when (or title on-clear)
      (flex/box
       {:style {:marginBottom "0.5rem" :alignItems "baseline"}}
       (when title
-        [:div {:style {:fontWeight "bold"}} title])
+        [:div {:data-test-id (str title "-title") :style {:fontWeight "bold"}} title])
       flex/spring
       (when on-clear
         [:div {:style {:fontSize "80%"}}
@@ -36,7 +36,8 @@
            [:div {:style {:display "flex" :paddingTop 5}}
             [:label {:style {:flex "1 1 auto"
                              :textOverflow "ellipsis" :overflow "hidden" :whiteSpace "nowrap"}
-                     :title rendered}
+                     :title rendered
+                     :data-test-id (str rendered "-item")}
              [:input {:type "checkbox"
                       :checked (contains? checked-items item)
                       :onChange #(on-change item (.. % -target -checked))}]
