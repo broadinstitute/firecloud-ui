49d2072da381eedfe340de9bf3a362607741ebaf
Isaac Zarsky <zarsky@broadinstitute.org>
2018-02-23 22:09:47 -0500

    all kinds of redesigning

--- src/cljs/main/broadfcui/auth.cljs
+++ src/cljs/main/broadfcui/auth.cljs
@@ -43,7 +43,7 @@
                     :fontSize (when (= (:context props) :logged-out) "88%")}}
       (when (= (:context props) :policy-page)
         (list
-         [:h3 {:style {:borderBottom style/dark-line :paddingBottom "0.5rem"}} "FireCloud Privacy Policy"]
+         [:h4 {} "FireCloud Privacy Policy"]
          [:p {}
           "The following Privacy Policy discloses our information gathering and dissemination
            practices for the Broad Institute FireCloud application accessed via the website "
@@ -118,7 +118,7 @@
           (links/create-external {:href (config/forum-url)} "User Forum")
           "."]
          [:hr]))
-      [:h3 {:style {:borderBottom style/dark-line :paddingBottom "0.5rem"}} "WARNING NOTICE"]
+      [:h4 {} "WARNING NOTICE"]
       [:p {}
        "You are accessing a US Government web site which may contain information that must be
         protected under the US Privacy Act or other sensitive information and is intended for
@@ -133,9 +133,9 @@
         communications or data transiting or stored on related to this website and is advised
         that if such monitoring reveals possible evidence of criminal activity, NIH may provide
         that evidence to law enforcement officials."]
-      [:h4 {} "WARNING NOTICE (when accessing controlled data)"]
+      [:h4 {} "WARNING NOTICE (when accessing TCGA controlled data)"]
       [:p {:style {:fontWeight "bold"}}
-       "You are reminded that when accessing controlled access information you are bound by the
+       "You are reminded that when accessing TCGA controlled access information you are bound by the
         dbGaP TCGA "
        (links/create-external {:href "http://cancergenome.nih.gov/pdfs/Data_Use_Certv082014"}
          "DATA USE CERTIFICATION AGREEMENT (DUCA)")
@@ -145,7 +145,7 @@
   {:render
    (fn []
      [:div {:style {:padding "1rem" :margin -20 :marginTop "1rem"
-                    :backgroundColor (:background-light style/colors)}}
+                    :backgroundColor "white"}}
       [Policy {:context :policy-page}]])})
 
 (react/defc LoggedOut
@@ -156,18 +156,18 @@
        ;; than removing it from the page.
        [:div {:style {:display (when (:hidden? props) "none") :marginTop "2rem"}}
         [:div {:style {:margin "0 auto" :maxWidth 716}}
-         [:div {:style (when-not import-page? {:display "none"})}
-          (external-importer/render-logged-out-import-tutorial #(this :-handle-sign-in-click))]
-         [:div {:style (when import-page? {:display "none"})}
-          [:div {:style {:margin "0 auto" :width 120}
-                 :className "g-signin2" :data-theme "dark" :data-width 120 :data-height 40
-                 :onClick #(this :-handle-sign-in-click)}]
-          [:div {:style {:margin "2rem 0"}}
-           [:div {} [:b {} "New user? FireCloud requires a Google account."]]
-           [:p {}
-            "Please use the \"Sign In\" button above to sign-in with your Google Account.
-             Once you have successfully signed-in with Google, you will be taken to the FireCloud
-             registration page."]]]]
+         [:h1 {:style {:marginBottom "0.3rem" :fontWeight 400}} (if import-page? external-importer/import-title "New User?")]
+         [:div {:style {:marginBottom "1.5rem"}} (if import-page? external-importer/import-subtitle "FireCloud requires a Google account.")]
+         [:div {:style {:display "flex"}}
+          [:div {:style {:paddingRight "2rem" :borderRight style/dark-line}}
+           (if import-page? external-importer/import-tutorial
+                            [:div {:style {:lineHeight "130%"}} "Need to create a FireCloud account? FireCloud uses your Google account. Once you have signed in and completed the user profile registration step, you can start using FireCloud."
+                             (links/create-external {:style {:display "block" :marginTop "0.3rem"}
+                                                     :href "https://software.broadinstitute.org/firecloud/documentation/article?id=9846"}
+                               "Learn how to create a Google account with any email address.")])]
+          [:div {:style {:width 180 :paddingLeft "2rem" :alignSelf "center"}
+                 :className "g-signin2" :data-theme "dark" :data-width 180 :data-height 40 :data-longtitle true
+                 :onClick #(this :-handle-sign-in-click)}]]]
         [Policy {:context :logged-out}]]))
    :component-did-mount
    (fn [{:keys [props locals]}]
--- src/cljs/main/broadfcui/page/external_importer.cljs
+++ src/cljs/main/broadfcui/page/external_importer.cljs
@@ -8,50 +8,39 @@
    [broadfcui.utils :as utils]
    ))
 
-(defn render-logged-out-import-tutorial [sign-in-handler]
+(def import-title "Import Workflow to FireCloud")
+(def import-subtitle "In order to import a workflow into FireCloud, you'll need two things:")
+
+(def import-tutorial
   (let [create-number-label
         (fn [number]
-          [:div {:style {:display "inline-block" :flexShrink 0
+          [:div {:style {:flexShrink 0 :height "2rem" :width "2rem" :borderRadius "50%"
                          :background-color (:button-primary style/colors) :color "white"
-                         :fontSize "1.25rem" :textAlign "center" :lineHeight "2rem"
-                         :height "2rem" :width "2rem" :borderRadius "50%"}}
+                         :fontSize "1.25rem" :textAlign "center" :lineHeight "2rem"}}
            number])]
-    [:div {}
-     [:h1 {:style {:marginBottom "0.3rem"}} "Import Workflow to FireCloud"]
-     "In order to import a workflow into FireCloud, you'll need two things:"
-     [:div {:style {:marginTop "1.5rem"}}
-      [:div {:style {:width "50%" :display "inline-flex"}}
-       (create-number-label 1)
-       [:div {:style {:verticalAlign "top" :margin "0 0.5rem"}}
-        [:h2 {:style {:margin "0 0 0.3rem"}} "Google Billing Project"]
-        "To pay any storage or compute costs."
-        [:p {:style {:fontSize "80%"}}
-         "If you have write access to an existing workspace, this step is not required. Otherwise, "
-         (links/create-external
-          {:href (config/billing-account-guide-url)}
-          "click here for instructions.")]]]
-      [:div {:style {:display "inline-flex" :width "50%"}}
-       (create-number-label 2)
-       [:div {:style {:verticalAlign "top" :marginLeft "0.5rem"}}
-        [:h2 {:style {:margin "0 0 0.3rem"}} "FireCloud Account"]
-        "To maintain control over your data."]]]
-     [:div {:style {:marginTop "2rem" :borderTop style/dark-line}}
-      [:div {:style {:display "inline-block"
-                     :borderRight style/dark-line :paddingRight "1rem"
-                     :width "50%" :boxSizing "border-box"}}
-       [:h3 {} "Need to create a FireCloud Account?"]
-       [:p {:style {:lineHeight "130%"}}
-        "FireCloud requires a Google account. Once you have signed in, you will be taken to the
-        FireCloud registration page."]
-       [:div {:style {:width 180}
-              :className "g-signin2"
-              :data-theme "dark" :data-longtitle true :data-height 40 :data-width 180
-              :onClick sign-in-handler}]]
-      [:div {:style {:display "inline-block" :verticalAlign "top" :paddingLeft "1rem"}}
-       [:h3 {} "Already have a FireCloud account?"]
-       [:div {:style {:width 120}
-              :className "g-signin2" :data-theme "light" :data-height 40 :data-width 120
-              :onClick sign-in-handler}]]]]))
+    [:div {:style {:marginTop "1.5rem"}}
+     [:div {:style {:display "flex"}}
+      (create-number-label 1)
+      [:div {:style {:margin "0 0.5rem"}}
+       [:h2 {:style {:margin "0 0 0.3rem"}} "Google Billing Project"]
+       "To pay any storage or compute costs"
+       [:p {:style {:fontSize "80%"}}
+        "Don’t know if you have one? If you own or have 'write & compute' access to an existing workspace, that means you have access to a billing project and you’re all set."
+        (links/create-external
+         {:style {:display "block" :marginTop "0.3rem"}
+          :href (config/billing-account-guide-url)}
+         "Click here for instructions on how to set up a new billing project.")]]]
+     [:div {:style {:display "flex"}}
+      (create-number-label 2)
+      [:div {:style {:marginLeft "0.5rem"}}
+       [:h2 {:style {:margin "0 0 0.3rem"}} "FireCloud Account"]
+       "To maintain control over your data."
+       [:p {:style {:fontSize "80%"}}
+        "Need to create a FireCloud account? FireCloud uses your Google account. Once you have signed in and completed the user profile registration step, you can start using FireCloud."
+        (links/create-external {:style {:display "block" :marginTop "0.3rem"}
+                                :href "https://software.broadinstitute.org/firecloud/documentation/article?id=9846"}
+          "Learn how to create a Google account with any email address.")
+        ]]]]))
 
 (react/defc Importer
   {:render
