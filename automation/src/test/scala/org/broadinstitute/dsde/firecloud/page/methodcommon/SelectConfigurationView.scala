8e175f1f27eaad3dcf1cf981ad0a96d17a7eddca
Matt Putnam <matt.s.putnam@gmail.com>
2018-02-27 13:25:03 -0500

    GAWB-3102: create ws when importing method (#1192)

---
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/page/methodcommon/SelectConfigurationView.scala
@@ -0,0 +1,28 @@
+package org.broadinstitute.dsde.firecloud.page.methodcommon
+
+import org.broadinstitute.dsde.firecloud.FireCloudView
+import org.broadinstitute.dsde.firecloud.component.Component._
+import org.broadinstitute.dsde.firecloud.component._
+import org.openqa.selenium.WebDriver
+
+class SelectConfigurationView(importing: Boolean)(implicit webDriver: WebDriver) extends FireCloudView {
+  private val configTable = Table("config-table")
+  private val useBlankButton = Button("use-blank-configuration-button")
+  private val useSelectedButton = Button("use-selected-configuration-button")
+  private def configLink(namespace: String, name: String, snapshotId: Int) = Link(s"$namespace-$name-$snapshotId-link")
+
+  override def awaitReady(): Unit = configTable.awaitReady()
+
+  def useBlankConfiguration(): ConfirmMethodImportExportView = {
+    useBlankButton.doClick()
+    await ready new ConfirmMethodImportExportView(importing)
+  }
+
+  def selectConfiguration(namespace: String, name: String, snapshotId: Int): ConfirmMethodImportExportView = {
+    configTable.filter(name)
+    configLink(namespace, name, snapshotId).doClick()
+    useSelectedButton.awaitEnabled()
+    useSelectedButton.doClick()
+    await ready new ConfirmMethodImportExportView(importing)
+  }
+}
--- src/cljs/main/broadfcui/components/workspace_selector.cljs
+++ src/cljs/main/broadfcui/components/workspace_selector.cljs
@@ -2,41 +2,66 @@
   (:require
    [dmohs.react :as react]
    [clojure.string :as string]
+   [broadfcui.common :as common]
    [broadfcui.common.style :as style]
    [broadfcui.components.spinner :refer [spinner]]
    [broadfcui.endpoints :as endpoints]
+   [broadfcui.page.workspace.create :as ws-create]
    [broadfcui.utils :as utils]
    ))
 
 
 (react/defc WorkspaceSelector
-  {:get-default-props
+  {:validate
+   (fn [{:keys [state locals]}]
+     (let [{:keys [selected-index]} @state]
+       (case selected-index
+         0 ["No workspace selected"]
+         1 ((:new-workspace-form @locals) :validate)
+         nil)))
+   :get-selected-workspace
+   (fn [{:keys [state locals]}]
+     (let [{:keys [workspaces selected-index]} @state]
+       (case selected-index
+         0 {:error "No workspace selected"}
+         1 {:new-workspace ((:new-workspace-form @locals) :get-field-values)}
+         {:existing-workspace (nth workspaces (- selected-index 2))})))
+   :get-default-props
    (fn []
      {:filter identity
       :sort-by (comp (partial mapv string/lower-case)
                      (juxt :namespace :name)
                      :workspace)})
+   :get-initial-state
+   (fn []
+     {:selected-index 0})
    :render
    (fn [{:keys [props state locals]}]
-     (let [{:keys [workspaces]} @state
-           {:keys [on-select style]} props]
-       (assert on-select ":on-select is required for WorkspaceSelector")
+     (let [{:keys [workspaces selected-index]} @state
+           {:keys [style]} props]
        (if-not workspaces
          (spinner "Loading workspaces...")
-         (style/create-select
-           {:defaultValue ""
-            :ref (fn [elem]
-                   ;; doing this manually because the ref handler thingy calls :did-mount overzealously
-                   (when (and elem (not (:select @locals)))
-                     (swap! locals assoc :select elem)
-                     (.on (.select2 (js/$ elem)) "select2:select"
-                          (fn [event]
-                            (let [selected (nth workspaces (js/parseInt (.-value (.-target event))))]
-                              (on-select selected))))
-                     (on-select (first workspaces))))
-            :style (merge {:width 500} style)}
-           (map (fn [ws] (string/join "/" (replace (:workspace ws) [:namespace :name])))
-                workspaces)))))
+         [:div {:data-test-id "workspace-selector"}
+          (style/create-select
+            {:data-test-id "destination-workspace"
+             :defaultValue ""
+             :ref (fn [elem]
+                    ;; doing this manually because the ref handler thingy calls :did-mount overzealously
+                    (when (and elem (not (:select @locals)))
+                      (swap! locals assoc :select elem)
+                      (.on (.select2 (js/$ elem)) "select2:select"
+                           (fn [event]
+                             (swap! state assoc :selected-index (-> event .-target .-value int))))))
+             :style (merge {:width 500} style)}
+            (->> workspaces
+                 (map (comp common/workspace-id->string :workspace))
+                 (concat ["Select a workspace" "Create new workspace..."])))
+          ;; Doing this via display: none to maintain state when the component is hidden
+          [:div {:style {:display (when-not (= 1 selected-index) "none")
+                         :border style/standard-line
+                         :padding "0.5rem" :paddingBottom 0
+                         :margin "-0.5rem" :marginTop "0.5rem"}}
+           [ws-create/WorkspaceCreationForm {:ref #(swap! locals assoc :new-workspace-form %)}]]])))
    :component-did-mount
    (fn [{:keys [props state]}]
      (endpoints/call-ajax-orch
