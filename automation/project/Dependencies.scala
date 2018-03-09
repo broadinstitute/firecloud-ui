b0818387c5f0f5b500fadc6947d4c131471ba333
dvoet <dvoet@users.noreply.github.com>
2018-03-09 10:18:38 -0500

    upgrade big query client libs (#1223)

--- automation/project/Dependencies.scala
+++ automation/project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
     ExclusionRule(organization = "org.broadinstitute.dsde.workbench", name = "workbench-metrics_2.11")
   )
 
-  val workbenchGoogleV = "0.16-8e9ac2a"
+  val workbenchGoogleV = "0.16-8fd86e8"
 
   val workbenchGoogle: ModuleID = "org.broadinstitute.dsde.workbench" %% "workbench-google" % workbenchGoogleV excludeAll(workbenchExclusions:_*)
   val workbenchServiceTest: ModuleID = "org.broadinstitute.dsde.workbench" %% "workbench-service-test" % serviceTestV % "test" classifier "tests" excludeAll(workbenchExclusions:_*)
