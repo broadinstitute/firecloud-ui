1b663592c9fce6bf8bc932a090bb94935c540397
aweng98 <bweng@broadinstitute.org>
2018-03-10 14:09:28 -0500

    remove isTapOpen in TabBar

--- automation/src/test/scala/org/broadinstitute/dsde/firecloud/component/TabBar.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/component/TabBar.scala
@@ -5,20 +5,11 @@ import org.openqa.selenium.WebDriver
 
 
 case class TabBar(private val queryString: QueryString = TestId("tabs"))(implicit webDriver: WebDriver) extends Component(queryString) {
-
   private def tab(name: String) = findInner(s"$name-tab")
 
   def goToTab(name: String): Unit = {
     awaitReady()
     click on tab(name)
-  }
-
-  /**
-    * If WebElement `tab` has some children div, then it's open.
-    * @param name
-    */
-  def isTabOpen(name: String): Unit = {
-    !findAll(s"${tab(name).queryString} > div").isEmpty
   }
 
 }
