c229a862c7bec0f85d02de6df66ee726a4a9f91a
Matt Bemis <MatthewBemis@users.noreply.github.com>
2018-03-20 11:17:29 -0400

    update serviceTest to 0.6-1781edd (#1235)

--- automation/project/Dependencies.scala
+++ automation/project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val akkaV = "2.5.7"
   val akkaHttpV = "10.0.10"
 
-  val serviceTestV = "0.6-47a4d69"
+  val serviceTestV = "0.6-1781edd"
 
   val workbenchModelV  = "0.10-6800f3a"
   val workbenchModel: ModuleID = "org.broadinstitute.dsde.workbench" %% "workbench-model" % workbenchModelV
