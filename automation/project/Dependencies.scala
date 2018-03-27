ed65fdcc407ac698abdb3b5d95bb46ab211f9313
Douglas Voet <dvoet@users.noreply.github.com>
2018-03-26 21:20:08 -0400

    upgrade service tests

--- automation/project/Dependencies.scala
+++ automation/project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val akkaV = "2.5.7"
   val akkaHttpV = "10.0.10"
 
-  val serviceTestV = "0.6-1781edd"
+  val serviceTestV = "0.6-737807d-SNAP"
 
   val workbenchModelV  = "0.10-6800f3a"
   val workbenchModel: ModuleID = "org.broadinstitute.dsde.workbench" %% "workbench-model" % workbenchModelV
