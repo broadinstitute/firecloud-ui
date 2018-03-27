05332fc2ec26220a2537bee9010b5adccbefadd4
aweng98 <bweng@broadinstitute.org>
2018-03-27 17:32:13 -0400

    deprecated old selectFile

--- automation/src/test/scala/org/broadinstitute/dsde/firecloud/component/FileSelector.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/component/FileSelector.scala
@@ -4,10 +4,25 @@ import org.openqa.selenium.WebDriver
 import org.openqa.selenium.remote.LocalFileDetector
 
 case class FileSelector(queryString: QueryString)(implicit webDriver: WebDriver) extends Component(queryString) {
+
   def selectFile(filename: String): Unit = {
     val fileDetector = new LocalFileDetector()
     val file = fileDetector.getLocalFile(filename)
     val webElement = find(query).get.underlying
     webElement.sendKeys(file.getAbsolutePath)
   }
+
+  /**
+    * This is the original selectFile() that uses JS to load file.
+    * Keep it for now until for sure don't need
+    * @param filename
+    */
+  @deprecated
+  def selectFileOld(filename: String): Unit = {
+    executeScript("var field = document.getElementsByName('entities'); field[0].style.display = '';")
+    val webElement = find(query).get.underlying
+    webElement.clear()
+    webElement.sendKeys(filename)
+  }
+
 }
