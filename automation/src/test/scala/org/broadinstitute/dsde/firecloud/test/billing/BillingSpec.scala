d2aea1363161db6bd22e9bd04c367619c731308a
Matt Bemis <MatthewBemis@users.noreply.github.com>
2018-03-29 22:29:55 -0400

    break out one test but consolidate the rest (#1221)

--- automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/billing/BillingSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/billing/BillingSpec.scala
@@ -8,10 +8,10 @@ import org.broadinstitute.dsde.firecloud.fixture.{TestData, UserFixtures}
 import org.broadinstitute.dsde.firecloud.page.billing.BillingManagementPage
 import org.broadinstitute.dsde.firecloud.page.workspaces.methodconfigs.WorkspaceMethodConfigDetailsPage
 import org.broadinstitute.dsde.workbench.auth.AuthToken
-import org.broadinstitute.dsde.workbench.config.{Config, UserPool}
+import org.broadinstitute.dsde.workbench.config.{Config, Credentials, UserPool}
 import org.broadinstitute.dsde.workbench.fixture._
 import org.broadinstitute.dsde.workbench.model.{UserInfo, WorkbenchEmail, WorkbenchUserId}
-import org.broadinstitute.dsde.workbench.service.{Google, Rawls}
+import org.broadinstitute.dsde.workbench.service.{Google, Orchestration, Rawls}
 import org.broadinstitute.dsde.workbench.service.test.{CleanUp, WebBrowserSpec}
 import org.scalatest.{FreeSpec, Matchers}
 
@@ -23,89 +23,56 @@ class BillingSpec extends FreeSpec with WebBrowserSpec with UserFixtures with Cl
 
   "A user" - {
     "with a billing account" - {
-      "should be able to create a billing project" in withWebDriver { implicit driver =>
-        val userOwner = UserPool.chooseProjectOwner
-        implicit val authToken: AuthToken = userOwner.makeAuthToken()
-
-        val billingProjectName = "billing-spec-create-" + makeRandomId() // is this a unique ID?
-
-        withSignIn(userOwner) { _ =>
-          val billingPage = new BillingManagementPage().open
-          logger.info(s"Creating billing project: $billingProjectName")
-
-          billingPage.createBillingProject(billingProjectName, Config.Projects.billingAccount)
-          register cleanUp Rawls.admin.deleteBillingProject(billingProjectName, UserInfo(OAuth2BearerToken(userOwner.makeAuthToken().value), WorkbenchUserId("0"), WorkbenchEmail(userOwner.email), 3600))(UserPool.chooseAdmin.makeAuthToken())
-
-          val status = billingPage.waitForCreateCompleted(billingProjectName)
-          withClue(s"Creating billing project: $billingProjectName") { status shouldEqual "success" }
-        }
-
-        //note that this next part of the test *could* be a different test on it's own, but we have chosen to
-        //build it into this test case because:
-        // a) having it in it's own test case would add another ~5 minutes to the test cycle
-        // b) this test would fail anyway if billing project creation failed
-
-        //should you decide to break this out into it's own test, billing project creation for it should
-        //be driven by API and not UI
-
-        val originalBillingAccount = Google.billing.getBillingProjectAccount(billingProjectName)
-        originalBillingAccount shouldBe Some(Config.Projects.billingAccountId)
-
-        Google.billing.removeBillingProjectAccount(billingProjectName)
-
-        val newBillingAccount = Google.billing.getBillingProjectAccount(billingProjectName)
-        newBillingAccount shouldBe None
-      }
-
       "with a new billing project" - {
-
-        "should be able to add a user to the billing project" in {
-          val ownerUser = UserPool.chooseProjectOwner
-          implicit val authToken: AuthToken = ownerUser.makeAuthToken()
+        /*
+         * This test does a lot more than we would traditionally like, but it's a sacrifice we will
+         * make because creating billing projects is slow and error-prone (because of Google).
+         *
+         * This single test was originally five test cases:
+         * A) Create a new billing project (in the UI)
+         * B) Create a new billing project + add a new user to that project
+         * C) Create a new billing project + create a new workspace in that project
+         * D) Create a new billing project + create a new workspace in that project + run an analysis in that workspace
+         * E) Should be able to change the billing account associated with the project
+         *
+         * A was a subset of B, C and D
+         * C was a subset of D
+         *
+         * Clearly a lot of functionality bleeds between these test cases. In order to save runtime during
+         * the tests, we're combining all five. The idea is that if any piece of basic functionality
+         * in these tests was broken (creating a workspace in a new project, creating a new project, etc), then
+         * most of these tests would fail anyway so grouping them together isn't much worse.
+         *
+         * NOTE: this single test case MUST create a brand new billing project each time it's run. If we use a
+         * project from GPAlloc, this entire test becomes worthless and our test coverage for billing is garbage.
+         *
+         * E comes last because it makes the project unusable.
+         */
+        "should be able add a new user, create a workspace, and run a method, change billing account" in {
+          val user = UserPool.chooseProjectOwner
+          implicit val authToken: AuthToken = user.makeAuthToken()
           val secondUser = UserPool.chooseStudent.email
 
-          withBillingProject("billing-spec-add-user") { billingProjectName =>
-            withWebDriver { implicit driver =>
-              withSignIn(ownerUser) { _ =>
-                val billingPage = new BillingManagementPage().open
-                billingPage.openBillingProject(billingProjectName)
-                billingPage.addUserToBillingProject(secondUser, "User")
-
-                val isSuccess = billingPage.isUserInBillingProject(secondUser)
-                isSuccess shouldEqual true
-              }
-            }
-          }
-        }
-
-        "should be able to create a workspace in the billing project" in {
-          // Create new billing project
-          val user = UserPool.chooseProjectOwner
-          implicit val authToken: AuthToken = user.makeAuthToken()
-
-          withBillingProject("billing-spec-make-ws") { billingProjectName =>
-            withWebDriver { implicit driver =>
-              withSignIn(user) { listPage =>
-                // create workspace and verify
-                val workspaceName = "BillingSpec_makeWorkspace_" + randomUuid
-                register cleanUp api.workspaces.delete(billingProjectName, workspaceName)
-                val detailPage = listPage.createWorkspace(billingProjectName, workspaceName)
-              }
-            }
-          }
-        }
-
-        "should be able to run a method in a new workspace in the billing project" in {
-          val user = UserPool.chooseProjectOwner
-          implicit val authToken: AuthToken = user.makeAuthToken()
 
           /*
            * This must continue to create a new billing project rather than using an allocated one.
            * Otherwise it's not covering the entirety of the intended scenario (which is one that we
            * have seen break in the past that relied on using a brand new billing project).
            */
-          withBillingProject("billing-spec-method") { billingProjectName =>
-            withWorkspace(billingProjectName, "BillingSpec_runMethod") { workspaceName =>
+          withWebDriver { implicit driver =>
+            withSignIn(user) { listPage =>
+              //BEGIN: Test creating billing project in UI
+              val billingPage = new BillingManagementPage().open
+              val billingProjectName = createNewBillingProject(user, billingPage)
+              //END: Test creating billing project in UI
+
+              //BEGIN: Test creating workspace
+              val workspaceName = "BillingSpec_makeWorkspace_" + randomUuid
+              register cleanUp api.workspaces.delete(billingProjectName, workspaceName)
+              val detailPage = listPage.open.createWorkspace(billingProjectName, workspaceName)
+              //END: Test creating workspace
+
+              //BEGIN: Test running analysis in workspace
               api.workspaces.waitForBucketReadAccess(billingProjectName, workspaceName)
               api.importMetaData(billingProjectName, workspaceName, "entities", TestData.SingleParticipant.participantEntity)
 
@@ -113,20 +80,58 @@ class BillingSpec extends FreeSpec with WebBrowserSpec with UserFixtures with Cl
               api.methodConfigurations.copyMethodConfigFromMethodRepo(billingProjectName, workspaceName, SimpleMethodConfig.configNamespace,
                 SimpleMethodConfig.configName, SimpleMethodConfig.snapshotId, SimpleMethodConfig.configNamespace, methodConfigName)
 
-              withWebDriver { implicit driver =>
-                withSignIn(user) { _ =>
-                  // verify running a method
-                  val methodConfigDetailsPage = new WorkspaceMethodConfigDetailsPage(billingProjectName, workspaceName, SimpleMethodConfig.configNamespace, methodConfigName).open
-                  val submissionDetailsPage = methodConfigDetailsPage.launchAnalysis(MethodData.SimpleMethod.rootEntityType, TestData.SingleParticipant.entityId)
+              // verify running a method
+              val methodConfigDetailsPage = new WorkspaceMethodConfigDetailsPage(billingProjectName, workspaceName, SimpleMethodConfig.configNamespace, methodConfigName).open
+              val submissionDetailsPage = methodConfigDetailsPage.launchAnalysis(MethodData.SimpleMethod.rootEntityType, TestData.SingleParticipant.entityId)
 
-                  submissionDetailsPage.waitUntilSubmissionCompletes()
-                  assert(submissionDetailsPage.verifyWorkflowSucceeded())
-                }
-              }
+              submissionDetailsPage.waitUntilSubmissionCompletes()
+              assert(submissionDetailsPage.verifyWorkflowSucceeded())
+              //END: Test running anlysis in workspace
+
+              //BEGIN: Test adding user to project
+              billingPage.open.openBillingProject(billingProjectName)
+              billingPage.addUserToBillingProject(secondUser, "User")
+
+              val isSuccess = billingPage.isUserInBillingProject(secondUser)
+              isSuccess shouldEqual true
+              //END: Test adding user to project
+
+              //BEGIN: should be able to change the billing account associated with the project
+              val originalBillingAccount = Google.billing.getBillingProjectAccount(billingProjectName)
+              originalBillingAccount shouldBe Some(Config.Projects.billingAccountId)
+
+              Google.billing.removeBillingProjectAccount(billingProjectName)
+
+              val newBillingAccount = Google.billing.getBillingProjectAccount(billingProjectName)
+              newBillingAccount shouldBe None
+              //END: should be able to change the billing account associated with the project
             }
           }
         }
       }
+    }
+  }
+
+  private def createNewBillingProject(user: Credentials, billingPage: BillingManagementPage, trials: Int = 3): String = {
+    val billingProjectName = "billing-spec-create-" + makeRandomId()
+    logger.info(s"Creating billing project: $billingProjectName")
+
+    billingPage.createBillingProject(billingProjectName, Config.Projects.billingAccount)
+    register cleanUp Rawls.admin.deleteBillingProject(billingProjectName, UserInfo(OAuth2BearerToken(user.makeAuthToken().value), WorkbenchUserId("0"), WorkbenchEmail(user.email), 3600))(UserPool.chooseAdmin.makeAuthToken())
+
+    val statusOption = billingPage.waitForCreateDone(billingProjectName)
+
+    statusOption match {
+      case None | Some("failure") if trials > 1 =>
+        logger.info(s"failure or timeout creating project $billingProjectName, retrying ${trials-1} more times")
+        createNewBillingProject(user, billingPage, trials-1)
+      case None =>
+        fail(s"timed out waiting billing project $billingProjectName to be ready")
+      case Some(status) =>
+        withClue(s"Creating billing project: $billingProjectName") {
+          status shouldEqual "success"
+        }
+        billingProjectName
     }
   }
 }
