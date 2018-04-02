c43bccf1806c0eb08b038491ed773a03a82a49b2
dvoet <dvoet@users.noreply.github.com>
2018-04-02 19:39:46 -0400

    removed hack

--- automation/src/test/scala/org/broadinstitute/dsde/firecloud/page/workspaces/summary/WorkspaceSummaryPage.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/page/workspaces/summary/WorkspaceSummaryPage.scala
@@ -279,11 +279,6 @@ class WorkspaceSummaryPage(namespace: String, name: String)(implicit webDriver: 
     else
       throw new IllegalStateException("Already editing")
 
-    val start = System.currentTimeMillis()
-    while(!isEditing && System.currentTimeMillis() < start + 10000) {
-      sidebar.clickEdit()
-    }
-    
     action
 
     if (isEditing) {
