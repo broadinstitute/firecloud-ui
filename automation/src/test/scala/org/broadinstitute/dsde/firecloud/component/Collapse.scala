ad29a8a78eb703237fe3f57b902d80dcd16daaf4
aweng98 <bweng@broadinstitute.org>
2018-04-04 14:10:53 -0400

    rework collapse/expand methods to include click retry

--- automation/src/test/scala/org/broadinstitute/dsde/firecloud/component/Collapse.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/component/Collapse.scala
@@ -20,20 +20,18 @@ case class Collapse[A <: FireCloudView](queryString: QueryString, private val in
     awaitReady()
   }
 
-  def ensureExpanded(): Unit = {
-    if (!isExpanded) {
-      click on toggleComponent
-      await condition isExpanded
+  def ensureCollapsed(): Unit = {
+    await condition {
+      if (isExpanded) toggle()
+      stateOf(toggleComponent) == "collapsed"
     }
-    awaitReady()
   }
 
-  def ensureCollapsed(): Unit = {
-    if (isExpanded) {
-      click on toggleComponent
-      await condition stateOf(toggleComponent) == "collapsed"
+  def ensureExpanded(): Unit = {
+    await condition {
+      if (!isExpanded) toggle()
+      stateOf(toggleComponent) == "expanded"
     }
-    awaitReady()
   }
 
   def getInner: A = {
