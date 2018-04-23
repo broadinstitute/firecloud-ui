c3f7e472cb6e1c73e097f327058cd75ea909ed5c
Jackie <jacqueline.roberti93@gmail.com>
2018-04-23 10:17:29 -0400

    skip tests: authenticate for perf tests using service account and python api (#1279)
    * authenticate for perf tests using service account and python api
    
    * enable call caching for perf tests

--- automation/alpha-perf-test.sh
+++ automation/alpha-perf-test.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 # Simple script to start perf test in alpha
 echo "Starting Perf test in alpha"
-echo "FOR EASE OF USE: IF YOU RUN THIS SCRIPT AND A BROWSER OPENS WITH GOOGLE LOGIN, KILL THE SCRIPT AND GCLOUD AUTH INTO EACH USER FIRST (see script comments)"
 
-# Log in with each user:
-# gcloud auth login dominique.testerson@gmail.com
-# gcloud auth login gary.testerson1@gmail.com
-# gcloud auth login felicity.testerson@gmail.com
-# gcloud auth login frida.testerson@gmail.com
-# gcloud auth login elvin.testerson@gmail.com
-# gcloud auth login test.firec@gmail.com
+
+VAULT_TOKEN=${1:-$(cat $HOME/.vault-token)}
+WORKING_DIR=${2:-$PWD}
+ENV=alpha
+
+JSON_CREDS=`docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN -e VAULT_ADDR=https://clotho.broadinstitute.org:8200 broadinstitute/dsde-toolbox vault read -format=json secret/dsde/firecloud/${ENV}/common/firecloud-account.pem | jq '.data'`
+
 
 launchSubmission() {
     user=$1
@@ -35,8 +34,7 @@ launchSubmission() {
         expression=$9
     "
 
-    gcloud auth login $user
-    ACCESS_TOKEN=$(gcloud auth print-access-token)
+    ACCESS_TOKEN=`docker run --rm -v $WORKING_DIR:/app/populate -w /app/populate broadinstitute/dsp-toolbox python get_bearer_token.py "${user}" "${JSON_CREDS}"`
 
     #check if $9 is set for 'expression'
     if [ -z ${9+x} ] ; then
@@ -46,11 +44,11 @@ launchSubmission() {
     fi
 }
 
-launchSubmission dominique.testerson@gmail.com perf-test-a Perf-test-A-workspace qamethods sleep1hr_echo_strings sample_set sample_set6k false "this.samples"
-launchSubmission gary.testerson1@gmail.com perf-test-b Perf-Test-B-W alex_methods sleep_echo_strings sample_set sample_set6k false "this.samples"
-launchSubmission felicity.testerson@gmail.com perf-test-d Perf-Test-D-W_copy alex_methods sleep_echo_strings sample_set sample_set6k false "this.samples"
-launchSubmission frida.testerson@gmail.com perf-test-e Perf-Test_E_W qamethods sleep1hr_echo_strings sample_set sample_set6k false "this.samples"
-launchSubmission elvin.testerson@gmail.com aa-test041417 Perf-Test-G-W alex_methods sleep_echo_strings sample_set sample_set6k false "this.samples"
+launchSubmission dominique.testerson@gmail.com perf-test-a Perf-test-A-workspace qamethods sleep1hr_echo_strings sample_set sample_set6k true "this.samples"
+launchSubmission gary.testerson1@gmail.com perf-test-b Perf-Test-B-W alex_methods sleep_echo_strings sample_set sample_set6k true "this.samples"
+launchSubmission felicity.testerson@gmail.com perf-test-d Perf-Test-D-W_copy alex_methods sleep_echo_strings sample_set sample_set6k true "this.samples"
+launchSubmission frida.testerson@gmail.com perf-test-e Perf-Test_E_W qamethods sleep1hr_echo_strings sample_set sample_set6k true "this.samples"
+launchSubmission elvin.testerson@gmail.com aa-test041417 Perf-Test-G-W alex_methods sleep_echo_strings sample_set sample_set6k true "this.samples"
 launchSubmission test.firec@gmail.com aa-test-042717a test-042717 anuMethods callCacheWDL participant subject_HCC1143 true
 
 printf "\nDone"
