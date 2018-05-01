b957481facf1f0f015031b65a2881b24e10de260
aweng98 <bweng@broadinstitute.org>
2018-04-30 20:28:22 -0400

    revert FreeTrialSpec

--- automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/user/FreeTrialSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/user/FreeTrialSpec.scala
@@ -10,7 +10,7 @@ import org.broadinstitute.dsde.workbench.config.{Config, Credentials, UserPool}
 import org.broadinstitute.dsde.workbench.model.{UserInfo, WorkbenchEmail, WorkbenchUserId}
 import org.broadinstitute.dsde.workbench.service.{Google, Orchestration, Rawls, Thurloe}
 import org.broadinstitute.dsde.workbench.service.test.{CleanUp, WebBrowserSpec}
-import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, FreeSpec, Matchers}
+import org.scalatest.{BeforeAndAfterEach, FreeSpec, Matchers}
 
 import scala.util.Try
 
@@ -18,12 +18,12 @@ import scala.util.Try
 /**
   * Tests for new user registration scenarios.
   */
-class FreeTrialSpec extends FreeSpec with Matchers with WebBrowserSpec
-  with UserFixtures with CleanUp with LazyLogging with BeforeAndAfterAll {
+class FreeTrialSpec extends FreeSpec with BeforeAndAfterEach with Matchers with WebBrowserSpec
+  with UserFixtures with CleanUp with LazyLogging {
 
-  var adminUser: Credentials = _
-  var campaignManager: Credentials = _
-  implicit var authToken: AuthToken = _
+  val adminUser: Credentials = UserPool.chooseAdmin
+  val campaignManager: Credentials = UserPool.chooseCampaignManager
+  implicit val authToken: AuthToken = adminUser.makeAuthToken()
   val trialKVPKeys = Seq("trialState", "trialBillingProjectName", "trialEnabledDate", "trialEnrolledDate",
     "trialTerminatedDate", "trialExpirationDate", "userAgreed")
 
@@ -31,12 +31,7 @@ class FreeTrialSpec extends FreeSpec with Matchers with WebBrowserSpec
   var userAuthToken: AuthToken = _
   var subjectId : String = _
 
-
-  override def beforeAll {
-    super.beforeAll()
-    adminUser = UserPool.chooseAdmin
-    campaignManager = UserPool.chooseCampaignManager
-    authToken = adminUser.makeAuthToken()
+  override def beforeEach {
     testUser = UserPool.chooseStudent
     userAuthToken = testUser.makeAuthToken()
     subjectId = Orchestration.profile.getUser()(userAuthToken)("userId").toString
@@ -57,118 +52,108 @@ class FreeTrialSpec extends FreeSpec with Matchers with WebBrowserSpec
   "A user whose free trial status is" - {
 
     "Blank" - {
-      "should not see the free trial banner" in {
-        withWebDriver { implicit driver =>
-          withSignIn(testUser) { _ =>
-            await ready new WorkspaceListPage()
-            val bannerTitleElement = Label(TestId("trial-banner-title")) // TODO: Define elements in page class
-            bannerTitleElement.isVisible shouldBe false
-          }
+      "should not see the free trial banner" in withWebDriver { implicit driver =>
+        withSignIn(testUser) { _ =>
+          await ready new WorkspaceListPage()
+          val bannerTitleElement = Label(TestId("trial-banner-title")) // TODO: Define elements in page class
+          bannerTitleElement.isVisible shouldBe false
         }
       }
     }
 
     "Enabled" - {
-      "should be able to see the free trial banner, enroll and get terminated" in {
+      "should be able to see the free trial banner, enroll and get terminated" in withWebDriver { implicit driver =>
         setUpEnabledUserAndProject(testUser)
         val trialAuthToken = TrialBillingAccountAuthToken()
         api.trial.reportTrialProjects().foreach { x =>
           register cleanUp Rawls.admin.deleteBillingProject(x.name, UserInfo(OAuth2BearerToken(trialAuthToken.value), WorkbenchUserId("0"), WorkbenchEmail("doesnt@matter.com"), 3600))(UserPool.chooseAdmin.makeAuthToken())
         }
 
-        withWebDriver { implicit driver =>
-          withSignIn(testUser) { _ =>
-            await ready new WorkspaceListPage()
-            val bannerTitleElement = Label(TestId("trial-banner-title"))
-            bannerTitleElement.isVisible shouldBe true
-            bannerTitleElement.getText shouldBe "Welcome to FireCloud!"
+        withSignIn(testUser) { _ =>
+          await ready new WorkspaceListPage()
+          val bannerTitleElement = Label(TestId("trial-banner-title"))
+          bannerTitleElement.isVisible shouldBe true
+          bannerTitleElement.getText shouldBe "Welcome to FireCloud!"
 
-            val bannerButton = await ready Button(TestId("trial-banner-button"))
-            bannerButton.doClick()
+          val bannerButton = await ready Button(TestId("trial-banner-button"))
+          bannerButton.doClick()
 
-            val reviewButton = await ready Button(TestId("review-terms-of-service"))
-            reviewButton.doClick()
+          val reviewButton = await ready Button(TestId("review-terms-of-service"))
+          reviewButton.doClick()
 
-            val agreeTermsCheckbox = await ready Checkbox(TestId("agree-terms"))
-            agreeTermsCheckbox.ensureChecked()
+          val agreeTermsCheckbox = await ready Checkbox(TestId("agree-terms"))
+          agreeTermsCheckbox.ensureChecked()
 
-            val agreeCloudTermsCheckbox = await ready Checkbox(TestId("agree-cloud-terms"))
-            agreeCloudTermsCheckbox.ensureChecked()
+          val agreeCloudTermsCheckbox = await ready Checkbox(TestId("agree-cloud-terms"))
+          agreeCloudTermsCheckbox.ensureChecked()
 
-            val acceptButton = await ready Button(TestId("accept-terms-of-service"))
-            acceptButton.doClick()
+          val acceptButton = await ready Button(TestId("accept-terms-of-service"))
+          acceptButton.doClick()
 
-            await condition bannerButton.getState == "ready"
-            bannerTitleElement.getText shouldBe "Access Free Credits"
-          }
-
-          // Verify that the user has been added to the corresponding billing project
-          val billingProject = Thurloe.keyValuePairs.getAll(subjectId).get("trialBillingProjectName")
-          assert(billingProject.nonEmpty, s"No trial billing project was allocated for the user ${testUser.email}.")
-
-          val userBillingProjects = api.profile.getUserBillingProjects()(userAuthToken)
-          assert(userBillingProjects.nonEmpty, s"The trial user ${testUser.email} has no billing projects.")
-
-          val userHasTheRightBillingProject: Boolean = userBillingProjects.exists(_.values.toList.contains(billingProject.get))
-          assert(userHasTheRightBillingProject)
-
-          // Verify that the user's project is removed from the account upon termination
-          val billingAccountUponEnrollment = Google.billing.getBillingProjectAccount(billingProject.get)(trialAuthToken)
-          assert(billingAccountUponEnrollment.nonEmpty, s"The user's project is not associated with a billing account.")
-
-          api.trial.terminateUser(testUser.email)
-
-          val billingAccountUponTermination = Google.billing.getBillingProjectAccount(billingProject.get)(trialAuthToken)
-          val errMsg = "The trial user's billing project should have been removed from the billing account."
-          assert(billingAccountUponTermination.isEmpty, errMsg)
-
-          registerCleanUpForDeleteTrialState()
+          await condition bannerButton.getState == "ready"
+          bannerTitleElement.getText shouldBe "Access Free Credits"
         }
+
+        // Verify that the user has been added to the corresponding billing project
+        val billingProject = Thurloe.keyValuePairs.getAll(subjectId).get("trialBillingProjectName")
+        assert(billingProject.nonEmpty, s"No trial billing project was allocated for the user ${testUser.email}.")
+
+        val userBillingProjects = api.profile.getUserBillingProjects()(userAuthToken)
+        assert(userBillingProjects.nonEmpty, s"The trial user ${testUser.email} has no billing projects.")
+
+        val userHasTheRightBillingProject: Boolean = userBillingProjects.exists(_.values.toList.contains(billingProject.get))
+        assert(userHasTheRightBillingProject)
+
+        // Verify that the user's project is removed from the account upon termination
+        val billingAccountUponEnrollment = Google.billing.getBillingProjectAccount(billingProject.get)(trialAuthToken)
+        assert(billingAccountUponEnrollment.nonEmpty, s"The user's project is not associated with a billing account.")
+
+        api.trial.terminateUser(testUser.email)
+
+        val billingAccountUponTermination = Google.billing.getBillingProjectAccount(billingProject.get)(trialAuthToken)
+        val errMsg = "The trial user's billing project should have been removed from the billing account."
+        assert(billingAccountUponTermination.isEmpty, errMsg)
+
+        registerCleanUpForDeleteTrialState()
       }
     }
 
     "Terminated" - {
-      "should see that they are inactive" in {
+      "should see that they are inactive" in withWebDriver { implicit driver =>
         registerCleanUpForDeleteTrialState()
         Thurloe.keyValuePairs.set(subjectId, "trialState", "Terminated")
 
-        withWebDriver { implicit driver =>
-          withSignIn(testUser) { _ =>
-            await ready new WorkspaceListPage()
-            val bannerTitleElement = Label(TestId("trial-banner-title"))
-            bannerTitleElement.isVisible shouldBe true
-            bannerTitleElement.getText shouldBe "Your free credits have expired"
-          }
+        withSignIn(testUser) { _ =>
+          await ready new WorkspaceListPage()
+          val bannerTitleElement = Label(TestId("trial-banner-title"))
+          bannerTitleElement.isVisible shouldBe true
+          bannerTitleElement.getText shouldBe "Your free credits have expired"
         }
       }
     }
 
     "Disabled" - {
-      "should not see the free trial banner" in {
+      "should not see the free trial banner" in withWebDriver { implicit driver =>
         registerCleanUpForDeleteTrialState()
         Thurloe.keyValuePairs.set(subjectId, "trialState", "Disabled")
 
-        withWebDriver { implicit driver =>
-          withSignIn(testUser) { _ =>
-            await ready new WorkspaceListPage()
-            val bannerTitleElement = Label(TestId("trial-banner-title"))
-            bannerTitleElement.isVisible shouldBe false
-          }
+        withSignIn(testUser) { _ =>
+          await ready new WorkspaceListPage()
+          val bannerTitleElement = Label(TestId("trial-banner-title"))
+          bannerTitleElement.isVisible shouldBe false
         }
       }
     }
    
    "Finalized" - {
-      "should not see the free trial banner" in {
+      "should not see the free trial banner" in withWebDriver { implicit driver =>
         registerCleanUpForDeleteTrialState()
         Thurloe.keyValuePairs.set(subjectId, "trialState", "Finalized")
 
-        withWebDriver { implicit driver =>
-          withSignIn(testUser) { _ =>
-            await ready new WorkspaceListPage()
-            val bannerTitleElement = Label(TestId("trial-banner-title"))
-            bannerTitleElement.isVisible shouldBe false
-          }
+        withSignIn(testUser) { _ =>
+          await ready new WorkspaceListPage()
+          val bannerTitleElement = Label(TestId("trial-banner-title"))
+          bannerTitleElement.isVisible shouldBe false
         }
       }
     }
