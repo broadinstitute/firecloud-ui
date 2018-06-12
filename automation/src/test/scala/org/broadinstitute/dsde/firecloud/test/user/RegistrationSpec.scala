af83c438b6cba6a55c538b4e25919876f036ee50
aweng98 <bweng@broadinstitute.org>
2018-06-12 19:02:55 -0400

    registration spec

--- automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/user/RegistrationSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/user/RegistrationSpec.scala
@@ -1,23 +1,20 @@
 package org.broadinstitute.dsde.firecloud.test.user
 
-import com.typesafe.scalalogging.LazyLogging
 import org.broadinstitute.dsde.firecloud.fixture.UserFixtures
 import org.broadinstitute.dsde.firecloud.page.library.DataLibraryPage
 import org.broadinstitute.dsde.firecloud.page.user.ProfilePage
 import org.broadinstitute.dsde.firecloud.page.workspaces.WorkspaceListPage
-import org.broadinstitute.dsde.firecloud.test.Tags
 import org.broadinstitute.dsde.workbench.auth.AuthToken
 import org.broadinstitute.dsde.workbench.config.{Config, Credentials, UserPool}
 import org.broadinstitute.dsde.workbench.service.{Sam, Thurloe}
-import org.broadinstitute.dsde.workbench.service.test.{CleanUp, WebBrowserSpec}
+import org.broadinstitute.dsde.workbench.service.test.WebBrowserSpec
 import org.scalatest.{BeforeAndAfter, FreeSpec, Matchers}
 
 
 /**
   * Tests for new user registration scenarios.
   */
-class RegistrationSpec extends FreeSpec with BeforeAndAfter with Matchers with WebBrowserSpec
-  with UserFixtures with CleanUp with LazyLogging {
+class RegistrationSpec extends FreeSpec with BeforeAndAfter with Matchers with WebBrowserSpec with UserFixtures {
 
   val testUser: Credentials = Config.Users.temp  // TODO: pull from user pool and fetch correct subject ID
   val subjectId: String = Config.Users.tempSubjectId
@@ -27,7 +24,6 @@ class RegistrationSpec extends FreeSpec with BeforeAndAfter with Matchers with W
 
   // Clean-up anything left over from any previous failures.
   before {
-    logger.debug(adminUser.email)
     if (Sam.admin.doesUserExist(subjectId).getOrElse(false)) {
       try { Sam.admin.deleteUser(subjectId) } catch nonFatalAndLog("Error deleting user before test but will try running the test anyway")
     }
@@ -41,8 +37,41 @@ class RegistrationSpec extends FreeSpec with BeforeAndAfter with Matchers with W
 
   "FireCloud registration" - {
 
-    "should allow a person to register" taggedAs Tags.SmokeTest in withWebDriver { implicit driver =>
+    "should allow a person to register" in {
 
+      withWebDriver { implicit driver =>
+        withSignInNewUserReal(testUser) { registrationPage =>
+          registerCleanUpForDeleteUser(subjectId)
+
+          registrationPage.register(
+            firstName = "Test",
+            lastName = "Dummy",
+            title = "Tester",
+            contactEmail = Some("test@firecloud.org"),
+            institute = "Broad",
+            institutionalProgram = "DSDE",
+            nonProfitStatus = true,
+            principalInvestigator = "Nobody",
+            city = "Cambridge",
+            state = "MA",
+            country = "USA")
+
+          new DataLibraryPage().validateLocation()
+
+          val profilePage = new ProfilePage().open
+          val username = testUser.email.split("@").head
+          /* Re-enable this code and remove the temporary code below after fixing rawls for GAWB-2933
+          profilePage.readProxyGroupEmail should (startWith (username) and endWith ("firecloud.org"))
+          */
+          profilePage.readProxyGroupEmail should endWith("firecloud.org")
+        }
+      }
+    }
+  }
+
+  "should show billing account instructions for a newly registered user" in {
+
+    withWebDriver { implicit driver =>
       withSignInNewUserReal(testUser) { registrationPage =>
         registerCleanUpForDeleteUser(subjectId)
 
@@ -59,45 +88,15 @@ class RegistrationSpec extends FreeSpec with BeforeAndAfter with Matchers with W
           state = "MA",
           country = "USA")
 
-        new DataLibraryPage().validateLocation()
+        await ready new DataLibraryPage()
 
-        val profilePage = new ProfilePage().open
-        val username = testUser.email.split("@").head
-/* Re-enable this code and remove the temporary code below after fixing rawls for GAWB-2933
-        profilePage.readProxyGroupEmail should (startWith (username) and endWith ("firecloud.org"))
-*/
-        profilePage.readProxyGroupEmail should endWith ("firecloud.org")
-/**/
+        val listPage = new WorkspaceListPage().open
+        listPage.clickCreateWorkspaceButton(true)
+
+        listPage.showsNoBillingProjectsModal() shouldBe true
+
+        listPage.closeModal()
       }
-    }
-  }
-
-  "should show billing account instructions for a newly registered user" in withWebDriver { implicit driver =>
-
-    withSignInNewUserReal(testUser) { registrationPage =>
-      registerCleanUpForDeleteUser(subjectId)
-
-      registrationPage.register(
-        firstName = "Test",
-        lastName = "Dummy",
-        title = "Tester",
-        contactEmail = Some("test@firecloud.org"),
-        institute = "Broad",
-        institutionalProgram = "DSDE",
-        nonProfitStatus = true,
-        principalInvestigator = "Nobody",
-        city = "Cambridge",
-        state = "MA",
-        country = "USA")
-
-      await ready new DataLibraryPage()
-
-      val listPage = new WorkspaceListPage().open
-      listPage.clickCreateWorkspaceButton(true)
-
-      listPage.showsNoBillingProjectsModal() shouldBe true
-
-      listPage.closeModal()
     }
   }
 
