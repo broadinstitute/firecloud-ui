2121f7ea64a89a5f5cd3067b1ff1c687e121daec
aweng98 <35533885+aweng98@users.noreply.github.com>
2018-06-21 11:52:16 -0400

    one account signin (#1321)

--- automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/user/SignInSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/user/SignInSpec.scala
@@ -1,7 +1,8 @@
 package org.broadinstitute.dsde.firecloud.test.user
 
 import org.broadinstitute.dsde.firecloud.fixture.UserFixtures
-import org.broadinstitute.dsde.workbench.config.UserPool
+import org.broadinstitute.dsde.firecloud.page.user.SignInPage
+import org.broadinstitute.dsde.firecloud.FireCloudConfig.{FireCloud, Users}
 import org.broadinstitute.dsde.workbench.service.test.WebBrowserSpec
 import org.scalatest._
 import org.scalatest.time.{Millis, Seconds, Span}
@@ -11,24 +12,29 @@ class SignInSpec extends FreeSpec with WebBrowserSpec with UserFixtures with Mat
 
   implicit override val patienceConfig = PatienceConfig(timeout = scaled(Span(5, Seconds)), interval = scaled(Span(500, Millis)))
 
-  "A user" - {
-    "with a registered account" - {
+  "A user with a registered account" - {
 
-      "should be able to log in and out multiple times as multiple users" in {
-        val Seq(user1, user2) = UserPool.chooseStudents(2)
-        withWebDriver { implicit driver =>
-          withSignInReal(user1) { listPage =>
-            eventually {
-              listPage.readUserEmail() shouldEqual user1.email
-            }
+    "should be able to log in and out as different users in same browser" in {
+      val user1 = Users.Students.getUserCredential("harry")
+      withWebDriver { implicit driver =>
+        withSignInReal(user1) { listPage =>
+          eventually { listPage.readUserEmail() shouldEqual user1.email }
+        }
+        // making sure Sign Out worked
+        eventually { new SignInPage(FireCloud.baseUrl).isOpen shouldEqual true }
+
+        // should be able to log in in same web browser as a different user
+        val user2 = Users.Students.getUserCredential("ron")
+        withSignInReal(user2) { listPage =>
+          eventually {
+            listPage.readUserEmail() shouldEqual user2.email
           }
         }
-        withWebDriver { implicit driver =>
-          withSignInReal(user2) { listPage =>
-            eventually { listPage.readUserEmail() shouldEqual user2.email }
-          }
-        }
+        eventually { new SignInPage(FireCloud.baseUrl).isOpen shouldEqual true }
       }
     }
+
   }
+
+
 }
