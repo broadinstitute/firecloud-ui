e4b2066425e6fc6e8ce150c0e3ec2f5139750a30
jbuabbud <jbuabbud@broadinstitute.org>
2018-06-22 13:47:23 -0400

    Initial broken test

--- automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/user/FreeTrialSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/user/FreeTrialSpec.scala
@@ -7,6 +7,7 @@ import org.broadinstitute.dsde.firecloud.fixture.UserFixtures
 import org.broadinstitute.dsde.firecloud.page.workspaces.WorkspaceListPage
 import org.broadinstitute.dsde.workbench.auth.{AuthToken, TrialBillingAccountAuthToken}
 import org.broadinstitute.dsde.workbench.config.{Credentials, UserPool}
+import org.broadinstitute.dsde.workbench.fixture.BillingFixtures
 import org.broadinstitute.dsde.workbench.fixture.TestReporterFixture
 import org.broadinstitute.dsde.workbench.model.{UserInfo, WorkbenchEmail, WorkbenchUserId}
 import org.broadinstitute.dsde.workbench.service._
@@ -20,7 +21,7 @@ import scala.util.Try
   * Tests for new user registration scenarios.
   */
 class FreeTrialSpec extends FreeSpec with BeforeAndAfterEach with Matchers with WebBrowserSpec
-  with UserFixtures with CleanUp with LazyLogging with TestReporterFixture {
+  with UserFixtures with BillingFixtures with CleanUp with LazyLogging with TestReporterFixture {
 
   val adminUser: Credentials = UserPool.chooseAdmin
   val campaignManager: Credentials = UserPool.chooseCampaignManager
@@ -39,9 +40,8 @@ class FreeTrialSpec extends FreeSpec with BeforeAndAfterEach with Matchers with 
     Try(trialKVPKeys foreach { k => Thurloe.keyValuePairs.delete(subjectId, k)})
   }
 
-  private def setUpEnabledUserAndProject(user: Credentials): Unit = {
+  private def setUpEnabledUser(user: Credentials): Unit = {
     implicit val authToken: AuthToken = campaignManager.makeAuthToken()
-    api.trial.createTrialProjects(1)
     logger.info(s"Attempting to enable user [${user.email}] as campaign manager [${campaignManager.email}]")
     api.trial.enableUser(user.email)
   }
@@ -65,63 +65,61 @@ class FreeTrialSpec extends FreeSpec with BeforeAndAfterEach with Matchers with 
     }
 
     "Enabled" - {
-      "should be able to see the free trial banner, enroll and get terminated" ignore {
-        setUpEnabledUserAndProject(testUser)
+      "should be able to see the free trial banner, enroll and get terminated" in {
         val trialAuthToken = TrialBillingAccountAuthToken()
-        api.trial.reportTrialProjects().foreach { x =>
-          register cleanUp Try(Rawls.admin.deleteBillingProject(x.name, UserInfo(OAuth2BearerToken(trialAuthToken.value),
-            WorkbenchUserId("0"), WorkbenchEmail("doesnt@matter.com"), 3600))(UserPool.chooseAdmin.makeAuthToken())).recover {
-              case ex: RestException => logger.warn(s"RestException occurred in Rawls.admin.deleteBillingProject(${x.name})", ex)
+        withCleanBillingProject(campaignManager, userEmails = List(testUser.email)) { projectName =>
+          api.trial.adoptTrialProject(projectName)(campaignManager.makeAuthToken())
+
+          withWebDriver { implicit driver =>
+            withSignIn(testUser) { _ =>
+              await ready new WorkspaceListPage()
+              val bannerTitleElement = Label(TestId("trial-banner-title"))
+              bannerTitleElement.isVisible shouldBe true
+              bannerTitleElement.getText shouldBe "Welcome to FireCloud!"
+
+              val bannerButton = await ready Button(TestId("trial-banner-button"))
+              bannerButton.doClick()
+
+              val reviewButton = await ready Button(TestId("review-terms-of-service"))
+              reviewButton.doClick()
+
+              val agreeTermsCheckbox = await ready Checkbox(TestId("agree-terms"))
+              agreeTermsCheckbox.ensureChecked()
+
+              val agreeCloudTermsCheckbox = await ready Checkbox(TestId("agree-cloud-terms"))
+              agreeCloudTermsCheckbox.ensureChecked()
+
+              val acceptButton = await ready Button(TestId("accept-terms-of-service"))
+              acceptButton.doClick()
+
+              await condition bannerButton.getState == "ready"
+              bannerTitleElement.getText shouldBe "Access Free Credits"
+            }
           }
+
+          // Verify that the user has been added to the corresponding billing project
+          val billingProject = Thurloe.keyValuePairs.getAll(subjectId).get("trialBillingProjectName")
+          assert(billingProject.nonEmpty, s"No trial billing project was allocated for the user ${testUser.email}.")
+
+          val userBillingProjects = api.profile.getUserBillingProjects()(userAuthToken)
+          assert(userBillingProjects.nonEmpty, s"The trial user ${testUser.email} has no billing projects.")
+
+          val userHasTheRightBillingProject: Boolean = userBillingProjects.exists(_.values.toList.contains(billingProject.get))
+          assert(userHasTheRightBillingProject)
+
+          // Verify that the user's project is removed from the account upon termination
+          val billingAccountUponEnrollment = Google.billing.getBillingProjectAccount(billingProject.get)(trialAuthToken)
+          assert(billingAccountUponEnrollment.nonEmpty, s"The user's project is not associated with a billing account.")
+
+          api.trial.terminateUser(testUser.email)
+
+          val billingAccountUponTermination = Google.billing.getBillingProjectAccount(billingProject.get)(trialAuthToken)
+          val errMsg = "The trial user's billing project should have been removed from the billing account."
+          assert(billingAccountUponTermination.isEmpty, errMsg)
+
+          registerCleanUpForDeleteTrialState()
+          register cleanUp api.trial.scratchTrialProject(projectName)
         }
-        withWebDriver { implicit driver =>
-          withSignIn(testUser) { _ =>
-            await ready new WorkspaceListPage()
-            val bannerTitleElement = Label(TestId("trial-banner-title"))
-            bannerTitleElement.isVisible shouldBe true
-            bannerTitleElement.getText shouldBe "Welcome to FireCloud!"
-
-            val bannerButton = await ready Button(TestId("trial-banner-button"))
-            bannerButton.doClick()
-
-            val reviewButton = await ready Button(TestId("review-terms-of-service"))
-            reviewButton.doClick()
-
-            val agreeTermsCheckbox = await ready Checkbox(TestId("agree-terms"))
-            agreeTermsCheckbox.ensureChecked()
-
-            val agreeCloudTermsCheckbox = await ready Checkbox(TestId("agree-cloud-terms"))
-            agreeCloudTermsCheckbox.ensureChecked()
-
-            val acceptButton = await ready Button(TestId("accept-terms-of-service"))
-            acceptButton.doClick()
-
-            await condition bannerButton.getState == "ready"
-            bannerTitleElement.getText shouldBe "Access Free Credits"
-          }
-        }
-
-        // Verify that the user has been added to the corresponding billing project
-        val billingProject = Thurloe.keyValuePairs.getAll(subjectId).get("trialBillingProjectName")
-        assert(billingProject.nonEmpty, s"No trial billing project was allocated for the user ${testUser.email}.")
-
-        val userBillingProjects = api.profile.getUserBillingProjects()(userAuthToken)
-        assert(userBillingProjects.nonEmpty, s"The trial user ${testUser.email} has no billing projects.")
-
-        val userHasTheRightBillingProject: Boolean = userBillingProjects.exists(_.values.toList.contains(billingProject.get))
-        assert(userHasTheRightBillingProject)
-
-        // Verify that the user's project is removed from the account upon termination
-        val billingAccountUponEnrollment = Google.billing.getBillingProjectAccount(billingProject.get)(trialAuthToken)
-        assert(billingAccountUponEnrollment.nonEmpty, s"The user's project is not associated with a billing account.")
-
-        api.trial.terminateUser(testUser.email)
-
-        val billingAccountUponTermination = Google.billing.getBillingProjectAccount(billingProject.get)(trialAuthToken)
-        val errMsg = "The trial user's billing project should have been removed from the billing account."
-        assert(billingAccountUponTermination.isEmpty, errMsg)
-
-        registerCleanUpForDeleteTrialState()
       }
     }
 
