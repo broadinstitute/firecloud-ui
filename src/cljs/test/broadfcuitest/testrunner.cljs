d2bcab5bfdafdc5c53b1a84bb8b49f4e72721196
David An <davidan@broadinstitute.org>
2018-06-22 12:22:44 -0400

    unit tests for (previewable?)

--- src/cljs/test/broadfcuitest/testrunner.cljs
+++ src/cljs/test/broadfcuitest/testrunner.cljs
@@ -2,6 +2,7 @@
   (:require
    cljs.test
    broadfcui.main
+   broadfcuitest.common.gcs-file-preview
    broadfcuitest.components.buttons
    broadfcuitest.components.research-purpose
    broadfcuitest.config
@@ -15,6 +16,7 @@
   (cljs.test/run-tests 'broadfcuitest.config)
   (cljs.test/run-tests 'broadfcuitest.duration)
   (cljs.test/run-tests 'broadfcuitest.style)
+  (cljs.test/run-tests 'broadfcuitest.common.gcs-file-preview)
   (cljs.test/run-tests 'broadfcuitest.components.buttons)
   (cljs.test/run-tests 'broadfcuitest.components.research-purpose)
   (cljs.test/run-tests 'broadfcuitest.strip-comment-leaders)
