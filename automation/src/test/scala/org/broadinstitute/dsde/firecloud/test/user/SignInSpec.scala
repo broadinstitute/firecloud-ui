b8d3ee68481e9f15b8ab6870541c268c68863d49
aweng98 <bweng@broadinstitute.org>
2018-07-09 17:51:48 -0400

    moved to workbench-lib

--- automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/user/SignInSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/user/SignInSpec.scala
@@ -1,15 +1,16 @@
 package org.broadinstitute.dsde.firecloud.test.user
 
-import org.broadinstitute.dsde.firecloud.fixture.{FailTestRetryable, UserFixtures}
+import org.broadinstitute.dsde.firecloud.fixture.UserFixtures
 import org.broadinstitute.dsde.firecloud.page.user.SignInPage
 import org.broadinstitute.dsde.firecloud.FireCloudConfig.{FireCloud, Users}
+import org.broadinstitute.dsde.workbench.fixture.FailedTestRetryable
 import org.broadinstitute.dsde.workbench.service.test.WebBrowserSpec
 import org.scalatest._
 import org.scalatest.tagobjects.Retryable
 import org.scalatest.time.{Millis, Seconds, Span}
 
 
-class SignInSpec extends FreeSpec with FailTestRetryable with WebBrowserSpec with UserFixtures with Matchers {
+class SignInSpec extends FreeSpec with FailedTestRetryable with WebBrowserSpec with UserFixtures with Matchers {
 
   implicit override val patienceConfig = PatienceConfig(timeout = scaled(Span(5, Seconds)), interval = scaled(Span(500, Millis)))
 
