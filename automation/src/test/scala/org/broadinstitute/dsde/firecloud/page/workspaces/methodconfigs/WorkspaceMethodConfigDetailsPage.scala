f75f99094730d6a8fc2e470cd29a9140604e4864
ansingh7115 <singh.an@husky.neu.edu>
2018-07-16 02:39:58 -0400

    download link click

--- automation/src/test/scala/org/broadinstitute/dsde/firecloud/page/workspaces/methodconfigs/WorkspaceMethodConfigDetailsPage.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/page/workspaces/methodconfigs/WorkspaceMethodConfigDetailsPage.scala
@@ -198,7 +198,7 @@ class WorkspaceMethodConfigDetailsPage(namespace: String, name: String, methodCo
     // await condition (windowHandles.size == 1, 30)
     // .submit call takess care waiting for a new window
     logger.info(s"form: ${form.queryString}")
-    downloadInputsJsonLink.submit()
+    downloadInputsJsonLink.doClick()
     println("download link click")
 
     for {
