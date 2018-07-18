b3594cce75a99c4a148f26f8313c6a239375bc80
ansingh7115 <singh.an@husky.neu.edu>
2018-07-17 20:24:53 -0400

    order of json is weird

--- automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/analysis/MethodConfigSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/analysis/MethodConfigSpec.scala
@@ -188,7 +188,8 @@ class MethodConfigSpec extends FreeSpec with Matchers with WebBrowserSpec with W
 
               // Check the downloaded file contains the right values
               val inputsList = Source.fromFile(inputsFile).mkString
-              inputsList shouldBe """{"w.t.inFloat":1.5,"w.t.inStringArray2":["say \"hi\"!"],"w.t.inThisRef":"$this.hello","w.t.inWorkspaceRef":"$workspace.hello","w.t.inStringMap":{"foo":"bar"},"w.t.inString":"test","w.t.inFile":"gs://foo/bar","w.t.inStringArray":["foo","bar"],"w.t.inBoolean":true,"w.t.inInt":2}"""
+              val expected = """{"w.t.inString":"test","w.t.inFloat":1.5,"w.t.inStringArray2":["say \"hi\"!"],"w.t.inThisRef":"$this.hello","w.t.inWorkspaceRef":"$workspace.hello","w.t.inInt":2,"w.t.inStringMap":{"foo":"bar"},"w.t.inBoolean":true,"w.t.inStringArray":["foo","bar"],"w.t.inFile":"gs://foo/bar"}"""
+              inputsList shouldBe expected
             }
           }
         }
