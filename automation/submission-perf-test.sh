37b838de0dee9ac5a5eec0b267f189b800b30df4
gdlex4015 <garyd@broadinstitute.org>
2018-07-18 17:22:50 -0400

    more wip

--- automation/submission-perf-test.sh
+++ automation/submission-perf-test.sh
@@ -34,10 +34,7 @@ callbackToNIH() {
         echo "This user needs its refresh token refreshed"
         exit 1
         fi
-      curl -X POST --header "Content-Type: application/json" --header "Accept: application/json" --header "Authorization: Bearer $ACCESS_TOKEN" -d"
-        {\"jwt": "eyJhbGciOiJIUzI1NiJ9.ZmlyZWNsb3VkLWRldg.NPXbSpTmAOUvJ1HX85TauAARnlMKfqBsPjumCC7zE7s"\}
-        "https://firecloud-orchestration.dsde-$ENV.broadinstitute.org/api/nih/callback"
-
+      curl -X POST --header "Content-Type: application/json" --header "Accept: application/json" --header "Authorization: Bearer $ACCESS_TOKEN" -d "{"jwt":"eyJhbGciOiJIUzI1NiJ9.ZmlyZWNsb3VkLWRldg.NPXbSpTmAOUvJ1HX85TauAARnlMKfqBsPjumCC7zE7s"}" "https://firecloud-orchestration.dsde-alpha.broadinstitute.org/api/nih/callback"
     }
 
 launchSubmission() {
