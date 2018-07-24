b227521f48a98c08556f032f315e8f6e16fdefe1
gdlex4015 <garyd@broadinstitute.org>
2018-07-23 20:52:17 -0400

    skip tests Gd callback to nih (#1366)
    * added callback to NIH function to performance test script
    
    * removed message about adding user to NIH
    
    * removed syntax errors from script
    
    * removed syntax errors from script take2
    
    * removed syntax errors from script take3
    
    * wip
    
    * more wip
    
    * more and more wip
    
    * again with wip
    
    * again with wip
    
    * and one more wip
    
    * one more last
    
    * and again
    
    * and again
    
    * removed set -x from script
    
    * added checkToken function and array of users to run this and callBackToNIH functions against
    
    * wip1
    
    * wip2
    
    * wip3
    
    * wip4
    
    * wip5
    
    * wip6
    
    * wip7
    
    * wip8
    
    * wip9
    
    * wip0
    
    * wip11
    
    * broke out check token function and itereting thought and array of users for it and callback to NIH
    
    * broke out check token function and itereting thought and array of users for it and callback to NIH1
    
    * broke out check token function and itereting thought and array of users for it and callback to NIH1
    
    * broke out check token function and itereting thought and array of users for it and callback to NIH2
    
    * broke out check token function and itereting thought and array of users for it and callback to NIH final cleanup
    
    * addressed ALL of Andrews concerns
    
    * synax fix1
    
    * added set -x for debugging
    
    * wip
    
    * removed verbose output

--- automation/submission-perf-test.sh
+++ automation/submission-perf-test.sh
@@ -1,20 +1,58 @@
 #!/bin/bash
-# Simple script to start perf test in $ENV
+# Script to start perf test in $ENV, authorize users with NIH
 
 set -e
 
 ENV=$1
 VAULT_TOKEN=${2:-$(cat $HOME/.vault-token)}
 WORKING_DIR=${3:-$PWD}
+NEED_TOKEN=false
 
 # Check if $ENV is not empty
 if [[ ! ${ENV} ]]; then
-    echo "ENV is empty. Run again \`sh submission-perf-test.sh <alpha or staging>\`"
+   echo "ENV is empty. Run again \`sh submission-perf-test.sh <alpha or staging>\`"
     exit 1
-else echo "Starting Perf test in $ENV"; fi
-
+else
+    echo "Starting Perf test in {$ENV}"
+fi
 
 JSON_CREDS=`docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN -e VAULT_ADDR=https://clotho.broadinstitute.org:8200 broadinstitute/dsde-toolbox vault read -format=json secret/dsde/firecloud/dev/common/firecloud-account.pem | jq '.data'`
+
+users=(
+     harry.potter@test.firecloud.org
+     ron.weasley@test.firecloud.org
+     mcgonagall.curator@test.firecloud.org
+     draco.malfoy@test.firecloud.org
+     hermione.owner@test.firecloud.org
+     dumbledore.admin@test.firecloud.org
+   )
+
+
+checkToken () {
+    user=$1
+
+    # Verify that user does not need to refresh their token
+    if
+        curl -f -v --silent -X GET --header "Accept: application/json" --header "Authorization: Bearer $ACCESS_TOKEN" "https://firecloud-orchestration.dsde-$ENV.broadinstitute.org/api/refresh-token-status"  2>&1 | grep '"requiresRefresh": true'
+    then
+        echo "$1 needs its refresh token refreshed"
+        NEED_TOKEN=true
+
+    fi
+
+}
+
+callbackToNIH() {
+    user=$1
+
+    echo "
+    Launching calback to NIH for:
+    user=$1
+    "
+     ACCESS_TOKEN=`docker run --rm -v $WORKING_DIR:/app/populate -w /app/populate broadinstitute/dsp-toolbox python get_bearer_token.py "${user}" "${JSON_CREDS}"`
+
+      curl -X POST --header "Content-Type: application/json" --header "Accept: application/json" --header "Authorization: Bearer $ACCESS_TOKEN" -d "{\"jwt\":\"eyJhbGciOiJIUzI1NiJ9.ZmlyZWNsb3VkLWRldg.NPXbSpTmAOUvJ1HX85TauAARnlMKfqBsPjumCC7zE7s\"}" "https://firecloud-orchestration.dsde-$ENV.broadinstitute.org/api/nih/callback"
+}
 
 launchSubmission() {
     user=$1
@@ -40,18 +78,9 @@ launchSubmission() {
         expression=$9
     "
 
-    ACCESS_TOKEN=`docker run --rm -v $WORKING_DIR:/app/populate -w /app/populate broadinstitute/dsp-toolbox python get_bearer_token.py "${user}" "${JSON_CREDS}"`
+     ACCESS_TOKEN=`docker run --rm -v $WORKING_DIR:/app/populate -w /app/populate broadinstitute/dsp-toolbox python get_bearer_token.py "${user}" "${JSON_CREDS}"`
 
-    # Verify that user does not need to refresh their token
-    if curl -f -v --silent -X GET --header "Accept: application/json" --header "Authorization: Bearer $ACCESS_TOKEN" "https://firecloud-orchestration.dsde-alpha.broadinstitute.org/api/refresh-token-status"  2>&1 | grep '"requiresRefresh": true'
-    then
-        echo "This user needs its refresh token refreshed"
-        exit 1
-    fi
-
-    echo "Note: If there is an error message below (with 'curl: (22)' and '404 Not Found') then that user needs to re-link to NIH."
-
-    #check if $9 is set for 'expression'
+    # check if $9 is set for expression
     if [ -z ${9+x} ] ; then
         curl -f "https://firecloud-orchestration.dsde-$ENV.broadinstitute.org/api/workspaces/$namespace/$name/submissions" -H "origin: https://firecloud.dsde-$ENV.broadinstitute.org" -H "accept-encoding: gzip, deflate, br" -H "authorization: Bearer $ACCESS_TOKEN" -H "content-type: application/json" --data-binary "{\"methodConfigurationNamespace\":\"$methodConfigurationNamespace\",\"methodConfigurationName\":\"$methodConfigurationName\",\"entityType\":\"$entityType\",\"entityName\":\"$entityName\",\"useCallCache\":$useCallCache}" --compressed
     else
@@ -59,8 +88,22 @@ launchSubmission() {
     fi
 }
 
-
 if [ $ENV = "alpha" ]; then
+    # check if user needs a token refresh
+    for user in "${users[@]}"
+    do
+        checkToken $user
+    done
+
+    if [ "$NEED_TOKEN" = true ]; then
+       exit 1
+    fi
+
+    # refresh user's NIH status
+    for user in "${users[@]}"
+    do
+        callbackToNIH $user
+    done
     launchSubmission harry.potter@test.firecloud.org perf-test-a Perf-test-A-workspace qamethods sleep1hr_echo_strings sample_set sample_set6k true "this.samples"
     launchSubmission ron.weasley@test.firecloud.org perf-test-b Perf-Test-B-W alex_methods sleep_echo_strings sample_set sample_set6k true "this.samples"
     launchSubmission mcgonagall.curator@test.firecloud.org perf-test-d Perf-Test-D-W_copy alex_methods sleep_echo_strings sample_set sample_set6k true "this.samples"
@@ -69,6 +112,20 @@ if [ $ENV = "alpha" ]; then
     launchSubmission dumbledore.admin@test.firecloud.org aa-test-042717a test-042717 anuMethods callCacheWDL participant subject_HCC1143 true
 
 elif [ $ENV = "staging" ]; then
+    # check if user needs a token refresh
+    for user in "${users[@]}"
+    do
+        checkToken $user
+    done
+    if [ "$NEED_TOKEN" = true ]; then
+       exit 1
+    fi
+
+    # refresh user's NIH status
+    for user in "${users[@]}"
+    do
+        callbackToNIH $user
+    done
     launchSubmission harry.potter@test.firecloud.org staging-submission-perf-test-a Perf-test-A-workspace submission-perf-test sleep1hr_echo_strings sample_set sample_set6k true "this.samples"
     launchSubmission ron.weasley@test.firecloud.org staging-submission-perf-test-b Perf-Test-B-W submission-perf-test sleep_20min_echo_strings sample_set sample_set6k true "this.samples"
     launchSubmission mcgonagall.curator@test.firecloud.org staging-submission-perf-test-d Perf-Test-D-W_copy submission-perf-test sleep_20min_echo_strings sample_set sample_set6k true "this.samples"
@@ -80,6 +137,5 @@ else
     echo "Could not find ENV"
     exit 1
 fi
-
 
 printf "\nDone"
