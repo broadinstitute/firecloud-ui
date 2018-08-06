881b04764011ea2e1e6e9454eee87ae80dc776f4
Isaac Zarsky <zarsky@broadinstitute.org>
2018-08-06 17:47:06 -0400

    working with fileSummary v1
    Co-Authored-By: Matt Bemis <matthewbemis@users.noreply.github.com>

--- src/cljs/main/broadfcui/utils/ajax.cljs
+++ src/cljs/main/broadfcui/utils/ajax.cljs
@@ -88,7 +88,7 @@
               (reset! server-down? false)))))
 
 (defn get-exponential-backoff-interval [attempt]
-  (* (.pow js/Math 2 attempt) 1000))                        ;; backoff interval in millis
+  (* (.pow js/Math 2 attempt) 1000)) ;; backoff interval in millis
 
 (defn- call-service [url-root path arg-map maybe-nil-service-prefix]
   (let [service-prefix (or maybe-nil-service-prefix "/api")]
@@ -111,14 +111,12 @@
 (defn call-sam [path arg-map & {:keys [service-prefix]}]
   (call-service (config/sam-url-root) path arg-map service-prefix))
 
-(defn call-martha [data arg-map]
-  (let [on-done (:on-done arg-map)]
-    (call (assoc arg-map
-            :url (config/martha-url)
-            :method "POST"
-            :data data
-            :on-done (fn [{:keys [status-code status-text] :as m}]
-                       (on-done m))))))
+(defn call-martha [uri arg-map]
+  (call (assoc arg-map
+          :method "POST"
+          :url (config/martha-url)
+          :headers (merge (@get-bearer-token-header) content-type=json)
+          :data (str "{\"uri\": \"" uri "\"}"))))
 
 (defn call-bond [path arg-map & {:keys [service-prefix] :or {service-prefix "/api"}}]
   (assert (= (subs path 0 1) "/") (str "Path must start with '/': " path))
