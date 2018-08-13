d205623278436b7a0df01bac15438cea14a1ba2a
David An <davidan@genomebridge.org>
2018-08-13 11:41:24 -0400

    Send an X-App-ID: FireCloud http header with every* ajax request (#1387)
    add X-App-ID=FireCloud for ajax calls to orch, leo, sam, bond, martha

--- src/cljs/main/broadfcui/utils/ajax.cljs
+++ src/cljs/main/broadfcui/utils/ajax.cljs
@@ -12,6 +12,8 @@
 ;; TODO - make this unnecessary
 (def content-type=json {"Content-Type" "application/json"})
 
+(def app-id {"X-App-ID" "FireCloud"})
+
 (defonce ^:private recent-ajax-calls (atom #{}))
 (def ^:private double-call-threshold 2000)
 
@@ -97,6 +99,7 @@
       (call (assoc arg-map
               :url (str url-root service-prefix path)
               :headers (merge (@get-bearer-token-header)
+                              app-id
                               (:headers arg-map))
               :on-done (fn [{:keys [status-code status-text] :as m}]
                          (update-health status-code status-text)
@@ -115,6 +118,7 @@
   (let [on-done (:on-done arg-map)]
     (call (assoc arg-map
             :url (config/martha-url)
+            :headers (merge app-id (:headers arg-map))
             :method "POST"
             :data data
             :on-done (fn [{:keys [status-code status-text] :as m}]
@@ -126,6 +130,7 @@
     (call (assoc arg-map
             :url (str (config/bond-url) service-prefix path)
             :headers (merge (@get-bearer-token-header)
+                            app-id
                             (:headers arg-map))
             :on-done (fn [{:keys [status-code status-text] :as m}]
                        (update-health status-code status-text)
