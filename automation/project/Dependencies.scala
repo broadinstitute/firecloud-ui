cbfecd6ca1cc55c1299e9c7be04dd24fbd62f64f
aweng98 <bweng@broadinstitute.org>
2018-08-27 14:25:22 -0400

    update workbench-lib

--- automation/project/Dependencies.scala
+++ automation/project/Dependencies.scala
@@ -4,7 +4,7 @@ import sbt.Keys._
 object Dependencies {
   val jacksonV = "2.9.0"
 
-  val serviceTestV = "0.12-218f0ce"
+  val serviceTestV = "0.12-fca0c5b"
 
   val workbenchExclusions = Seq(
     ExclusionRule(organization = "org.broadinstitute.dsde.workbench", name = s"workbench-model_$scalaBinaryVersion"),
