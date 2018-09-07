b57fb3385b6ccef8e7e599e92943c053b0a2da16
David An <davidan@broadinstitute.org>
2018-09-07 11:55:01 -0400

    code cleanup

--- automation/src/test/scala/org/broadinstitute/dsde/firecloud/page/library/DataLibraryPage.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/page/library/DataLibraryPage.scala
@@ -120,53 +120,40 @@ class ResearchPurposeModal(implicit webDriver: WebDriver) extends OKCancelModal(
   def enterOntologySearchText(text: String): Seq[String] = {
     ontologySearch.setText(s"$text ") // appends a whitespace
 
+    // find the DOM element representing the search input
     val uel = ontologySearch.query.element.underlying
 
-    logger.warn(s"==========>>>>>>>>>> enterOntologySearchText uel is ${uel.getTagName} : ${uel.toString}")
-
-    // Thread sleep 500 // micro sleep before checking for visibility
-
-    // wait for the dropdown to be displayed
-    await condition uel.isDisplayed
-
-    // and wait for it to be expanded
-    // await condition uel.getAttribute("aria-expanded") == "true"
-
-    // look for the "aria-owns" or "aria-controls" attributes. Either could be populated.
-    val ownedId = Option(uel.getAttribute("aria-owns"))
-    val controlledId = Option(uel.getAttribute("aria-controls"))
-
-    val dropdownIdOption = (ownedId ++ controlledId).headOption
-
-    logger.warn(s"==========>>>>>>>>>> enterOntologySearchText ownedId is $ownedId : controlledId is $controlledId")
-
-    assert(dropdownIdOption.nonEmpty, s"Could not determine dropdownId from aria-owns [$ownedId] : aria-controls [$controlledId]")
-
-    val dropdownId = dropdownIdOption.get // safe given the previous assert
-
-    import org.openqa.selenium.JavascriptExecutor
-    val executor = webDriver.asInstanceOf[JavascriptExecutor]
-
-
-    val uelAttrs = executor.executeScript("var items = {}; for (index = 0; index < arguments[0].attributes.length; ++index) { items[arguments[0].attributes[index].name] = arguments[0].attributes[index].value }; return items;", uel)
-
-    logger.warn(s"==========>>>>>>>>>> enterOntologySearchText uelAttrs is $uelAttrs")
-
-    logger.warn(s"==========>>>>>>>>>> enterOntologySearchText dropdownId is [$dropdownId]")
-
-
-    val listOptionXpath = s"//div[@id='$dropdownId']/ul[@role='listbox']/li[@role='option']"
-    // wait for dropdown to contain at least one option
+    // the  autocomplete JavaScript will add either an aria-owns or an aria-controls attribute to the input element.
+    // It's unclear which attribute is used under which conditions by the third-party autocomplete JavaScript,
+    // so we look for either here.
+    //
+    // the value of this attribute will be the id of the DOM element (a div) that contains the suggestion elements.
+    // see https://www.w3.org/WAI/PF/aria-1.1/states_and_properties#attrs_relationships for info on aria-owns/aria-controls.
+    //
+    // wait for the aria-owns/aria-controls attribute to exist:
     await condition {
-      find(xpath(s"//div[@id='$dropdownId']")).exists(_.isDisplayed)
-      val firstFoundValues = findAll(xpath(listOptionXpath)).map(_.text).toSeq
-      logger.warn(s"==========>>>>>>>>>> enterOntologySearchText firstFoundValues is [${firstFoundValues.toList}]")
-      firstFoundValues.nonEmpty // getting Element's text force screen scroll if item is outside of viewport
+      Option(uel.getAttribute("aria-owns")).nonEmpty ||
+      Option(uel.getAttribute("aria-controls")).nonEmpty
     }
 
-    val secondFoundValues = findAll(xpath(listOptionXpath)).map(_.text).toSeq
-    logger.warn(s"==========>>>>>>>>>> enterOntologySearchText secondFoundValues is [${secondFoundValues.toList}]")
-    secondFoundValues
+    // reduce the value of the "aria-owns" or "aria-controls" attributes - either could be populated - into
+    // the DOM id that contains the suggestions.
+    val ownedId = Option(uel.getAttribute("aria-owns"))
+    val controlledId = Option(uel.getAttribute("aria-controls"))
+    val dropdownId = (ownedId ++ controlledId).headOption match {
+      case Some(id) => id
+      case None => throw new Exception(s"Could not determine dropdownId from aria-owns [$ownedId] : aria-controls [$controlledId]")
+    }
+
+    // wait for dropdown to contain at least one option
+    val listOptionXpath = s"//div[@id='$dropdownId']/ul[@role='listbox']/li[@role='option']"
+    await condition {
+      find(xpath(s"//div[@id='$dropdownId']")).exists(_.isDisplayed)
+      findAll(xpath(listOptionXpath)).map(_.text).toSeq.nonEmpty // getting Element's text force screen scroll if item is outside of viewport
+    }
+
+    // return the value of the options text
+    findAll(xpath(listOptionXpath)).map(_.text).toSeq
   }
 
   def selectSuggestion(suggestionTestId: String): Unit = {
