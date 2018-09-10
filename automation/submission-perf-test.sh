107d0a5bdafdf5d891de7a4c74f82ac8cbcc502c
gdlex4015 <garyd@broadinstitute.org>
2018-09-10 16:49:54 -0400

    skip tests Gd change perf (#1405)
    * Added code to monitor one-off submission in Perf test
    
    * wip1
    
    * wip2
    
    * wip3
    
    * wip4
    
    * wip5
    
    * wip6
    
    * wip 7
    
    * wip 8
    
    * wip8
    
    * wip 10
    
    * wip11
    
    * wip12
    
    * test passed, final wip
    
    * addressing Matts nitpicks
    
    * fixed bug in the for loop
    
    * fun with bash 1
    
    * funwith bash 2
    
    * fun with bash 3
    
    * fun with bash 5
    
    * fun with bash 6
    
    * fun with bash 7
    
    * fun with bash 8
    
    * committing fun with bash changes
    
    * committing fun with bash changes
    
    * removed cold cache from submissions
    
    * minor changes in the time allocated for the test to run before failure
    
    * changed method to run for perf test

--- automation/submission-perf-test.sh
+++ automation/submission-perf-test.sh
@@ -129,11 +129,11 @@ monitorSubmission() {
     done
 
 if [ $ENV = "alpha" ]; then
-    launchSubmission harry.potter@test.firecloud.org perf-test-a Perf-test-A-workspace qamethods sleep1hr_echo_strings sample_set sample_set6k true "this.samples"
-    launchSubmission ron.weasley@test.firecloud.org perf-test-b Perf-Test-B-W alex_methods sleep_echo_strings sample_set sample_set6k true "this.samples"
-    launchSubmission mcgonagall.curator@test.firecloud.org perf-test-d Perf-Test-D-W_copy alex_methods sleep_echo_strings sample_set sample_set6k true "this.samples"
-    launchSubmission draco.malfoy@test.firecloud.org perf-test-e Perf-Test_E_W qamethods sleep1hr_echo_strings sample_set sample_set6k true "this.samples"
-    launchSubmission hermione.owner@test.firecloud.org aa-test041417 Perf-Test-G-W alex_methods sleep_echo_strings sample_set sample_set6k true "this.samples"
+    launchSubmission harry.potter@test.firecloud.org perf-test-a Perf-test-A-workspace abcd no_sleep1hr_echo_files sample_set sample_set6k true "this.samples"
+    launchSubmission ron.weasley@test.firecloud.org perf-test-b Perf-Test-B-W abcd no_sleep1hr_echo_files sample_set sample_set6k true "this.samples"
+    launchSubmission mcgonagall.curator@test.firecloud.org perf-test-d Perf-Test-D-W_copy abcd no_sleep1hr_echo_files sample_set sample_set6k true "this.samples"
+    launchSubmission draco.malfoy@test.firecloud.org perf-test-e Perf-Test_E_W abcd no_sleep1hr_echo_files sample_set sample_set6k true "this.samples"
+    launchSubmission hermione.owner@test.firecloud.org aa-test041417 Perf-Test-G-W abcd no_sleep1hr_echo_files sample_set sample_set6k true "this.samples"
     launchSubmission dumbledore.admin@test.firecloud.org aa-test-042717a test-042717 anuMethods callCacheWDL participant subject_HCC1143 true
 
     #Monitor the progress of the perf test
@@ -142,7 +142,7 @@ if [ $ENV = "alpha" ]; then
 
 
    i=1
-   while [ "$submissionStatus" != "Done" ] && [ "$i" -le 19 
+   while [ "$submissionStatus" != "Done" ] && [ "$i" -le 19 ]
 
     do
             echo $i
