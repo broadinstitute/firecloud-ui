cfd6fe8819f691339d907ba36be33fd96070e7cd
aweng98 <35533885+aweng98@users.noreply.github.com>
2018-09-19 15:04:33 -0400

    autosuggestion test failure (#1414)
    * fix
    
    * log debug
    
    * more debug
    
    * logback
    
    * logback
    
    * log

--- automation/src/test/scala/org/broadinstitute/dsde/firecloud/component/Suggests.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/component/Suggests.scala
@@ -15,7 +15,7 @@ trait Suggests extends LazyLogging { this: Component =>
 
   def getSuggestions()(implicit webDriver: WebDriver): Seq[String] = {
     val wait = new FluentWait[WebDriver](webDriver)
-        .withTimeout(Duration.ofSeconds(10))
+        .withTimeout(Duration.ofSeconds(30))
         .pollingEvery(Duration.ofMillis(600))
         .withMessage("Reading autoSuggestions")
         .ignoring(classOf[StaleElementReferenceException])
@@ -43,6 +43,7 @@ trait Suggests extends LazyLogging { this: Component =>
     // see https://www.w3.org/WAI/PF/aria-1.1/states_and_properties#attrs_relationships for info on aria-owns/aria-controls.
     //
     // wait for the aria-owns/aria-controls attribute to exist:
+    logger.info(s"(remove) autosuggestion: check aria-owns or aria-controls is nonEmpty")
     await condition {
       Option(uel.getAttribute("aria-owns")).nonEmpty ||
       Option(uel.getAttribute("aria-controls")).nonEmpty
@@ -50,6 +51,7 @@ trait Suggests extends LazyLogging { this: Component =>
 
     // reduce the value of the "aria-owns" or "aria-controls" attributes - either could be populated - into
     // the DOM id that contains the suggestions.
+    logger.info(s"(remove) autosuggestion: determine dropdownId")
     val ownedId = Option(uel.getAttribute("aria-owns"))
     val controlledId = Option(uel.getAttribute("aria-controls"))
     val dropdownId = (ownedId ++ controlledId).headOption match {
@@ -57,20 +59,21 @@ trait Suggests extends LazyLogging { this: Component =>
       case None => throw new Exception(s"Could not determine dropdownId from aria-owns [$ownedId] : aria-controls [$controlledId]." +
         " Is this input field enabled for suggestions? Has your test activated the suggestions dropdown?")
     }
-    // wait for dropdown visible
-    await condition {
-      find(xpath(s"//div[@id='$dropdownId']")).exists(_.isDisplayed)
-    }
 
     // wait for dropdown contains at least one option and every option text is visible
-    val listOptionXpath = s"//div[@id='$dropdownId']/ul[@role='listbox']/li[@role='option']"
+    val listOptionXpath = s"#$dropdownId li"
+    logger.info(s"(remove) autosuggestion: dropdownId: $dropdownId")
     await condition {
-      val options = findAll(xpath(listOptionXpath))
-      options.map(_.text).toSeq.nonEmpty && options.forall { elem: Element => elem.isDisplayed }
+      val options = findAll(cssSelector(listOptionXpath))
+      val displayed = options.forall {_.isDisplayed}
+      logger.info(s"(remove) autosuggestion: texts displayed: $displayed")
+      displayed
     }
 
     // return the value of the options text
-    findAll(xpath(listOptionXpath)).map(_.text).toSeq
+    val li = findAll(cssSelector(listOptionXpath)).map(_.text).toSeq
+    logger.info(s"(remove) autosuggestion: dropdown list contains ${li.size} li: $li")
+    li
   }
 
 }
