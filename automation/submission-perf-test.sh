a2937a1cfebd34a112ba71cc4580500015654628
gdlex4015 <garyd@broadinstitute.org>
2018-10-17 16:20:47 -0400

    wip

--- automation/submission-perf-test.sh
+++ automation/submission-perf-test.sh
@@ -173,7 +173,7 @@ if [ $ENV = "alpha" ]; then
 
     if [ "$submissionStatus" == "Done" ] && [ "$workflowsStatus" == "Succeeded" ]; then
       echo "One-off workflow finished within 3 hours with workflow status: $workflowsStatus"
-      exit 0
+
     else
       echo "failing with submission status: $submissionStatus and workflow status: $workflowsStatus"
       exit 1
