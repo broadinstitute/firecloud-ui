7f1d5bb1b65fa01b056ceeb46cd8e170dc226cb5
aweng98 <bweng@broadinstitute.org>
2018-11-03 14:24:28 -0400

    testing on workbench-lib 0.12-eb6af64

--- automation/project/Dependencies.scala
+++ automation/project/Dependencies.scala
@@ -4,7 +4,7 @@ import sbt.Keys._
 object Dependencies {
   val jacksonV = "2.9.0"
 
-  val serviceTestV = "0.13-7a663ed"
+  val serviceTestV = "0.12-eb6af64" // "0.13-7a663ed"
 
   val workbenchExclusions = Seq(
     ExclusionRule(organization = "org.broadinstitute.dsde.workbench", name = s"workbench-metrics_2.12")
