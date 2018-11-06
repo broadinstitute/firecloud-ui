6d220d02c7d78d3294aa727099218c86d3cfe6ca
aweng98 <bweng@broadinstitute.org>
2018-11-06 12:44:16 -0500

    login screenshot

--- automation/src/test/scala/org/broadinstitute/dsde/firecloud/fixture/UserFixtures.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/fixture/UserFixtures.scala
@@ -1,6 +1,9 @@
 package org.broadinstitute.dsde.firecloud.fixture
 
 
+import java.io.{File, FileInputStream, FileOutputStream}
+import java.text.SimpleDateFormat
+
 import org.broadinstitute.dsde.firecloud.FireCloudConfig
 import org.scalatest.concurrent.{Eventually, ScaledTimeSpans}
 import org.broadinstitute.dsde.firecloud.page.AuthenticatedPage
@@ -9,9 +12,10 @@ import org.broadinstitute.dsde.firecloud.page.workspaces.WorkspaceListPage
 import org.broadinstitute.dsde.workbench.auth.AuthTokenScopes
 import org.broadinstitute.dsde.workbench.config.Credentials
 import org.broadinstitute.dsde.workbench.service.test.{CleanUp, WebBrowserSpec}
-import org.openqa.selenium.WebDriver
+import org.openqa.selenium.{OutputType, TakesScreenshot, WebDriver}
 import org.scalatest.TestSuite
 import org.broadinstitute.dsde.workbench.service.util.Retry.retry
+import org.openqa.selenium.remote.Augmenter
 
 import scala.concurrent.duration._
 
@@ -70,11 +74,32 @@ trait UserFixtures extends CleanUp with ScaledTimeSpans with Eventually { self: 
       // workaround for failed forceSignedIn
       var counter = 0
       retry(Seq.fill(2)(1.seconds)) ({
-        new SignInPage(FireCloudConfig.FireCloud.baseUrl).open
+
+        logger.info(s"Open page URL ${FireCloudConfig.FireCloud.baseUrl}")
+        executeScript(s"window.location.href='${FireCloudConfig.FireCloud.baseUrl}'")
+
+        logger.info("sleeping 1 second")
+        Thread.sleep(1000)
+
+        val date = new SimpleDateFormat("HH-mm-ss-SSS").format(new java.util.Date())
+        val path = "failure_screenshots"
+        val name = s"${suiteName}_${date}"
+        val screenshotFileName = s"$path/${name}.png"
+        val tmpFile = new Augmenter().augment(webDriver).asInstanceOf[TakesScreenshot].getScreenshotAs(OutputType.FILE)
+        logger.info(s"Login page screenshot saved to $screenshotFileName")
+        new FileOutputStream(new File(screenshotFileName)).getChannel.transferFrom(
+          new FileInputStream(tmpFile).getChannel, 0, Long.MaxValue)
+
+        logger.info("SignIn page wait for ready")
+        await ready new SignInPage(FireCloudConfig.FireCloud.baseUrl)
+
+        logger.info("SignIn page execute Javascript forceSignedIn")
         executeScript(s"window.forceSignedIn('${user.makeAuthToken(scopes).value}')")
+
         if (counter > 0) logger.warn(s"Retrying forceSignedIn. $counter")
         counter +=1
         try {
+          logger.info(("try block: page awaitReady."))
           page.awaitReady()
           Some(page)
         } catch {
@@ -90,7 +115,8 @@ trait UserFixtures extends CleanUp with ScaledTimeSpans with Eventually { self: 
                                                     (testCode: T => Any)
                                                     (implicit webDriver: WebDriver): Unit = {
     withSignIn(user, {
-      new SignInPage(FireCloudConfig.FireCloud.baseUrl).open.signIn(user.email, user.password)
+      new SignInPage(FireCloudConfig.FireCloud.baseUrl).open
+        .signIn(user.email, user.password)
     }, page, testCode)
   }
 
