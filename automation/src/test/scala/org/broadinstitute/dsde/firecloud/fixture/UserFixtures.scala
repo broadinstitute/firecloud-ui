8eef8fc6673236ca9267f614c8cb4142201819c4
aweng98 <bweng@broadinstitute.org>
2018-11-07 19:36:05 -0500

    webDriver page refresh when encounter TimeoutException

--- automation/src/test/scala/org/broadinstitute/dsde/firecloud/fixture/UserFixtures.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/fixture/UserFixtures.scala
@@ -1,6 +1,10 @@
 package org.broadinstitute.dsde.firecloud.fixture
 
 
+import java.io.{File, FileInputStream, FileOutputStream}
+import java.text.SimpleDateFormat
+
+import scala.collection.JavaConverters._
 import org.broadinstitute.dsde.firecloud.FireCloudConfig
 import org.scalatest.concurrent.{Eventually, ScaledTimeSpans}
 import org.broadinstitute.dsde.firecloud.page.AuthenticatedPage
@@ -9,9 +13,11 @@ import org.broadinstitute.dsde.firecloud.page.workspaces.WorkspaceListPage
 import org.broadinstitute.dsde.workbench.auth.AuthTokenScopes
 import org.broadinstitute.dsde.workbench.config.Credentials
 import org.broadinstitute.dsde.workbench.service.test.{CleanUp, WebBrowserSpec}
-import org.openqa.selenium.WebDriver
+import org.openqa.selenium.{OutputType, TakesScreenshot, TimeoutException, WebDriver}
 import org.scalatest.TestSuite
 import org.broadinstitute.dsde.workbench.service.util.Retry.retry
+import org.openqa.selenium.logging.LogType
+import org.openqa.selenium.remote.Augmenter
 
 import scala.concurrent.duration._
 
@@ -58,7 +64,6 @@ trait UserFixtures extends CleanUp with ScaledTimeSpans with Eventually { self: 
         val registrationPage = await ready new RegistrationPage
 
         testCode(registrationPage)
-
     }
   }
 
@@ -68,14 +73,41 @@ trait UserFixtures extends CleanUp with ScaledTimeSpans with Eventually { self: 
                                                 (implicit webDriver: WebDriver): Unit = {
 
     logger.info(s"withSignIn (${user.email}) starting ...")
+
     executeTestCodeWithSignIn(user, {
       // workaround for failed forceSignedIn
       var counter = 0
       retry(Seq.fill(2)(1.seconds)) ({
-        logger.info(s"withSignIn (${user.email}) opening SignInPage ...")
-        new SignInPage(FireCloudConfig.FireCloud.baseUrl).open
+        logger.info(s"withSignIn (${user.email}) opening SignInPage.")
+        try {
+          new SignInPage(FireCloudConfig.FireCloud.baseUrl).open
+        } catch {
+            case _: TimeoutException =>
+              logger.info("Encountered Selenium.TimeoutException. webDriver forcing page refresh.")
+              webDriver.navigate().refresh()
+        }
+
+        // take screenshot and log console text on Login page
+        val date = new SimpleDateFormat("HH-mm-ss-SSS").format(new java.util.Date())
+        val path = "login_page_screenshots"
+        val name = s"${suiteName}_${date}"
+
+        val screenshotFileName = s"$path/${name}.png"
+        val tmpFile = new Augmenter().augment(webDriver).asInstanceOf[TakesScreenshot].getScreenshotAs(OutputType.FILE)
+        logger.info(s"Login page screenshot saved to $screenshotFileName")
+        new FileOutputStream(new File(screenshotFileName)).getChannel.transferFrom(
+          new FileInputStream(tmpFile).getChannel, 0, Long.MaxValue)
+
+        val logFileName = s"$path/${name}_console.txt"
+        val logLines = webDriver.manage().logs().get(LogType.BROWSER).iterator().asScala.toList
+        if (logLines.nonEmpty) {
+          val logString = logLines.map(_.toString).reduce(_ + "\n" + _)
+          new FileOutputStream(new File(logFileName)).write(logString.getBytes)
+        }
+
         logger.info(s"withSignIn (${user.email}) executing script forceSignedIn ...")
         executeScript(s"window.forceSignedIn('${user.makeAuthToken(scopes).value}')")
+
         if (counter > 0) logger.warn(s"Retrying forceSignedIn. $counter")
         counter +=1
         try {
@@ -99,7 +131,8 @@ trait UserFixtures extends CleanUp with ScaledTimeSpans with Eventually { self: 
 
     logger.info(s"withSignInReal: ${user.email} ...")
     executeTestCodeWithSignIn(user, {
-      new SignInPage(FireCloudConfig.FireCloud.baseUrl).open.signIn(user.email, user.password)
+      new SignInPage(FireCloudConfig.FireCloud.baseUrl).open
+        .signIn(user.email, user.password)
     }, page, testCode)
   }
 
