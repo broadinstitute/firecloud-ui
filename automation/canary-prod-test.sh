658644b6cb702e01a2a138f68435c9c9a6d6c3c1
gdlex4015 <garyd@broadinstitute.org>
2018-12-04 11:27:44 -0500

    added timer

--- automation/canary-prod-test.sh
+++ automation/canary-prod-test.sh
@@ -104,7 +104,7 @@ monitorSubmission() {
 
 
 if [ $ENV = "prod" ]; then
-
+    SECONDS=0
     launchSubmission dumbledore.admin@test.firecloud.org broad-firecloud-dsde CanaryTest wdl-testing hello-world participant subject_HCC1143 false
 
     #Monitor the progress of the perf test
@@ -123,13 +123,16 @@ if [ $ENV = "prod" ]; then
     done
 
     if [ "$submissionStatus" == "Done" ] && [ "$workflowsStatus" == "Succeeded" ]; then
+      timer=$SECONDS
       echo "One-off workflow finished within 5 minutes with workflow status: $workflowsStatus"
-      echo "[{\"eventType\":\"CanaryTestProd\",\"type\":\"Workflow\",\"status\": \"$workflowsStatus\"}]" > canary_events.json
+      echo "[{\"eventType\":\"CanaryTestProd\",\"type\":\"Workflow\",\"status\": \"$workflowsStatus\",\"timeToComplete\":\"$timer\"}]" > canary_events.json
       cat canary_events.json | gzip -c | curl --data-binary @- -X POST -H "Content-Type: application/json" -H "X-Insert-Key: 85x1qvfYI2oNARBSxWk6cytmY8EpQhPE" -H "Content-Encoding: gzip" https://insights-collector.newrelic.com/v1/accounts/1862859/events
+
       exit 0
     else
+      timer=$SECONDS
       echo "failing with submission status: $submissionStatus and workflow status: $workflowsStatus"
-      echo "[{"eventType":"CanaryTestProd","type":"Workflow","status": $workflowsStatus}]" > canary_events.json
+      echo "[{\"eventType\":\"CanaryTestProd\",\"type\":\"Workflow\",\"status\": \"$workflowsStatus\",\"timeToComplete\":\"$timer\"}]" > canary_events.json
       cat canary_events.json | gzip -c | curl --data-binary @- -X POST -H "Content-Type: application/json" -H "X-Insert-Key: 85x1qvfYI2oNARBSxWk6cytmY8EpQhPE" -H "Content-Encoding: gzip" https://insights-collector.newrelic.com/v1/accounts/1862859/events
       exit 1
     fi
