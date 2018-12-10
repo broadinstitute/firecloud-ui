db2a90c9f5b6082468179c7431408c05b211c28d
ansingh7115 <singh.an@husky.neu.edu>
2018-12-10 02:43:48 -0500

    real hash

--- automation/project/Dependencies.scala
+++ automation/project/Dependencies.scala
@@ -4,7 +4,7 @@ import sbt.Keys._
 object Dependencies {
   val jacksonV = "2.9.0"
 
-  val serviceTestV = "0.15-c7a8999"
+  val serviceTestV = "0.15-40f9df6"
 
   val workbenchExclusions = Seq(
     ExclusionRule(organization = "org.broadinstitute.dsde.workbench", name = s"workbench-metrics_2.12")
