739d66f5d599fdf0a8e0e0bfa4b26155a77cbb4a
Matt Bemis <mbemis@broadinstitute.org>
2018-12-14 15:57:21 -0500

    wrong prefix

--- src/cljs/main/broadfcui/page/workspace/create.cljs
+++ src/cljs/main/broadfcui/page/workspace/create.cljs
@@ -176,7 +176,7 @@
                    :attributes (if (or (:description props) (not (string/blank? description)))
                                  {:description description}
                                  {})
-                   :copyFilesWithPrefix "/notebooks"
+                   :copyFilesWithPrefix "notebooks/"
                    :authorizationDomain auth-domain}
          :headers ajax/content-type=json
          :on-done (fn [{:keys [success? get-parsed-response]}]
