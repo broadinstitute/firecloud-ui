417e73544df88101f6dd117349b920f983ad3c97
aweng98 <bweng@broadinstitute.org>
2018-12-14 21:49:07 -0500

    retry request statusCode 504

--- automation/project/Dependencies.scala
+++ automation/project/Dependencies.scala
@@ -4,7 +4,7 @@ import sbt.Keys._
 object Dependencies {
   val jacksonV = "2.9.0"
 
-  val serviceTestV = "0.15-46b4a91-SNAP"
+  val serviceTestV = "0.15-2bc5578-SNAP"
 
   val workbenchExclusions = Seq(
     ExclusionRule(organization = "org.broadinstitute.dsde.workbench", name = s"workbench-metrics_2.12")
