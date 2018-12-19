a085c6d2542893fc61f54d707845a7c85d2778f9
aweng98 <bweng@broadinstitute.org>
2018-12-19 13:20:30 -0500

    new snapshot

--- automation/project/Dependencies.scala
+++ automation/project/Dependencies.scala
@@ -4,7 +4,7 @@ import sbt.Keys._
 object Dependencies {
   val jacksonV = "2.9.0"
 
-  val serviceTestV = "0.16-0d3cb2f-SNAP"
+  val serviceTestV = "0.16-a58b505-SNAP"
 
   val workbenchExclusions = Seq(
     ExclusionRule(organization = "org.broadinstitute.dsde.workbench", name = s"workbench-metrics_2.12")
