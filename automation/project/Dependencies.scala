eb8234c0689d082a4959879a1bb3d00f7a451206
aweng98 <bweng@broadinstitute.org>
2019-01-14 12:00:42 -0500

    6e8475d-SNAP

--- automation/project/Dependencies.scala
+++ automation/project/Dependencies.scala
@@ -4,7 +4,7 @@ import sbt.Keys._
 object Dependencies {
   val jacksonV = "2.9.0"
 
-  val serviceTestV = "0.16-bf6aac5-SNAP"
+  val serviceTestV = "0.16-6e8475d-SNAP"
 
   val workbenchExclusions = Seq(
     ExclusionRule(organization = "org.broadinstitute.dsde.workbench", name = s"workbench-metrics_2.12")
