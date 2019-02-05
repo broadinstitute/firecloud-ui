5293b0204b3582ca78b4cf4bd2a80d6a3037ccef
gdlex4015 <garyd@broadinstitute.org>
2019-02-05 17:43:35 -0500

    Gd ignore tests (#1510)
    * ignoring auto complete tests
    
    * fixed compile errors
    
    * ignore tag

--- automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/library/UnpublishedWorkspaceSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/firecloud/test/library/UnpublishedWorkspaceSpec.scala
@@ -83,8 +83,8 @@ class UnpublishedWorkspaceSpec extends FreeSpec with ParallelTestExecution with 
     }
   }
 
-  "DUOS autocomplete" - {
-    "should give multiple results for a partial word" in {
+  "DUOS autocomplete" -  {
+    "should give multiple results for a partial word" ignore {
       val user = UserPool.chooseAnyUser
       // there is no need for an auth token for this test, except that the api wrapper expects one
       implicit val authToken: AuthToken = user.makeAuthToken()
