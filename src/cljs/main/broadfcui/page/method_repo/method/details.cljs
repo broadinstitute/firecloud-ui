a492b5cb3bf45828cd0ace4d66843e6ac62b4fa4
Elizabeth Zhao <elizzhao@broadinstitute.org>
2019-04-12 10:21:58 -0400

    correct or statements

--- src/cljs/main/broadfcui/page/method_repo/method/details.cljs
+++ src/cljs/main/broadfcui/page/method_repo/method/details.cljs
@@ -73,10 +73,10 @@
             :ok-button
             {:text "Yes"
              :onClick #(mc-sync/flag-synchronization)
-             :href (if (or common/has-terra-return? common/has-firecloud-return?)
+             :href (if (common/has-terra-return?)
                      (let [{:keys [namespace name]} (:dest-config-id @state)]
                        (string/join "/" [(config/terra-url) (nav/get-link :workspace-summary (:dest-workspace-id @state)) "tools" namespace name]))
-                       (nav/get-link :workspace-method-config (:dest-workspace-id @state) (:dest-config-id @state)))}}])
+                     (nav/get-link :workspace-method-config (:dest-workspace-id @state) (:dest-config-id @state)))}}])
         [:div {:style {:display "flex" :marginTop "1.5rem" :padding "0 1.5rem" :alignItems "flex-end"}}
          (tab-bar/render-title
           "METHOD"
