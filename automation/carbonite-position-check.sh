5ad4d3d913f1c20dee4e1431c01f293846c1a7c5
Adam Nichols <aednichols@gmail.com>
2020-03-06 15:06:39 -0500

    Comments

--- automation/carbonite-position-check.sh
+++ automation/carbonite-position-check.sh
@@ -7,6 +7,8 @@ WORKING_DIR=$PWD
 
 JSON_CREDS=`docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN -e VAULT_ADDR=https://clotho.broadinstitute.org:8200 broadinstitute/dsde-toolbox vault read -format=json secret/dsde/firecloud/dev/common/firecloud-account.json | jq '.data'`
 
+# Substantially all workflows on alpha were run by the `PerformanceTest-against-Alpha` job, which uses the Harry Potter users.
+# The two target workflows happen to have been run by Hermione, so use her token to inspect metadata.
 ACCESS_TOKEN=$(
         docker \
             run \
@@ -16,6 +18,9 @@ ACCESS_TOKEN=$(
             broadinstitute/dsp-toolbox \
             python get_bearer_token.py "hermione.owner@test.firecloud.org" "${JSON_CREDS}"
      )
+
+# The archiving start position is set by config [0] as `services.MetadataService.config.carbonite-metadata-service.minimum-summary-entry-id`
+# [0] https://github.com/broadinstitute/firecloud-develop/blob/dev/base-configs/cromwell/cromwell.conf.ctmpl#L458-L469
 
 # The last unarchived workflow, position 91824073
 curl \
