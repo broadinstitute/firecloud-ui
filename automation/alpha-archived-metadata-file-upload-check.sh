d265f8f94e1724703ef1f26b29786c3da11d80ee
Saloni Shah <salonipshah11@gmail.com>
2021-04-15 16:13:49 -0400

    Cleanup

---
+++ automation/alpha-archived-metadata-file-upload-check.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -euo pipefail
+
+VAULT_TOKEN=$(cat /etc/vault-token-dsde)
+
+# Get a workflow which completed at least 40 minutes ago and whose metadata is archived
+# (archive-delay in alpha is set to 30 minutes. Hence adding 10 more minutes should give Cromwell enough time to
+# archive such a workflow)
+ARCHIVED_WORKFLOW_RESULT=$(
+  docker run --rm \
+     -e VAULT_TOKEN=$VAULT_TOKEN \
+     broadinstitute/dsde-toolbox mysql-connect.sh -p firecloud -a cromwell1 -e alpha \
+     "SELECT WORKFLOW_EXECUTION_UUID, ROOT_WORKFLOW_EXECUTION_UUID FROM cromwell.WORKFLOW_METADATA_SUMMARY_ENTRY WHERE METADATA_ARCHIVE_STATUS = 'Archived' AND END_TIMESTAMP <= DATE_SUB(NOW(), INTERVAL 40 minute) ORDER BY END_TIMESTAMP DESC LIMIT 1;"\
+     | tail -n1
+)
+
+WORKFLOW_ID=$(echo $ARCHIVED_WORKFLOW_RESULT | cut -d' ' -f1)
+ROOT_WORKFLOW_ID=$(echo $ARCHIVED_WORKFLOW_RESULT | cut -d' ' -f2)
+
+if [[ $ROOT_WORKFLOW_ID == 'NULL' ]]
+then
+  FILE_LOCATION=${WORKFLOW_ID}/${WORKFLOW_ID}.csv
+else
+  FILE_LOCATION=${ROOT_WORKFLOW_ID}/${WORKFLOW_ID}.csv
+fi
+
+echo "Checking if CSV file exists for workflow ${WORKFLOW_ID} in bucket 'cromwell-carbonited-workflows-alpha'..."
+
+FILE_EXISTS=$(gsutil -q stat gs://cromwell-carbonited-workflows-alpha/${FILE_LOCATION}; echo $?)
+
+if [[ $FILE_EXISTS == 0 ]]
+then
+  echo "Archived file exists for ${WORKFLOW_ID}"
+  exit 0
+else
+  echo "Something went wrong while archiving workflow ${WORKFLOW_ID}. Archived file doesn't exist!!"
+  exit 1
+fi
