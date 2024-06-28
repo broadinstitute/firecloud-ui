be3ced636bbab070809678d2f29dafd867b795b8
jyang-broad <85525681+jyang-broad@users.noreply.github.com>
2024-06-28 08:29:09 -0700

    Revert "Revert "[DDO-3749] use gsm instead of vault" (#1633)"
    This reverts commit 2648a79818b6629f5fa92e95984fcd37a2be4ed9.

--- automation/complex-prod-workflow-test.sh
+++ automation/complex-prod-workflow-test.sh
@@ -6,7 +6,6 @@ set -e
 set -x
 
 ENV=$1
-VAULT_TOKEN=${2:-$(cat $HOME/.vault-token)}
 WORKING_DIR=${3:-$PWD}
 NEED_TOKEN=false
 
@@ -18,7 +17,17 @@ else
     echo "Starting complex workflow test in Production"
 fi
 
-JSON_CREDS=`docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN -e VAULT_ADDR=https://clotho.broadinstitute.org:8200 broadinstitute/dsde-toolbox vault read -format=json secret/dsde/firecloud/prod/common/canary/firecloud-account.json | jq '.data'`
+
+DOCKER_ARGS=(
+  "run"
+  "--rm"
+  "-v ${HOME}/.config/gcloud:/root/.config/gcloud"
+  "google/cloud-sdk"
+)
+
+SECRET_ACCESS_ACCOUNT=jenkins-firecloud@broad-dsp-techops.iam.gserviceaccount.com
+# Expand the array of args and pass them to `docker`
+JSON_CREDS=$(docker ${DOCKER_ARGS[*]} /bin/bash -c "gcloud config set account ${SECRET_ACCESS_ACCOUNT} && gcloud secrets versions access latest --project broad-dsde-dev --secret firecloud-sa")
 
 users=(
      dumbledore.admin@test.firecloud.org
@@ -59,7 +68,7 @@ if [ $ENV = "prod" ]; then
 
     do
             echo $i
-            sleep 5m
+            sleep 300
             monitorSubmission dumbledore.admin@test.firecloud.org broad-firecloud-dsde complex-featured-workflow $submissionId
             ((i++))
     done
